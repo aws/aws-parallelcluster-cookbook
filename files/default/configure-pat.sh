924d64d3308ade466d0cabe8b166c1f95caae1f5
Dougal Ballantyne <dougalb@amazon.com>
2015-11-11 12:45:34 -0800

    Making configure pat more universal

--- files/default/configure-pat.sh
+++ files/default/configure-pat.sh
@@ -13,7 +13,7 @@
 
 set -x
 echo "Determining the MAC address on eth0"
-ETH0_MAC=`/sbin/ifconfig  | /bin/grep eth0 | awk '{print tolower($5)}' | grep '^[0-9a-f]\{2\}\(:[0-9a-f]\{2\}\)\{5\}$'`
+ETH0_MAC=`ip addr show eth0 | /bin/grep 'link/ether' | awk '{print tolower($2)}' | grep '^[0-9a-f]\{2\}\(:[0-9a-f]\{2\}\)\{5\}$'`
 if [ $? -ne 0 ] ; then
    echo "Unable to determine MAC address on eth0" | logger -t "ec2"
    exit 1
