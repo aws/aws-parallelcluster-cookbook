3cdc0f4195dd262e735eccd9f5d3eee24b994925
Dougal Ballantyne <dougalb@amazon.com>
2015-11-13 13:28:41 -0800

    Cleanup for automated builds

--- build_ami.sh
+++ build_ami.sh
@@ -3,6 +3,6 @@
 rm -rf ../vendor/cookbooks
 berks vendor ../vendor/cookbooks
 export BUILD_DATE=`date +%Y%m%d%H%M`
-packer build -var-file=packer_variables.json packer_centos6.json
-packer build -var-file=packer_variables.json packer_alinux.json
-packer build -var-file=packer_variables.json packer_ubuntu.json
+packer build -var-file=packer_variables.json packer_centos6.json | tee build_centos6.log
+packer build -var-file=packer_variables.json packer_alinux.json | tee build_alinux.log
+packer build -var-file=packer_variables.json packer_ubuntu.json | tee build_ubuntu.log
