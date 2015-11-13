62a4fe3c174ac592adc95bc6a793adfc53219555
Dougal Ballantyne <dougalb@amazon.com>
2015-11-13 11:07:45 -0800

    Updates for automated builds

--- build_ami.sh
+++ build_ami.sh
@@ -2,5 +2,7 @@
 
 rm -rf ../vendor/cookbooks
 berks vendor ../vendor/cookbooks
+export BUILD_DATE=`date +%Y%m%d%H%M`
 packer build -var-file=packer_variables.json packer_centos6.json
 packer build -var-file=packer_variables.json packer_alinux.json
+packer build -var-file=packer_variables.json packer_ubuntu.json
