ee39b1c7f619a4bdde0eca52c03989ebc2593781
Adam Boeglin <boeglina@amazon.com>
2015-12-17 09:13:08 -0800

    Force short hostname with torque compute nodes.

--- recipes/_compute_torque_config.rb
+++ recipes/_compute_torque_config.rb
@@ -21,6 +21,18 @@ template '/var/spool/torque/mom_priv/config' do
   mode '0644'
 end
 
+case node['platform_family']
+when 'rhel'
+  ruby_block "report short name" do
+    block do
+      fe = Chef::Util::FileEdit.new("/etc/sysconfig/pbs_mom")
+      fe.insert_line_if_no_match(/#{node['hostname']}/,
+                               "export args=\"$args -A #{node['hostname']}\"")
+      fe.write_file
+    end
+  end
+end
+
 # Copy pbs_mom service script
 remote_file "install pbs_mom service" do
   path "/etc/init.d/pbs_mom"
