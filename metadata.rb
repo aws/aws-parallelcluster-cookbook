0395fad25399affcd5da5f21c5f29c7b3b0196e9
Dougal Ballantyne <dougalb at amazon dot com>
2016-10-04 08:01:20 -0700

    Updates for Amazon Linux 2016.09

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures cfncluster'
 long_description 'Installs/Configures cfncluster'
 issues_url 'https://github.com/awslabs/cfncluster-cookbook/issues'
 source_url 'https://github.com/awslabs/cfncluster-cookbook'
-version '1.3.0'
+version '1.3.2'
 
 depends 'build-essential', '~> 6.0.3'
 depends 'poise-python', '~> 1.4.0'
