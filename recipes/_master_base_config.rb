b466c0b9ffa4ff7e4b4de3f2b707727011585a1c
Luca Carrogu <carrogu@amazon.com>
2018-05-22 12:45:12 +0200

    fix ruby syntax error "tried to create Proc object without a block"

--- recipes/_master_base_config.rb
+++ recipes/_master_base_config.rb
@@ -62,7 +62,7 @@ end
 # Add volume to /etc/fstab
 mount node['cfncluster']['cfn_shared_dir'] do
   device dev_path
-  fstype(DelayedEvaluator.new) { node['cfncluster']['cfn_volume_fs_type'] }
+  fstype(DelayedEvaluator.new { node['cfncluster']['cfn_volume_fs_type'] })
   options "_netdev"
   pass 0
   action %i[mount enable]
