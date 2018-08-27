6651bdc32546f356062f4afb759b3bcccbc03661
Luca Carrogu <carrogu@amazon.com>
2018-08-27 12:01:10 +0200

    Fix packer build for ubuntu1404 updating awscli version
    This fix the following errors on ubuntu1404:
    - awscli 1.15.40 has requirement botocore==1.10.40%!(PACKER_COMMA)
    but you'll have botocore 1.11.1 which is incompatible.
    - Cannot uninstall 'urllib3'. It is a distutils installed project and
    thus we cannot accurately determine which files belong to it which
    would lead to only a partial uninstall.
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- recipes/base_install.rb
+++ recipes/base_install.rb
@@ -61,7 +61,10 @@ end
 # Install AWSCLI
 python_package 'awscli' do
   action :upgrade
-  version '1.15.40'
+  version '1.16.2'
+  if node['platform'] == 'ubuntu' && node['platform_version'] == "14.04"
+    install_options '--ignore-installed urllib3'
+  end
 end
 
 # Install boto3
