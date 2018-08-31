2ef6178f26f9699fca6d8f4b49323802a72ce2c5
Luca Carrogu <carrogu@amazon.com>
2018-08-31 08:50:26 +0200

    Unpin awscli package version
    Install latest version of awscli at packer time, or do nothing if
    already installed, so to avoid to break package dependencies.
    
    For CentOS6 and CentOS7, we need also to cleanup any old awscli package
    installed via staging scripts, in order to avoid package dependencies
    break when installing latest boto3 package.
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- recipes/_update_packages.rb
+++ recipes/_update_packages.rb
@@ -13,16 +13,36 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-case node['platform_family']
-when 'rhel', 'amazon'
-  execute 'yum-update' do
-    command "yum -y update && package-cleanup -y --oldkernels --count=1"
-  end
-when 'debian'
-  execute 'apt-update' do
-    command "apt-get update"
-  end
-  execute 'apt-upgrade' do
-    command "DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::=\"--force-confdef\" -o Dpkg::Options::=\"--force-confold\" upgrade && apt-get autoremove"
+if node['platform'] == 'centos' && node['platform_version'].to_i >= 7
+  # CentOS7
+  bash 'remove awscli' do
+    code <<-AWSCLI
+      yum -y remove awscli
+    AWSCLI
   end
 end
+
+if not (node['platform'] == 'centos' && node['platform_version'].to_i < 7)
+  # not CentOS6
+  case node['platform_family']
+  when 'rhel', 'amazon'
+    execute 'yum-update' do
+      command "yum -y update && package-cleanup -y --oldkernels --count=1"
+    end
+  when 'debian'
+    execute 'apt-update' do
+      command "apt-get update"
+    end
+    execute 'apt-upgrade' do
+      command "DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::=\"--force-confdef\" -o Dpkg::Options::=\"--force-confold\" upgrade && apt-get autoremove"
+    end
+  end
+else
+  # CentOS6
+  bash 'remove awscli' do
+    code <<-AWSCLI
+      pip uninstall -y awscli
+    AWSCLI
+  end
+end
+
