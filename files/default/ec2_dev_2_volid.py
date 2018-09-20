e5006b9f401089d006fcdc5c81ee5e25ccfd0c9c
Enrico Usai <usai@amazon.com>
2018-09-20 11:51:09 +0200

    Add main function to the ec2_dev_2_volid.py script
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- files/default/ec2_dev_2_volid.py
+++ files/default/ec2_dev_2_volid.py
@@ -7,47 +7,55 @@ import syslog
 import time
 import boto3
 
-# Get dev
-try:
-    dev = str(sys.argv[1])
-except IndexError:
-    syslog.syslog(syslog.LOG_ERR, "Provide block device i.e. xvdf")
-    sys.exit(1)
 
-# Convert dev to mapping format
-if 'nvme' in dev:
-    # For newer instances which expose EBS volumes as NVMe devices, translate the
-    # device name so boto can discover it.
-    output = os.popen('sudo /usr/local/sbin/cfncluster-ebsnvme-id -v /dev/' + dev).read().split(":")[1].strip()
-    print(output)
-    sys.exit(0)
-else:
-    dev = dev.replace('xvd', 'sd')
-    dev = '/dev/' + dev
+def main():
+    syslog.syslog("Starting ec2_dev_2_volid.py script")
+    # Get dev
+    try:
+        dev = str(sys.argv[1])
+        syslog.syslog("Input block device is %s" % dev)
+    except IndexError:
+        syslog.syslog(syslog.LOG_ERR, "Provide block device i.e. xvdf")
 
-# Get instance ID
-instanceId = urllib2.urlopen("http://169.254.169.254/latest/meta-data/instance-id").read()
+    # Convert dev to mapping format
+    if 'nvme' in dev:
+        # For newer instances which expose EBS volumes as NVMe devices, translate the
+        # device name so boto can discover it.
+        output = os.popen('sudo /usr/local/sbin/cfncluster-ebsnvme-id -v /dev/' + dev).read().split(":")[1].strip()
+        print(output)
+        sys.exit(0)
+    else:
+        dev = dev.replace('xvd', 'sd')
+        dev = '/dev/' + dev
 
-# Get region
-region = urllib2.urlopen("http://169.254.169.254/latest/meta-data/placement/availability-zone").read()
-region = region[:-1]
+    # Get instance ID
+    instanceId = urllib2.urlopen("http://169.254.169.254/latest/meta-data/instance-id").read()
 
-# Connect to AWS using boto
-ec2 = boto3.client('ec2', region_name=region)
+    # Get region
+    region = urllib2.urlopen("http://169.254.169.254/latest/meta-data/placement/availability-zone").read()
+    region = region[:-1]
 
-# Poll for blockdevicemapping
-devices = ec2.describe_instance_attribute(InstanceId=instanceId, Attribute='blockDeviceMapping').get('BlockDeviceMappings')
-devmap = dict((d.get('DeviceName'), d) for d in devices)
-x = 0
-while not devmap.has_key(dev):
-    if x == 36:
-        syslog.syslog("Dev %s did not appears in 180 seconds." % dev)
-        sys.exit(1)
-    syslog.syslog("Looking for dev %s in devmap %s" % (dev, devmap))
-    time.sleep(5)
+    # Connect to AWS using boto
+    ec2 = boto3.client('ec2', region_name=region)
+
+    # Poll for blockdevicemapping
     devices = ec2.describe_instance_attribute(InstanceId=instanceId, Attribute='blockDeviceMapping').get('BlockDeviceMappings')
     devmap = dict((d.get('DeviceName'), d) for d in devices)
-    x += 1
+    x = 0
+    while not devmap.has_key(dev):
+        if x == 36:
+            syslog.syslog("Dev %s did not appears in 180 seconds." % dev)
+            sys.exit(1)
+        syslog.syslog("Looking for dev %s in devmap %s" % (dev, devmap))
+        time.sleep(5)
+        devices = ec2.describe_instance_attribute(InstanceId=instanceId, Attribute='blockDeviceMapping').get('BlockDeviceMappings')
+        devmap = dict((d.get('DeviceName'), d) for d in devices)
+        x += 1
 
-volumeId = devmap.get(dev).get('Ebs').get('VolumeId')
-print(volumeId)
+    # Return volumeId
+    volumeId = devmap.get(dev).get('Ebs').get('VolumeId')
+    print(volumeId)
+
+
+if __name__ == '__main__':
+    main()
