76b08496adb5b6c6cac4a52fff11aab6d9cab120
Luca Carrogu <carrogu@amazon.com>
2018-10-02 12:30:03 +0200

    Add more tests at the end of the Kitchen runs
    Check if some scheduler commands can be executed (on Master Node) and
    if the scheduler root is mounted (on Compute Node) for SGE and Slurm.
    
    Check if some scheduler commands can be executed (both on Master and
    Compute Node) for Torque.
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- recipes/tests.rb
+++ recipes/tests.rb
@@ -18,3 +18,71 @@ execute 'execute awscli' do
   environment('PATH' => '/usr/local/bin:/usr/bin/:$PATH')
   user node['cfncluster']['cfn_cluster_user']
 end
+
+if node['cfncluster']['cfn_scheduler'] == 'sge'
+  case node['cfncluster']['cfn_node_type']
+  when 'MasterServer'
+    execute 'execute qhost' do
+      command "qhost -help"
+      environment('PATH' => '/opt/sge/bin:/opt/sge/bin/lx-amd64:/bin:/usr/bin:$PATH', 'SGE_ROOT' => '/opt/sge')
+      user node['cfncluster']['cfn_cluster_user']
+    end
+
+    execute 'execute qstat' do
+      command "qstat -help"
+      environment('PATH' => '/opt/sge/bin:/opt/sge/bin/lx-amd64:/bin:/usr/bin:$PATH', 'SGE_ROOT' => '/opt/sge')
+      user node['cfncluster']['cfn_cluster_user']
+    end
+
+    execute 'execute qsub' do
+      command "qsub -help"
+      environment('PATH' => '/opt/sge/bin:/opt/sge/bin/lx-amd64:/bin:/usr/bin:$PATH', 'SGE_ROOT' => '/opt/sge')
+      user node['cfncluster']['cfn_cluster_user']
+    end
+  when 'ComputeFleet'
+    execute 'ls sge root' do
+      command "ls /opt/sge"
+      user node['cfncluster']['cfn_cluster_user']
+    end
+  else
+    raise "cfn_node_type must be MasterServer or ComputeFleet"
+  end
+end
+
+if node['cfncluster']['cfn_scheduler'] == 'torque'
+  execute 'execute qstat' do
+    command "qstat --version"
+    environment('PATH' => '/opt/torque/bin:/opt/torque/sbin:$PATH')
+    user node['cfncluster']['cfn_cluster_user']
+  end
+
+  execute 'execute qsub' do
+    command "qsub --version"
+    environment('PATH' => '/opt/torque/bin:/opt/torque/sbin:$PATH')
+    user node['cfncluster']['cfn_cluster_user']
+  end
+end
+
+if node['cfncluster']['cfn_scheduler'] == 'slurm'
+  case node['cfncluster']['cfn_node_type']
+  when 'MasterServer'
+    execute 'execute sinfo' do
+      command "sinfo --help"
+      environment('PATH' => '/opt/slurm/bin:/bin:/usr/bin:$PATH')
+      user node['cfncluster']['cfn_cluster_user']
+    end
+
+    execute 'execute scontrol' do
+      command "scontrol --help"
+      environment('PATH' => '/opt/slurm/bin:/bin:/usr/bin:$PATH')
+      user node['cfncluster']['cfn_cluster_user']
+    end
+  when 'ComputeFleet'
+    execute 'ls slurm root' do
+      command "ls /opt/slurm"
+      user node['cfncluster']['cfn_cluster_user']
+    end
+  else
+    raise "cfn_node_type must be MasterServer or ComputeFleet"
+  end
+end
