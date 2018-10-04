1e30b06c13312492b9ec3e1e67007e8a9e7207c3
Sean Smith <seaam@amazon.com>
2018-10-04 12:52:27 -0700

    Add AWS Batch Recipe
    Signed-off-by: Sean Smith <seaam@amazon.com>

---
+++ recipes/awsbatch_config.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: cfncluster
+# Recipe:: aws_batch_config
+#
+# Copyright 2013-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the
+# License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Use these recipes to add a custom scheduler
+include_recipe 'cfncluster::base_config'
+include_recipe 'cfncluster::base_install'
