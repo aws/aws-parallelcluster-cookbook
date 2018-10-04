4b07ac63860cea10850e6360f4ddf73e71313c60
Balaji Sridharan <fnubalaj@amazon.com>
2018-10-04 15:11:58 -0700

    Update version
    Signed-off-by: Balaji Sridharan <fnubalaj@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,7 +19,7 @@ default['cfncluster']['sources_dir'] = "#{node['cfncluster']['base_dir']}/source
 default['cfncluster']['scripts_dir'] = "#{node['cfncluster']['base_dir']}/scripts"
 default['cfncluster']['license_dir'] = "#{node['cfncluster']['base_dir']}/licenses"
 # Python packages
-default['cfncluster']['cfncluster-node-version'] = '1.6.0rc3'
+default['cfncluster']['cfncluster-node-version'] = '1.6.0rc4'
 default['cfncluster']['cfncluster-supervisor-version'] = '3.3.1'
 # URLs to software packages used during install receipes
 # Gridengine software
