2e0fbc27915614eadc637c09eef7c7bff7631a90
Rex Chen <shuningc@amazon.com>
2018-10-04 14:08:14 -0700

    Adding support for multiple EBS volumes
    - Takes in comma separated list of shared directory name and volume-id as string through dna.json, parse into list in program.
    
    - Run for each loop for each volume-id, and carry out all attach and mounting procedure in _master_base_config. Note: it is crucial the execute or ruby block within the for each loop to have a unique name, so that the workflow is independent for each volume-id.
    
    - Run for each loop for each shared directory, and carry out all NFS mounting procedure in _compute_base_config.
    
    - Note: it is crucial to use mountpoints throughout the program, for example, if the user want to mount to a share directory called "apps", we need to mount to "/apps" throughout the program. Mounting to "apps", without the slash, will cause lots of error and prevent AutoScalingGroup from creating.
    
    - This change is to prepare cfncluster for mounting multiple EBS volumes. This cookbook modification should remain valid for arbitary number of EBS volumes, as long as the input format is consistent.
    
    Signed-off-by: Rex Chen <shuningc@amazon.com>

--- recipes/_compute_base_config.rb
+++ recipes/_compute_base_config.rb
@@ -13,26 +13,11 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Created shared mount point
-directory node['cfncluster']['cfn_shared_dir'] do
-  mode '1777'
-  owner 'root'
-  group 'root'
-  recursive true
-  action :create
-end
 
 node.default['cfncluster']['cfn_master'] = node['cfncluster']['cfn_master'].split('.')[0]
 
 nfs_master = node['cfncluster']['cfn_master']
 
-# Mount shared volume over NFS
-mount node['cfncluster']['cfn_shared_dir'] do
-  device "#{nfs_master}:#{node['cfncluster']['cfn_shared_dir']}"
-  fstype 'nfs'
-  options 'hard,intr,noatime,vers=3,_netdev'
-  action %i[mount enable]
-end
 
 # Mount /home over NFS
 mount '/home' do
@@ -63,6 +48,33 @@ user node['cfncluster']['cfn_cluster_user'] do
   comment 'cfncluster user'
   home "/home/#{node['cfncluster']['cfn_cluster_user']}"
   shell '/bin/bash'
+end
+
+# Parse shared directory info and turn into an array
+_shared_dir_array = node['cfncluster']['cfn_shared_dir'].split(',')
+_shared_dir_array.each_with_index do |dir, index|
+    _shared_dir_array[index] = dir.strip
+    _shared_dir_array[index] = "/"+_shared_dir_array[index]
+end
+
+# Mount each volume with NFS
+_shared_dir_array.each do |dirname|
+    # Created shared mount point
+    directory dirname do
+      mode '1777'
+      owner 'root'
+      group 'root'
+      recursive true
+      action :create
+    end
+
+    # Mount shared volume over NFS
+    mount dirname do
+      device "#{nfs_master}:#{dirname}"
+      fstype 'nfs'
+      options 'hard,intr,noatime,vers=3,_netdev'
+      action %i[mount enable]
+    end
 end
 
 # Install nodewatcher.cfg
