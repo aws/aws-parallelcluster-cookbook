2f3ccefe62688471ff45770ff7f33acc45299c37
Sean Smith <seaam@amazon.com>
2018-12-14 10:47:34 -0800

    Release 2.1.0
    Signed-off-by: Sean Smith <seaam@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,8 +19,8 @@ default['cfncluster']['sources_dir'] = "#{node['cfncluster']['base_dir']}/source
 default['cfncluster']['scripts_dir'] = "#{node['cfncluster']['base_dir']}/scripts"
 default['cfncluster']['license_dir'] = "#{node['cfncluster']['base_dir']}/licenses"
 # Python packages
-default['cfncluster']['cfncluster-version'] = '2.0.2'
-default['cfncluster']['cfncluster-node-version'] = '2.0.2'
+default['cfncluster']['cfncluster-version'] = '2.1.0'
+default['cfncluster']['cfncluster-node-version'] = '2.1.0'
 default['cfncluster']['cfncluster-supervisor-version'] = '3.3.1'
 # URLs to software packages used during install recipes
 # Gridengine software
