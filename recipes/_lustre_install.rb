c20d89f412a02ea9c7a839df119766a7e03f7feb
Luca Carrogu <carrogu@amazon.com>
2019-02-25 16:47:58 +0100

    Define Lustre variables only for Centos
    Lustre version is defined only in Centos
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- recipes/_lustre_install.rb
+++ recipes/_lustre_install.rb
@@ -13,10 +13,10 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-lustre_kmod_rpm = "#{node['cfncluster']['sources_dir']}/kmod-lustre-client-#{node['cfncluster']['lustre']['version']}.x86_64.rpm"
-lustre_client_rpm = "#{node['cfncluster']['sources_dir']}/lustre-client-#{node['cfncluster']['lustre']['version']}.x86_64.rpm"
-
 if node['platform'] == 'centos'
+  lustre_kmod_rpm = "#{node['cfncluster']['sources_dir']}/kmod-lustre-client-#{node['cfncluster']['lustre']['version']}.x86_64.rpm"
+  lustre_client_rpm = "#{node['cfncluster']['sources_dir']}/lustre-client-#{node['cfncluster']['lustre']['version']}.x86_64.rpm"
+
   # Get Lustre Kernel Module RPM
   remote_file lustre_kmod_rpm do
     source node['cfncluster']['lustre']['kmod_url']
