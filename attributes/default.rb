1f1ec9b5fb9d01db474fa54becef48ac52078fdd
Luca Carrogu <carrogu@amazon.com>
2019-04-01 16:48:19 +0200

    Fix installation of kernel devel packages on Ubuntu 14
    Kernel devel packages are needed for NVIDIA driver installation.
    For older kernel version install linux-image-extra
    For newer kernel version install linux-generic
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -125,7 +125,8 @@ when 'debian'
                                               apache2 libboost-dev libdb-dev tcsh libssl-dev libncurses5-dev libpam0g-dev libxt-dev
                                               libmotif-dev libxmu-dev libxft-dev libhwloc-dev man-db lvm2 libmpich-dev libopenmpi-dev
                                               r-base libatlas-dev libblas-dev libfftw3-dev libffi-dev libssl-dev libxml2-dev mdadm]
-  default['cfncluster']['kernel_devel_pkg']['name'] = "linux-generic"
+  default['cfncluster']['kernel_generic_pkg'] = "linux-generic"
+  default['cfncluster']['kernel_extra_pkg'] = "linux-image-extra-#{node['kernel']['release']}"
   default['cfncluster']['ganglia']['apache_user'] = 'www-data'
   default['cfncluster']['ganglia']['gmond_service'] = 'ganglia-monitor'
   default['cfncluster']['ganglia']['httpd_service'] = 'apache2'
