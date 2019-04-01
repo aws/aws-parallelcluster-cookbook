6b6e00a4020cd2369eb989a031d7efa5c5b8955d
Enrico Usai <usai@amazon.com>
2019-04-01 10:53:01 +0200

    Install linux-generic package in place of linux-image-extra
    We were using the kernel-release as linux-image-extra package version.
    In this specific moment the kernel is 3.13.0-167-generic but the
    linux-image-extra-3.13.0-167-generic doesn't exist.
    
    We do not want to install the image-extra package directly.
    Instead, we are now using the linux-generic meta-package,
    which will ensure that upgrades work correctly, and that supporting
    packages are also installed.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -125,13 +125,7 @@ when 'debian'
                                               apache2 libboost-dev libdb-dev tcsh libssl-dev libncurses5-dev libpam0g-dev libxt-dev
                                               libmotif-dev libxmu-dev libxft-dev libhwloc-dev man-db lvm2 libmpich-dev libopenmpi-dev
                                               r-base libatlas-dev libblas-dev libfftw3-dev libffi-dev libssl-dev libxml2-dev mdadm]
-  if Chef::VersionConstraint.new('< 16.04').include?(node['platform_version'])
-    default['cfncluster']['kernel_devel_pkg']['name'] = "linux-image-extra"
-    default['cfncluster']['kernel_devel_pkg']['version'] = node['kernel']['release']
-  else
-    default['cfncluster']['kernel_devel_pkg']['name'] = ""
-    default['cfncluster']['kernel_devel_pkg']['version'] = ""
-  end
+  default['cfncluster']['kernel_devel_pkg']['name'] = "linux-generic"
   default['cfncluster']['ganglia']['apache_user'] = 'www-data'
   default['cfncluster']['ganglia']['gmond_service'] = 'ganglia-monitor'
   default['cfncluster']['ganglia']['httpd_service'] = 'apache2'
