308d26a48eb0bb38305caee6919ee5268ba778c6
Luca Carrogu <carrogu@amazon.com>
2019-04-23 19:09:04 +0200

    Delete previous network configuration left by cloud-init
    This patch avoids network service restart failures when a configuration
    file of an old network interface (not present anymore in the current
    instance launch) was found
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- files/default/ami_cleanup.sh
+++ files/default/ami_cleanup.sh
@@ -5,5 +5,12 @@ rm -rf /var/lib/cloud/instances/*
 rm -f /var/lib/cloud/instance
 rm -rf /etc/ssh/ssh_host_*
 rm -f /etc/udev/rules.d/70-persistent-net.rules
+for ifcfg in $(ls /etc/sysconfig/network-scripts/ifcfg-*)
+do
+    if [ "$(basename ${ifcfg})" != "ifcfg-lo" ]
+    then
+        rm -f "${ifcfg}"
+    fi
+done
 find /var/log -type f -exec /bin/rm -v {} \;
 touch /var/log/lastlog
