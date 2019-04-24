0c7cec244fecd2cef5fd1936a07c05a6654f3aa7
Francesco De Martino <fdm@amazon.com>
2019-04-24 13:29:26 +0200

    ami_cleanup: remove ifcfg-eth0 in centos7
    Related bug https://bugs.centos.org/view.php?id=13836#c33128
    
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- files/default/ami_cleanup.sh
+++ files/default/ami_cleanup.sh
@@ -6,5 +6,12 @@ rm -f /var/lib/cloud/instance
 rm -rf /etc/ssh/ssh_host_*
 rm -f /etc/udev/rules.d/70-persistent-net.rules
 grep -l "Created by cloud-init on instance boot automatically" /etc/sysconfig/network-scripts/ifcfg-* | xargs rm -f
+
+# https://bugs.centos.org/view.php?id=13836#c33128
+source /etc/os-release
+if [ "${ID}${VERSION_ID}" == "centos7" ]; then
+    rm -f /etc/sysconfig/network-scripts/ifcfg-eth0
+fi
+
 find /var/log -type f -exec /bin/rm -v {} \;
 touch /var/log/lastlog
