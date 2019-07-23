8423939bf424f0853d46db1cbcd279398ab314d7
Francesco De Martino <fdm@amazon.com>
2019-07-23 16:01:33 +0200

    efa: skip openmpi removal if efa installed
    openmpi installed by efa was being removed at runtime while
    efa was not being reinstalled.
    
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- recipes/_efa_install.rb
+++ recipes/_efa_install.rb
@@ -30,16 +30,19 @@ case node['platform_family']
 when 'rhel', 'amazon'
   package %w[openmpi-devel openmpi] do
     action :remove
+    not_if { ::Dir.exist?('/opt/amazon/efa') }
   end
 when 'debian'
   package "libopenmpi-dev" do
     action :remove
+    not_if { ::Dir.exist?('/opt/amazon/efa') }
   end
 end
 
 bash "install efa" do
   cwd node['cfncluster']['sources_dir']
   code <<-EFAINSTALL
+    set -e
     tar -xzf #{efa_tarball}
     cd aws-efa-installer
     ./efa_installer.sh -y --skip-limit-conf
