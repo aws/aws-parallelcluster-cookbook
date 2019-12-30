5605b66394a930056221cb91c358acce9fbda40a
Rex <shuningc@amazon.com>
2019-12-30 13:17:52 -0600

    Install debian mysql-dev library to pre-build slurm accounting plugin
    * Installing mysql-dev library for Ubuntu so that slurm accounting mysql plugins are compiled
    * Centos and Alinux already has mysql-devel/mariadb-devel installed, no action needed
    * Add kitchen test to test that mysql accounting plugins are present
    
    Signed-off-by: Rex <shuningc@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -189,7 +189,7 @@ when 'debian'
                                               apache2 libboost-dev libdb-dev tcsh libssl-dev libncurses5-dev libpam0g-dev libxt-dev
                                               libmotif-dev libxmu-dev libxft-dev libhwloc-dev man-db lvm2 libmpich-dev python python-pip
                                               r-base libatlas-dev libblas-dev libfftw3-dev libffi-dev libssl-dev libxml2-dev mdadm
-                                              libgcrypt20-dev]
+                                              libgcrypt20-dev libmysqlclient-dev]
   if node['platform_version'] == '18.04'
     default['cfncluster']['base_packages'].delete('libatlas-dev')
     default['cfncluster']['base_packages'].push('libatlas-base-dev', 'libssl1.0-dev', 'libglvnd-dev')
