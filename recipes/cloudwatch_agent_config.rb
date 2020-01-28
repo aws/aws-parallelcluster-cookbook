c48fd22f9b2a52d414dfdd541060506317061141
Tim Lane <tilne@amazon.com>
2020-01-28 15:43:24 -0800

    Fix CloudWatch agent status check
    The call to `system` in the `not_if` guard of the resource that starts
    the CloudWatch agent uses `[[` instead of `[`, which might break on
    OSes where the default shell does not support it (namely ubuntu). This
    fixes that.
    
    Signed-off-by: Tim Lane <tilne@amazon.com>

--- recipes/cloudwatch_agent_config.rb
+++ recipes/cloudwatch_agent_config.rb
@@ -81,7 +81,7 @@ execute "cloudwatch-agent-start" do
   user 'root'
   command "/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s"
   not_if do
-    system("[[ $(/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a status | jq --raw-output .status) = running ]]") ||
+    system("[ $(/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a status | jq --raw-output .status) = running ]") ||
       node['cfncluster']['cfn_cluster_cw_logging_enabled'] != 'true'
   end
 end
