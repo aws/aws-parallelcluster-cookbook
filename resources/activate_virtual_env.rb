bbfd3656d0be18b4dda0794809b9d7cf9e08a7e4
Tim Lane <tilne@amazon.com>
2020-02-17 20:14:12 -0800

    Use single system pyenv installation
    Use a single system-level pyenv installation instead of multiple user
    installations.
    
    Signed-off-by: Tim Lane <tilne@amazon.com>

--- resources/activate_virtual_env.rb
+++ resources/activate_virtual_env.rb
@@ -3,11 +3,10 @@
 resource_name :activate_virtual_env
 provides :activate_virtual_env
 
-# Resource to create a Python virtual environment for a given user and install a list of packages on it
+# Resource to create a Python virtual environment and install a list of packages on it
 
 property :pyenv_name, String, name_property: true
 property :pyenv_path, String, required: true
-property :pyenv_user, String, default: 'root'
 property :python_version, String, required: true
 property :requirements_path, String, default: ""
 
@@ -16,15 +15,12 @@ default_action :run
 action :run do
   pyenv_script "pyenv virtualenv #{new_resource.pyenv_name}" do
     code "pyenv virtualenv #{new_resource.python_version} #{new_resource.pyenv_name}"
-    user new_resource.pyenv_user
   end
 
   unless new_resource.requirements_path.empty?
     # Copy requirements file
     cookbook_file "#{new_resource.pyenv_path}/requirements.txt" do
       source new_resource.requirements_path
-      owner new_resource.pyenv_user
-      group new_resource.pyenv_user
       mode '0755'
     end
 
@@ -32,7 +28,6 @@ action :run do
     pyenv_pip "#{new_resource.pyenv_path}/requirements.txt" do
       virtualenv new_resource.pyenv_path
       requirement true
-      user new_resource.pyenv_user
     end
   end
 end
