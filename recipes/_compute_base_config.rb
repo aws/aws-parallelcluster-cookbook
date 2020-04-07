aws/aws-parallelcluster-cookbook

commit b569176603c909c7c38d4f9f7402fe901e0fbf96

Skip *_install recipes if AMI is bootstrapped
Skip execution of *_install recipes if AMI is already bootstrapped with same version.
The old behaviour can be restored setting the property skip_install_recipes to "no" through extra_json.

Signed-off-by: Luca Carrogu <carrogu@amazon.com>

recipes/base_install.rb
@@ -15,6 +15,8 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
+return if node['conditions']['ami_bootstrapped']
+
 case node['platform_family']
 when 'rhel', 'amazon'
   include_recipe 'yum'

attributes/conditions.rb
@@ -20,3 +20,5 @@ default['conditions']['efa_supported'] = (node['platform'] == 'centos' && node['
 
 default['conditions']['intel_mpi_supported'] = (node['platform'] == 'centos' && node['platform_version'].to_i >= 7) \
   || node['platform'] == 'amazon' || node['platform'] == 'ubuntu'
+
+default['conditions']['ami_bootstrapped'] = ami_bootstrapped?

attributes/default.rb
@@ -292,3 +292,4 @@ default['cfncluster']['custom_node_package'] = nil
 default['cfncluster']['custom_awsbatchcli_package'] = nil
 default['cfncluster']['cfn_raid_parameters'] = 'NONE'
 default['cfncluster']['cfn_raid_vol_ids'] = nil
+default['cfncluster']['skip_install_recipes'] = 'yes'

libraries/helpers.rb
@@ -138,3 +138,18 @@ def graphic_instance?
 
   is_graphic_instance
 end
+
+#
+# Check if the AMI is bootstrapped
+#
+def ami_bootstrapped?
+  version = ''
+  bootstrapped_file = '/opt/parallelcluster/.bootstrapped'
+
+  if ::File.exist?(bootstrapped_file)
+    version = IO.read(bootstrapped_file).chomp
+    Chef::Log.info("Detected bootstrap file #{version}")
+  end
+
+  'aws-parallelcluster-' + node['cfncluster']['cfncluster-version'] == version && node['cfncluster']['skip_install_recipes'] == 'yes'
+end

recipes/dcv_install.rb
@@ -15,6 +15,8 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+return if node['conditions']['ami_bootstrapped']
+
 # Utility function to install a list of packages
 def install_package_list(packages)
   packages.each do |package_name|

recipes/munge_install.rb
@@ -15,6 +15,8 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
+return if node['conditions']['ami_bootstrapped']
+
 include_recipe 'aws-parallelcluster::base_install'
 
 munge_tarball = "#{node['cfncluster']['sources_dir']}/munge-#{node['cfncluster']['munge']['munge_version']}.tar.gz"

recipes/sge_install.rb
@@ -15,6 +15,8 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
+return if node['conditions']['ami_bootstrapped']
+
 include_recipe 'aws-parallelcluster::base_install'
 
 case node['cfncluster']['cfn_node_type']

recipes/slurm_install.rb
@@ -15,6 +15,8 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
+return if node['conditions']['ami_bootstrapped']
+
 include_recipe 'aws-parallelcluster::base_install'
 include_recipe 'aws-parallelcluster::munge_install'
 

commit 70d8753fdcb81c24c98f33216b15794ea8b626eb

Separate Ganglia installation from configuration
ganglia is now managed by two recipes, one for the installation and one for the configuration.
The installation is executed at AMI build, while the configuration is executed at cluster creation time if ganglia is enabled through the ['ganglia_enabled'] variable.

This solves a regression introduced by https://github.com/aws/aws-parallelcluster-cookbook/pull/563, where the ganglia installation recipe wasn't executed at runtime.

Signed-off-by: Luca Carrogu <carrogu@amazon.com>

recipes/_compute_base_config.rb
@@ -59,19 +59,7 @@ mount '/opt/intel' do
 end
 
 # Configure Ganglia
-if node['cfncluster']['ganglia_enabled'] == "yes"
-  template '/etc/ganglia/gmond.conf' do
-    source 'gmond.conf.erb'
-    owner 'root'
-    group 'root'
-    mode '0644'
-  end
-
-  service node['cfncluster']['ganglia']['gmond_service'] do
-    supports restart: true
-    action %i[enable restart]
-  end
-end
+include_recipe 'aws-parallelcluster::ganglia_config'
 
 # Setup cluster user
 user node['cfncluster']['cfn_cluster_user'] do

recipes/_ganglia_install.rb
@@ -1,65 +1,0 @@
-# frozen_string_literal: true
-
-#
-# Cookbook Name:: aws-parallelcluster
-# Recipe:: _ganglia_install
-#
-# Copyright 2013-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the
-# License. A copy of the License is located at
-#
-# http://aws.amazon.com/apache2.0/
-#
-# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
-# OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
-# limitations under the License.
-
-if node['cfncluster']['ganglia_enabled'] == 'yes'
-  case node['platform']
-  when "redhat", "centos", "amazon", "scientific" # ~FC024
-    package %w[ganglia ganglia-gmond ganglia-gmetad ganglia-web httpd php php-gd rrdtool] do
-      retries 3
-      retry_delay 5
-    end
-
-    cookbook_file 'ganglia-webfrontend.conf' do
-      path '/etc/httpd/conf.d/ganglia.conf'
-      user 'root'
-      group 'root'
-      mode '0644'
-    end
-
-    if node['platform_family'] == 'rhel' && node['platform_version'].to_i == 7 || node['platform'] == 'amazon' && node['platform_version'].to_i == 2
-      # Fix circular dependency multi-user.target -> cloud-init-> gmond -> multi-user.target
-      # gmond is started by chef during cloud-init, but gmond service is configured to start after multi-user.target
-      # which doesn't start until cloud-init run is finished. So gmond service is stuck into starting, which keep
-      # hanging chef until the 600s timeout.
-      replace_or_add "change gmond service dependency" do
-        path "/usr/lib/systemd/system/gmond.service"
-        pattern "After=multi-user.target"
-        line "After=network.target"
-        replace_only true
-      end
-    end
-  when "ubuntu"
-    package %w[ganglia-monitor rrdtool gmetad ganglia-webfrontend] do
-      retries 3
-      retry_delay 5
-    end
-
-    directory '/var/lib/ganglia/rrds' do
-      owner 'ganglia'
-      group 'ganglia'
-      mode 0755
-      recursive true
-      action :create
-    end
-
-    # Setup ganglia-web.conf apache config
-    execute "copy ganglia apache conf" do
-      command "cp /etc/ganglia-webfrontend/apache.conf /etc/apache2/sites-enabled/ganglia.conf"
-      not_if "test -f /etc/apache2/sites-enabled/ganglia.conf"
-    end
-  end
-end

recipes/_master_base_config.rb
@@ -142,37 +142,8 @@ end
 # Setup RAID array on master node
 include_recipe 'aws-parallelcluster::setup_raid_on_master'
 
-# Configure Ganglia on the Master
-if node['cfncluster']['ganglia_enabled'] == 'yes'
-  template '/etc/ganglia/gmetad.conf' do
-    source 'gmetad.conf.erb'
-    owner 'root'
-    group 'root'
-    mode '0644'
-  end
-
-  template '/etc/ganglia/gmond.conf' do
-    source 'gmond.conf.erb'
-    owner 'root'
-    group 'root'
-    mode '0644'
-  end
-
-  service "gmetad" do
-    supports restart: true
-    action %i[enable restart]
-  end
-
-  service node['cfncluster']['ganglia']['gmond_service'] do
-    supports restart: true
-    action %i[enable restart]
-  end
-
-  service node['cfncluster']['ganglia']['httpd_service'] do
-    supports restart: true, reload: true
-    action %i[enable restart]
-  end
-end
+# Configure Ganglia
+include_recipe 'aws-parallelcluster::ganglia_config'
 
 # Setup cluster user
 user node['cfncluster']['cfn_cluster_user'] do

recipes/base_install.rb
@@ -203,7 +203,7 @@ cookbook_file "ami_cleanup.sh" do
 end
 
 # Install Ganglia
-include_recipe "aws-parallelcluster::_ganglia_install"
+include_recipe "aws-parallelcluster::ganglia_install"
 
 # Install NVIDIA and CUDA
 include_recipe "aws-parallelcluster::_nvidia_install"

recipes/ganglia_config.rb
@@ -1,0 +1,89 @@
+# frozen_string_literal: true
+
+#
+# Cookbook Name:: aws-parallelcluster
+# Recipe:: ganglia_config
+#
+# Copyright 2013-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the
+# License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+
+if node['cfncluster']['ganglia_enabled'] == 'yes'
+  case node['cfncluster']['cfn_node_type']
+  when 'MasterServer'
+    case node['platform']
+    when "redhat", "centos", "amazon", "scientific" # ~FC024
+      cookbook_file 'ganglia-webfrontend.conf' do
+        path '/etc/httpd/conf.d/ganglia.conf'
+        user 'root'
+        group 'root'
+        mode '0644'
+      end
+    when "ubuntu"
+      directory '/var/lib/ganglia/rrds' do
+        owner 'ganglia'
+        group 'ganglia'
+        mode 0755
+        recursive true
+        action :create
+      end
+
+      # Setup ganglia-web.conf apache config
+      execute "copy ganglia apache conf" do
+        command "cp /etc/ganglia-webfrontend/apache.conf /etc/apache2/sites-enabled/ganglia.conf"
+        not_if "test -f /etc/apache2/sites-enabled/ganglia.conf"
+      end
+    end
+
+    template '/etc/ganglia/gmetad.conf' do
+      source 'gmetad.conf.erb'
+      owner 'root'
+      group 'root'
+      mode '0644'
+    end
+
+    service "gmetad" do
+      supports restart: true
+      action %i[enable restart]
+    end
+
+    service node['cfncluster']['ganglia']['httpd_service'] do
+      supports restart: true, reload: true
+      action %i[enable restart]
+    end
+  end
+
+  # For ComputeFleet and MasterServer
+
+  if node['platform_family'] == 'rhel' && node['platform_version'].to_i == 7 || node['platform'] == 'amazon' && node['platform_version'].to_i == 2
+    # Fix circular dependency multi-user.target -> cloud-init-> gmond -> multi-user.target
+    # gmond is started by chef during cloud-init, but gmond service is configured to start after multi-user.target
+    # which doesn't start until cloud-init run is finished. So gmond service is stuck into starting, which keep
+    # hanging chef until the 600s timeout.
+    replace_or_add "change gmond service dependency" do
+      path "/usr/lib/systemd/system/gmond.service"
+      pattern "After=multi-user.target"
+      line "After=network.target"
+      replace_only true
+    end
+  end
+
+  template '/etc/ganglia/gmond.conf' do
+    source 'gmond.conf.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
+  end
+
+  service node['cfncluster']['ganglia']['gmond_service'] do
+    supports restart: true
+    action %i[enable restart]
+  end
+end

recipes/ganglia_install.rb
@@ -1,0 +1,45 @@
+# frozen_string_literal: true
+
+#
+# Cookbook Name:: aws-parallelcluster
+# Recipe:: ganglia_install
+#
+# Copyright 2013-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the
+# License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+
+case node['cfncluster']['cfn_node_type']
+when 'MasterServer', nil
+  case node['platform']
+  when "redhat", "centos", "amazon", "scientific" # ~FC024
+    package %w[ganglia ganglia-gmond ganglia-gmetad ganglia-web httpd php php-gd rrdtool] do
+      retries 3
+      retry_delay 5
+    end
+  when "ubuntu"
+    package %w[ganglia-monitor rrdtool gmetad ganglia-webfrontend] do
+      retries 3
+      retry_delay 5
+    end
+  end
+when 'ComputeFleet'
+  case node['platform']
+  when "redhat", "centos", "amazon", "scientific" # ~FC024
+    package %w[ganglia-gmond] do
+      retries 3
+      retry_delay 5
+    end
+  when "ubuntu"
+    package %w[ganglia-monitor] do
+      retries 3
+      retry_delay 5
+    end
+  end
+end