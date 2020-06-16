fe2c55f08fade4152dd9e5c9752c497aedb632b2
Francesco De Martino <fdm@amazon.com>
2020-06-16 14:29:12 +0200

    Dynamically retrieve master node address when not set in dna.json
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- recipes/_compute_base_config.rb
+++ recipes/_compute_base_config.rb
@@ -15,7 +15,17 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-nfs_master = node['cfncluster']['cfn_master']
+# Retrieve master node ip if not set already
+unless node['cfncluster']['cfn_master']
+  ruby_block "retrieve master ip" do
+    block do
+      _, master_private_dns = master_address(node['cfncluster']['cfn_region'], node['cfncluster']['stack_name'])
+      node.force_default['cfncluster']['cfn_master'] = master_private_dns
+    end
+    retries 5
+    retry_delay 3
+  end
+end
 
 # Parse and get RAID shared directory info and turn into an array
 raid_shared_dir = node['cfncluster']['cfn_raid_parameters'].split(',')[0]
@@ -34,7 +44,7 @@ if raid_shared_dir != "NONE"
 
   # Mount RAID directory over NFS
   mount raid_shared_dir do
-    device "#{nfs_master}:#{raid_shared_dir}"
+    device(lazy { "#{node['cfncluster']['cfn_master']}:#{raid_shared_dir}" })
     fstype 'nfs'
     options 'hard,intr,noatime,vers=3,_netdev'
     action %i[mount enable]
@@ -43,7 +53,7 @@ end
 
 # Mount /home over NFS
 mount '/home' do
-  device "#{nfs_master}:/home"
+  device(lazy { "#{node['cfncluster']['cfn_master']}:/home" })
   fstype 'nfs'
   options 'hard,intr,noatime,vers=3,_netdev'
   action %i[mount enable]
@@ -51,7 +61,7 @@ end
 
 # Mount /opt/intel over NFS
 mount '/opt/intel' do
-  device "#{nfs_master}:/opt/intel"
+  device(lazy { "#{node['cfncluster']['cfn_master']}:/opt/intel" })
   fstype 'nfs'
   options 'hard,intr,noatime,vers=3,_netdev'
   action %i[mount enable]
@@ -90,7 +100,7 @@ shared_dir_array.each do |dir|
 
     # Mount shared volume over NFS
     mount dirname do
-      device "#{nfs_master}:#{dirname}"
+      device(lazy { "#{node['cfncluster']['cfn_master']}:#{dirname}" })
       fstype 'nfs'
       options 'hard,intr,noatime,vers=3,_netdev'
       action %i[mount enable]
