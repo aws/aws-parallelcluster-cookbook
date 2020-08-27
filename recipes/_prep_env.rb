7e8a36726b4a802c9eab4565fb97708b11dcf8ae
Enrico Usai <usai@amazon.com>
2020-08-27 10:29:00 +0200

    Retrieve master info and slurm hostname at prep_env time and save in a file
    With this patch we're saving the master_ip and the master_hostname in
    the configuration file of the daemons.
    
    These values will be saved by the daemons in the DB and we're retrieving
    them from the compute nodes at runtime.
    
    We're doing a single query call at prep_env time and saving
    the values in files, to be reused in subsequents calls.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- recipes/_prep_env.rb
+++ recipes/_prep_env.rb
@@ -68,5 +68,50 @@ include_recipe "aws-parallelcluster::_setup_python"
 # Install cloudwatch, write configuration and start it.
 include_recipe "aws-parallelcluster::cloudwatch_agent_config"
 
+# retrieve compute and master node info from dynamodb and save into files
+if node['cfncluster']['cfn_scheduler'] == 'slurm' && node['cfncluster']['cfn_node_type'] == "ComputeFleet"
+
+  # Ensure slurm plugin directory is in place
+  directory "#{node['cfncluster']['slurm_plugin_dir']}" do
+    user 'slurm'
+    group 'slurm'
+    mode '0755'
+    action :create
+    recursive true
+  end
+
+  ruby_block "retrieve compute node info" do
+    block do
+      slurm_nodename, master_private_ip, master_private_dns = hit_dynamodb_info
+      node.run_state['slurm_nodename'] = slurm_nodename
+      node.run_state['cfn_master'] = master_private_dns
+      node.run_state['cfn_master_private_ip'] = master_private_ip
+    end
+    retries 5
+    retry_delay 3
+  end
+
+  file "#{node['cfncluster']['slurm_plugin_dir']}/slurm_nodename" do
+    content(lazy { node.run_state['slurm_nodename'] })
+    mode '0644'
+    owner 'root'
+    group 'root'
+  end
+
+  file "#{node['cfncluster']['slurm_plugin_dir']}/master_private_dns" do
+    content(lazy { node.run_state['cfn_master'] })
+    mode '0644'
+    owner 'root'
+    group 'root'
+  end
+
+  file "#{node['cfncluster']['slurm_plugin_dir']}/master_private_ip" do
+    content(lazy { node.run_state['cfn_master_private_ip'] })
+    mode '0644'
+    owner 'root'
+    group 'root'
+  end
+end
+
 # Configure hostname and DNS
 include_recipe "aws-parallelcluster::dns_config"
