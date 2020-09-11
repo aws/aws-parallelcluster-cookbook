d55980fd1e9e1b736508d220cdad847b4c7fb749
Hanwen <hanwenli@amazon.com>
2020-09-11 13:53:20 -0700

    Disallow using AMIs created by other versions of pcluster
    As a result, a AMI created by other versions of pcluster cannot be used as a BASE_AMI in AMI creation or a custom_AMI in cluster creation.
    
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -145,13 +145,19 @@ end
 def ami_bootstrapped?
   version = ''
   bootstrapped_file = '/opt/parallelcluster/.bootstrapped'
+  current_version = 'aws-parallelcluster-' + node['cfncluster']['cfncluster-version']
 
   if ::File.exist?(bootstrapped_file)
     version = IO.read(bootstrapped_file).chomp
     Chef::Log.info("Detected bootstrap file #{version}")
+    if version != current_version
+      raise "This AMI was created with " + version + ", but is trying to be used with " +  current_version +
+                ". Please either use an AMI created with " + current_version + " or change your ParallelCluster to "+
+                version
+    end
   end
 
-  'aws-parallelcluster-' + node['cfncluster']['cfncluster-version'] == version && ( node['cfncluster']['skip_install_recipes'] == 'yes' || node['cfncluster']['skip_install_recipes'] == true )
+  version != '' && ( node['cfncluster']['skip_install_recipes'] == 'yes' || node['cfncluster']['skip_install_recipes'] == true )
 end
 
 #
