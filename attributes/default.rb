e1b07d392fceeb836a821c35a2c80d8e090d256a
Hanwen <68928867+hanwen-pcluste@users.noreply.github.com>
2020-09-29 16:08:31 -0700

    Track and validate cookbook version in .bootstrapped file (#748)
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -53,6 +53,7 @@ default['cfncluster']['intelmpi']['version'] = '2019.7.217'
 default['cfncluster']['intelmpi']['modulefile'] = "/opt/intel/impi/#{node['cfncluster']['intelmpi']['version']}/intel64/modulefiles/mpi"
 # Python packages
 default['cfncluster']['cfncluster-version'] = '2.9.1'
+default['cfncluster']['cfncluster-cookbook-version'] = '2.9.1'
 default['cfncluster']['cfncluster-node-version'] = '2.9.1'
 # URLs to software packages used during install recipes
 # Gridengine software
