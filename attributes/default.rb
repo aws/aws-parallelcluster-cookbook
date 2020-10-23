86e1df1e963c9bcc13e98fe4a8d8af1d6a4889ff
Rex <shuningc@amazon.com>
2020-10-23 15:50:36 -0700

    CentOS 8: Install Intel HPC packages
    * Enable installation of Intel HPC packages for CentOS 8
    * Install Intel PSXE 2020, which does not require yum4 to be used
    
    Signed-off-by: Rex <shuningc@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -46,8 +46,14 @@ default['cfncluster']['cookbook_virtualenv_path'] = "#{node['cfncluster']['syste
 default['cfncluster']['node_virtualenv_path'] = "#{node['cfncluster']['system_pyenv_root']}/versions/#{node['cfncluster']['python-version']}/envs/#{node['cfncluster']['node_virtualenv']}"
 
 # Intel Packages
-default['cfncluster']['psxe']['version'] = '2019.5'
-default['cfncluster']['intelhpc']['version'] = '2018.0-*.el7'
+default['cfncluster']['psxe']['version'] = '2020.4'
+default['cfncluster']['intelhpc']['platform_name'] = value_for_platform(
+  'centos' => {
+    '~>8' => 'el8',
+    '~>7' => 'el7'
+  }
+)
+default['cfncluster']['intelhpc']['version'] = "2018.0-*.#{node['cfncluster']['intelhpc']['platform_name']}"
 default['cfncluster']['intelpython2']['version'] = '2019.4'
 default['cfncluster']['intelpython3']['version'] = '2019.4'
 
