aws/aws-parallelcluster-cookbook

commit 2c0de640eda12248c606bdf8428923ef866c6c5a

Enable base config logic for CentOS 8
* Modify logic to get EBS device to volume id mapping. Specifically ec2_dev_2_volid.py and parallelcluster-ebsnvme-id are modified for CentOS 8 to use nvme-cli to retrieve volume id for a device following this guide: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/nvme-ebs-volumes.html#identify-nvme-ebs-device
* Install firewalld so iptables executable becomes available
* Enable EPEL repo by default
* Restore logic of running nfs::server recipe when Ubuntu. Original issue is still relevant
* Address comments from base_install commit

Signed-off-by: Rex <shuningc@amazon.com>

files/centos-8/ec2_dev_2_volid.py
@@ -1,0 +1,71 @@
+# Same as default/ec2_dev_2_volid.py except call to /usr/local/sbin/parallelcluster-ebsnvme-id
+import requests
+import sys
+import os
+import syslog
+import time
+import boto3
+import configparser
+from botocore.config import Config
+
+
+def main():
+    syslog.syslog("Starting ec2_dev_2_volid.py script")
+    # Get dev
+    try:
+        dev = str(sys.argv[1])
+        syslog.syslog("Input block device is %s" % dev)
+    except IndexError:
+        syslog.syslog(syslog.LOG_ERR, "Provide block device i.e. xvdf")
+
+    # Convert dev to mapping format
+    if 'nvme' in dev:
+        # For newer instances which expose EBS volumes as NVMe devices, translate the
+        # device name so boto can discover it.
+        output = os.popen('sudo /usr/local/sbin/parallelcluster-ebsnvme-id /dev/' + dev).read().split(":")[1].strip()
+        print(output)
+        sys.exit(0)
+    else:
+        dev = dev.replace('xvd', 'sd')
+        dev = '/dev/' + dev
+
+    # Get instance ID
+    instanceId = requests.get("http://169.254.169.254/latest/meta-data/instance-id").text
+
+    # Get region
+    region = requests.get("http://169.254.169.254/latest/meta-data/placement/availability-zone").text
+    region = region[:-1]
+
+    # Parse configuration file to read proxy settings
+    config = configparser.RawConfigParser()
+    config.read('/etc/boto.cfg')
+    proxy_config = Config()
+    if config.has_option('Boto', 'proxy') and config.has_option('Boto', 'proxy_port'):
+        proxy = config.get('Boto', 'proxy')
+        proxy_port = config.get('Boto', 'proxy_port')
+        proxy_config = Config(proxies={'https': "{0}:{1}".format(proxy, proxy_port)})
+
+    # Connect to AWS using boto
+    ec2 = boto3.client('ec2', region_name=region, config=proxy_config)
+
+    # Poll for blockdevicemapping
+    devices = ec2.describe_instance_attribute(InstanceId=instanceId, Attribute='blockDeviceMapping').get('BlockDeviceMappings')
+    devmap = dict((d.get('DeviceName'), d) for d in devices)
+    x = 0
+    while dev not in devmap:
+        if x == 36:
+            syslog.syslog("Dev %s did not appears in 180 seconds." % dev)
+            sys.exit(1)
+        syslog.syslog("Looking for dev %s in devmap %s" % (dev, devmap))
+        time.sleep(5)
+        devices = ec2.describe_instance_attribute(InstanceId=instanceId, Attribute='blockDeviceMapping').get('BlockDeviceMappings')
+        devmap = dict((d.get('DeviceName'), d) for d in devices)
+        x += 1
+
+    # Return volumeId
+    volumeId = devmap.get(dev).get('Ebs').get('VolumeId')
+    print(volumeId)
+
+
+if __name__ == '__main__':
+    main()

files/centos-8/parallelcluster-ebsnvme-id
@@ -1,0 +1,24 @@
+#!/bin/bash
+
+# Copyright (C) 2020 Amazon.com, Inc. or its affiliates.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#    http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the
+# License.
+
+# Usage:
+# Read EBS device information and provide information about the volume.
+
+# Sample output: vol067f083a4f6xxxxx
+# Insert '-' after 'vol' so that output looks like vol-067f083a4f6xxxxx
+vol_id=$(sudo nvme id-ctrl -v ${1} | grep -oP "sn\s+:\s\K(.+)" | sed 's/^vol/&-/')
+echo "Volume ID: ${vol_id}"

attributes/default.rb
@@ -204,6 +204,14 @@ when 'rhel', 'amazon'
                                                   libical-devel postgresql-devel postgresql-server sendmail libxml2-devel libglvnd-devel mdadm python python-pip
                                                   libssh2-devel libgcrypt-devel libevent-devel glibc-static bind-utils]
     end
+    if node['platform_version'].to_i == 8
+      # Install python3 instead of unversioned python
+      default['cfncluster']['base_packages'].delete('python')
+      default['cfncluster']['base_packages'].delete('python-pip')
+      # Install firewalld for iptables used in configure-pat.sh
+      # Install nvme-cli package used to retrieve info about EBS volumes in parallelcluster-ebsnvme-id
+      default['cfncluster']['base_packages'].push(%w[python3 python3-pip firewalld nvme-cli])
+    end
     if node['platform_version'].to_i >= 8
       # gdisk required for FSx
       # environment-modules required for IntelMPI

recipes/_update_packages.rb
@@ -19,7 +19,7 @@ unless node['platform'] == 'centos' && node['platform_version'].to_i < 7
   # not CentOS6
   case node['platform_family']
   when 'rhel', 'amazon'
-    if node['platform_version'].to_i == 8
+    if node['platform'] == 'centos' && node['platform_version'].to_i == 8
       execute 'dnf-update' do
         command "dnf -y update"
       end

recipes/base_config.rb
@@ -38,7 +38,6 @@ include_recipe 'aws-parallelcluster::chrony_config'
 # EFA runtime configuration
 include_recipe "aws-parallelcluster::efa_config"
 
-# case node['cfncluster']['cfn_node_type']
 case node['cfncluster']['cfn_node_type']
 when 'MasterServer'
   include_recipe 'aws-parallelcluster::_master_base_config'

recipes/base_install.rb
@@ -26,14 +26,16 @@ when 'rhel', 'amazon'
     include_recipe "yum-epel"
   end
 
-  if node['platform_version'].to_i == 7
+  unless node['platform_version'].to_i < 7
     execute 'yum-config-manager_skip_if_unavail' do
       command "yum-config-manager --setopt=\*.skip_if_unavailable=1 --save"
     end
-  elsif node['platform_version'].to_i == 8
+  end
+  if node['platform'] == 'centos' && node['platform_version'].to_i == 8
     # Enable PowerTools Repo so *-devel packages can be installed with DNF
-    execute 'dnf enable powertools' do
-      command "dnf config-manager --set-enabled PowerTools"
+    # Enable EPEL repos
+    execute 'dnf enable powertools and EPEL repos' do
+      command "dnf config-manager --set-enabled PowerTools && dnf install -y epel-release"
     end
   end
 
@@ -74,7 +76,8 @@ package "install kernel packages" do
   case node['platform_family']
   when 'rhel', 'amazon'
     package_name node['cfncluster']['kernel_devel_pkg']['name']
-    if node['platform_version'].to_i < 8
+    if node['platform'] == 'centos' && node['platform_version'].to_i < 8
+      # Do not enforce kernel_devel version on CentOS8 because kernel_devel package with same version as kernel release version cannot be found
       version node['cfncluster']['kernel_devel_pkg']['version']
     end
   when 'debian'
@@ -120,6 +123,10 @@ cookbook_file 'AWS-ParallelCluster-License-README.txt' do
   mode '0644'
 end
 
+if node['platform'] == 'ubuntu' && node['platform_version'].to_f >= 16.04
+  # FIXME: https://github.com/atomic-penguin/cookbook-nfs/issues/93
+  include_recipe "nfs::server"
+end
 if node['platform'] == 'centos' && node['platform_version'].to_i == 8
   # FIXME: https://github.com/atomic-penguin/cookbook-nfs/issues/116
   include_recipe "nfs::server"
@@ -219,23 +226,18 @@ end
 # Install Ganglia
 include_recipe "aws-parallelcluster::ganglia_install"
 
-# FIXME:
-# # Install NVIDIA and CUDA
-# include_recipe "aws-parallelcluster::_nvidia_install"
+# Install NVIDIA and CUDA
+include_recipe "aws-parallelcluster::_nvidia_install"
 
-# FIXME:
-# # Install FSx options
-# include_recipe "aws-parallelcluster::_lustre_install"
+# Install FSx options
+include_recipe "aws-parallelcluster::_lustre_install"
 
-# FIXME:
-# # Install EFA & Intel MPI
-# include_recipe "aws-parallelcluster::efa_install"
-# include_recipe "aws-parallelcluster::intel_mpi"
+# Install EFA & Intel MPI
+include_recipe "aws-parallelcluster::efa_install"
+include_recipe "aws-parallelcluster::intel_mpi"
 
-# FIXME:
-# # Install the AWS cloudwatch agent
-# include_recipe "aws-parallelcluster::cloudwatch_agent_install"
+# Install the AWS cloudwatch agent
+include_recipe "aws-parallelcluster::cloudwatch_agent_install"
 
-# FIXME:
-# # Install Amazon Time Sync
-# include_recipe "aws-parallelcluster::chrony_install"
+# Install Amazon Time Sync
+include_recipe "aws-parallelcluster::chrony_install"

commit ef1e783c8d2b4efb5b5c66fac36a5f521d42d40c

CentOS 8: Fix bug in parallelcluster-ebsnvme-id
* Fix bug introduced in 2c0de640eda12248c606bdf8428923ef866c6c5a; centos8 specific parallelcluster-ebsnvme-id does not accept option and was unable to print out volume device info when called from attachVolume.py, causing mounting of multiple EBS to fail
* parallelcluster-ebsnvme-id needs to accept the options -v/-b/-u to output volume id and block device information when called from ec2_dev_2_volid.py and attachVolume.py
* Modify centos8 specific parallelcluster-ebsnvme-id to output correct info based on option specified
* Centos8 specific ec2_dev_2_volid.py no longer needed and removed, as new parallelcluster-ebsnvme-id script will accept -v option to output volume id

Signed-off-by: Rex <shuningc@amazon.com>

files/centos-8/ec2_dev_2_volid.py
@@ -1,71 +1,0 @@
-# Same as default/ec2_dev_2_volid.py except call to /usr/local/sbin/parallelcluster-ebsnvme-id
-import requests
-import sys
-import os
-import syslog
-import time
-import boto3
-import configparser
-from botocore.config import Config
-
-
-def main():
-    syslog.syslog("Starting ec2_dev_2_volid.py script")
-    # Get dev
-    try:
-        dev = str(sys.argv[1])
-        syslog.syslog("Input block device is %s" % dev)
-    except IndexError:
-        syslog.syslog(syslog.LOG_ERR, "Provide block device i.e. xvdf")
-
-    # Convert dev to mapping format
-    if 'nvme' in dev:
-        # For newer instances which expose EBS volumes as NVMe devices, translate the
-        # device name so boto can discover it.
-        output = os.popen('sudo /usr/local/sbin/parallelcluster-ebsnvme-id /dev/' + dev).read().split(":")[1].strip()
-        print(output)
-        sys.exit(0)
-    else:
-        dev = dev.replace('xvd', 'sd')
-        dev = '/dev/' + dev
-
-    # Get instance ID
-    instanceId = requests.get("http://169.254.169.254/latest/meta-data/instance-id").text
-
-    # Get region
-    region = requests.get("http://169.254.169.254/latest/meta-data/placement/availability-zone").text
-    region = region[:-1]
-
-    # Parse configuration file to read proxy settings
-    config = configparser.RawConfigParser()
-    config.read('/etc/boto.cfg')
-    proxy_config = Config()
-    if config.has_option('Boto', 'proxy') and config.has_option('Boto', 'proxy_port'):
-        proxy = config.get('Boto', 'proxy')
-        proxy_port = config.get('Boto', 'proxy_port')
-        proxy_config = Config(proxies={'https': "{0}:{1}".format(proxy, proxy_port)})
-
-    # Connect to AWS using boto
-    ec2 = boto3.client('ec2', region_name=region, config=proxy_config)
-
-    # Poll for blockdevicemapping
-    devices = ec2.describe_instance_attribute(InstanceId=instanceId, Attribute='blockDeviceMapping').get('BlockDeviceMappings')
-    devmap = dict((d.get('DeviceName'), d) for d in devices)
-    x = 0
-    while dev not in devmap:
-        if x == 36:
-            syslog.syslog("Dev %s did not appears in 180 seconds." % dev)
-            sys.exit(1)
-        syslog.syslog("Looking for dev %s in devmap %s" % (dev, devmap))
-        time.sleep(5)
-        devices = ec2.describe_instance_attribute(InstanceId=instanceId, Attribute='blockDeviceMapping').get('BlockDeviceMappings')
-        devmap = dict((d.get('DeviceName'), d) for d in devices)
-        x += 1
-
-    # Return volumeId
-    volumeId = devmap.get(dev).get('Ebs').get('VolumeId')
-    print(volumeId)
-
-
-if __name__ == '__main__':
-    main()

files/centos-8/parallelcluster-ebsnvme-id
@@ -16,9 +16,75 @@
 # License.
 
 # Usage:
-# Read EBS device information and provide information about the volume.
+# Read EBS device information using nvme-cli and provide information about the volume.
+display_help() {
+    echo "Usage: $0 [options] {device_name}" >&2
+    echo
+    echo "   -v, --volume                   Return volume-id"
+    echo "   -b, --block-dev                Return block device mapping"
+    echo "   -u, --udev                     Output data in format suitable for udev rules, i.e. /dev/sdb -> sdb"
+    echo "   -h, --help                     Print usage info"
+    echo
+}
 
-# Sample output: vol067f083a4f6xxxxx
-# Insert '-' after 'vol' so that output looks like vol-067f083a4f6xxxxx
-vol_id=$(sudo nvme id-ctrl -v ${1} | grep -oP "sn\s+:\s\K(.+)" | sed 's/^vol/&-/')
-echo "Volume ID: ${vol_id}"
+print_all=1
+print_volume=0
+print_block_device_mapping=0
+print_udev_format=0
+
+# Parse arguments
+for i in "$@"
+do
+    case $i in
+        -h|--help)
+        display_help
+        exit 0
+        ;;
+        -v|--volume)
+        print_volume=1
+        print_all=0
+        shift
+        ;;
+        -b|--block-dev)
+        print_block_device_mapping=1
+        print_all=0
+        shift
+        ;;
+        -u|--udev)
+        print_udev_format=1
+        print_all=0
+        shift
+        ;;
+        *)
+        ;;
+    esac
+done
+
+# Check if device argument is provided
+if [[ "$#" -ne 1 ]]; then
+    display_help
+    exit 1
+fi
+
+if [[ $print_all -eq 1 || $print_volume -eq 1 ]]; then
+    # Sample volume info from nvme-cli:
+    # sn      : vol01234567890abcdef
+    # See https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/nvme-ebs-volumes.html#identify-nvme-ebs-device
+    # Insert '-' after 'vol' so that output looks like vol-067f083a4f6xxxxx
+    vol_id=$(sudo nvme id-ctrl -v ${1} | grep -oP "sn\s+:\s\K(.+)" | sed 's/^vol/&-/')
+    echo "Volume ID: ${vol_id}"
+fi
+
+if [[ $print_all -eq 1 || $print_block_device_mapping -eq 1 || $print_udev_format -eq 1 ]]; then
+    # Sample device name info from nvme-cli:
+    # 0000: 2f 64 65 76 2f 73 64 6a 20 20 20 20 20 20 20 20 "/dev/sdf..."
+    # See https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/nvme-ebs-volumes.html#identify-nvme-ebs-device
+    if [[ $print_udev_format -eq 1 ]]; then
+        # Strip /dev/ prefix if -u option is specified
+        device_name=$(sudo nvme id-ctrl -v ${1} | grep -oP '^0000:.+"\K([\/\w]+)(?=\.*"$)' | sed "s/^\/dev\///")
+    else
+        device_name=$(sudo nvme id-ctrl -v ${1} | grep -oP '^0000:.+"\K([\/\w]+)(?=\.*"$)')
+    fi
+
+    echo "${device_name}"
+fi

files/default/attachVolume.py
@@ -11,7 +11,7 @@ from botocore.config import Config
 def convert_dev(dev):
     # Translate the device name as provided by the OS to the one used by EC2
     # FIXME This approach could be broken in some OS variants, see
-    # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/device_naming.html
+    # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/nvme-ebs-volumes.html#identify-nvme-ebs-device
     if '/nvme' in dev:
         return '/dev/' + os.popen('sudo /usr/local/sbin/parallelcluster-ebsnvme-id -u -b ' + dev).read().strip()
     elif '/hd' in dev: