2b2ab7088fad468305562f31b390f6c6e02c1f27
Enrico Usai <usai@amazon.com>
2020-10-30 11:10:49 +0100

    Deprecate CentOS6
    Removed:
    * packer files, scripts used to build the base ami and from `build_ami.sh` script
    * `platform_supports_impi`, `platform_supports_lustre_on_x86_64` and `platform_supports_pmix` helpers
    * specific default attributes
    
    Removed from:
    * conditions
    * comments
    * kitchen tests
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- recipes/dcv_config.rb
+++ recipes/dcv_config.rb
@@ -20,10 +20,10 @@
 
 # Configure the system to enable NICE DCV to have direct access to the Linux server's GPU and enable GPU sharing.
 def allow_gpu_acceleration
-  # On CentOS >=7 fix circular dependency multi-user.target -> cloud-init-> isolate multi-user.target.
+  # On CentOS fix circular dependency multi-user.target -> cloud-init-> isolate multi-user.target.
   # multi-user.target doesn't start until cloud-init run is finished. So isolate multi-user.target
   # is stuck into starting, which keep hanging chef until the 3600s timeout.
-  unless node['platform'] == 'centos' && node['platform_version'].to_i >= 7
+  unless node['platform'] == 'centos'
     # Turn off X
     execute "Turn off X" do
       command "systemctl isolate multi-user.target"
