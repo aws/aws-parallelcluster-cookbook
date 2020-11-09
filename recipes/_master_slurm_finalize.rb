9deb0e83c1d980f9d297b6d5ce7f870ba9bc200e
Francesco De Martino <fdm@amazon.com>
2020-11-09 15:50:59 +0100

    Move fleet status initialization from finalize to config recipe
    This is needed to have the status initialized before daemons are started by supervisord in finalize recipe
    
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- recipes/_master_slurm_finalize.rb
+++ recipes/_master_slurm_finalize.rb
@@ -15,14 +15,6 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-execute 'initialize compute fleet status' do
-  # Initialize the status of the compute fleet in the DynamoDB table. Set it to RUNNING.
-  command "#{node['cfncluster']['cookbook_virtualenv_path']}/bin/aws dynamodb put-item --table-name #{node['cfncluster']['cfn_ddb_table']}"\
-          " --item '{\"Id\": {\"S\": \"COMPUTE_FLEET\"}, \"Status\": {\"S\": \"RUNNING\"}}' --region #{node['cfncluster']['cfn_region']}"
-  retries 3
-  retry_delay 5
-end
-
 ruby_block "submit dynamic fleet initialization jobs" do
   block do
     require 'json'
