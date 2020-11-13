98ec59157c1bf5ab9124b3e2dab936de336d4125
Luca Carrogu <carrogu@amazon.com>
2020-11-13 15:19:48 +0100

    Improve Lustre client installation for CentOS
    Add new records to handle mapping between the Lustre repository and the CentOS kernel version, as described in https://docs.aws.amazon.com/fsx/latest/LustreGuide/install-lustre-client.html
    
    New AMI for CentOS 7.9 was released on November 10 2020 (e.g. ami-00e87074e52e6c9f9 in us-east-1)
    New AMI for CentOS 8.3 will be released soon
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -335,8 +335,8 @@ default['cfncluster']['lustre']['public_key'] = value_for_platform(
 default['cfncluster']['lustre']['base_url'] = value_for_platform(
   'centos' => {
     # node['kernel']['machine'] contains the architecture: 'x86_64' or 'aarch64'
-    '>=8' => "https://fsx-lustre-client-repo.s3.amazonaws.com/el/8/#{node['kernel']['machine']}/",
-    'default' => "https://fsx-lustre-client-repo.s3.amazonaws.com/el/7.#{find_rhel7_kernel_minor_version}/x86_64/"
+    '>=8' => "https://fsx-lustre-client-repo.s3.amazonaws.com/el/8#{find_rhel_minor_version}/#{node['kernel']['machine']}/",
+    'default' => "https://fsx-lustre-client-repo.s3.amazonaws.com/el/7#{find_rhel_minor_version}/x86_64/"
   },
   'ubuntu' => { 'default' => "https://fsx-lustre-client-repo.s3.amazonaws.com/ubuntu" }
 )
