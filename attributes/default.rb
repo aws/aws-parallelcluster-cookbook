a561e62b6bcfe52199c6eeeca923f5956b609c39
ddeidda <ddeidda@amazon.com>
2020-12-10 21:16:13 +0100

    Change source package for gridengine on Ubuntu 1804
    Eoan (19.10) repo is no longer available. With this commit the package will be downloaded from Focal Fossa (20.04).
    
    Signed-off-by: ddeidda <ddeidda@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -302,7 +302,7 @@ when 'debian'
   if node['platform_version'] == '18.04'
     default['cfncluster']['base_packages'].delete('libatlas-dev')
     default['cfncluster']['base_packages'].push('libatlas-base-dev', 'libssl-dev', 'libglvnd-dev')
-    default['cfncluster']['sge']['version'] = '8.1.9+dfsg-9build1'
+    default['cfncluster']['sge']['version'] = '8.1.9+dfsg-9build2'
   end
 
   # Modulefile Directory
