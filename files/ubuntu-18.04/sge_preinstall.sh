817b733df9641ebdcf78bd171104774c0d25ad06
ddeidda <ddeidda@amazon.com>
2020-12-10 22:11:47 +0100

    Change source package for gridengine on Ubuntu 1804
    Eoan (19.10) repo is no longer available. With this commit the package will be downloaded from Focal Fossa (20.04).
    
    Signed-off-by: ddeidda <ddeidda@amazon.com>

--- files/ubuntu-18.04/sge_preinstall.sh
+++ files/ubuntu-18.04/sge_preinstall.sh
@@ -4,16 +4,16 @@ echo "Downloading and extracting source packages for $TARBALL_ROOT_DIR"
 
 pkg_version="$VERSION"
 
-#adds eon source packages
+#adds focal source packages
 src_bionic=`sed -n '/#\s*deb-src .* bionic universe/p' /etc/apt/sources.list`
 
 # FIXME: if China region use US repository
 if [ "${REGION}" != "${REGION#cn-*}" ]; then
-  src_eoan="deb-src http://us-east-1.ec2.archive.ubuntu.com/ubuntu/ eoan universe"
+  src_focal="deb-src http://us-east-1.ec2.archive.ubuntu.com/ubuntu/ focal universe"
 else
-  src_eoan=`echo "${src_bionic}" | sed -e 's/#//' -e 's/bionic/eoan/'`
+  src_focal=`echo "${src_bionic}" | sed -e 's/#//' -e 's/bionic/focal/'`
 fi
-echo $src_eoan >> /etc/apt/sources.list
+echo $src_focal >> /etc/apt/sources.list
 apt update
 
 mkdir /tmp/gridengine && cd /tmp/gridengine
@@ -22,7 +22,7 @@ root_dir=`ls -d */`
 mv "$root_dir" $TARBALL_ROOT_DIR
 tar cvfz $TARBALL_PATH $TARBALL_ROOT_DIR
 
-#removes eon source packages
+#removes focal source packages
 sed -i '$d' /etc/apt/sources.list
 apt update
 echo "Artifact $TARBALL_ROOT_DIR.tar.gz correctly created"
