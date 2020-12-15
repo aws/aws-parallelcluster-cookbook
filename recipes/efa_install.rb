db6764341641f681ada64d15d13c750067ee2cd7
Rex <shuningc@amazon.com>
2020-12-15 11:16:04 -0800

    Install hwloc dev package explicitly after EFA install
    Signed-off-by: Rex <shuningc@amazon.com>

--- recipes/efa_install.rb
+++ recipes/efa_install.rb
@@ -57,3 +57,15 @@ bash "install efa" do
   EFAINSTALL
   not_if { ::Dir.exist?('/opt/amazon/efa') && !efa_gdr_enabled?}
 end
+
+# EFA installer v1.11.0 removes libibverbs-core, which contains hwloc-devel during install
+# hwloc-devel is needed to compile SGE
+# Reinstall hwloc-devel by itself explicitly
+package "install hwloc-devel explicitly" do
+  package_name value_for_platform(
+    'default' => "hwloc-devel",
+    'ubuntu' => { 'default' => "libhwloc-dev" }
+  )
+  retries 3
+  retry_delay 5
+end
