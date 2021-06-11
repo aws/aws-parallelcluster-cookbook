243f70f16b571ae8fde7801a98cd546649491f17
Rex <shuningc@amazon.com>
2021-06-11 15:12:36 -0700

    Install EFA before Lustre
    * Install EFA before Lustre as a workaround for a compatibility issue between efa_installer v1.12.1 and lustre-client-modules for ubuntu2004
    
    Signed-off-by: Rex <shuningc@amazon.com>

--- recipes/base_install.rb
+++ recipes/base_install.rb
@@ -197,12 +197,12 @@ end
 # Install NVIDIA and CUDA
 include_recipe "aws-parallelcluster::nvidia_install"
 
-# Install FSx options
-include_recipe "aws-parallelcluster::lustre_install"
-
 # Install EFA & Intel MPI
 include_recipe "aws-parallelcluster::efa_install"
 include_recipe "aws-parallelcluster::intel_mpi"
+
+# Install FSx options
+include_recipe "aws-parallelcluster::lustre_install"
 
 # Install the AWS cloudwatch agent
 include_recipe "aws-parallelcluster::cloudwatch_agent_install"
