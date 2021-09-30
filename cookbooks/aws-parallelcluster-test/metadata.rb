071ccc9e3ad1fe81fbab21b56a739dc063641c47
Charlie Gruenwald <cgruenwa@amazon.com>
2021-09-30 14:04:21 -0600

    [sub-cookbooks] Move test recipes to separate cookbook.

---
+++ cookbooks/aws-parallelcluster-test/metadata.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+name 'aws-parallelcluster-test'
+maintainer 'Amazon Web Services'
+maintainer_email ''
+license 'Apache-2.0'
+description 'Tests AWS ParallelCluster'
+long_description 'Tests AWS ParallelCluster'
+issues_url 'https://github.com/aws/aws-parallelcluster-cookbook/issues'
+source_url 'https://github.com/aws/aws-parallelcluster-cookbook'
+chef_version '17.2.29'
+version '3.0.0'
+
+supports 'amazon', '>= 2'
+supports 'centos', '>= 7'
+supports 'ubuntu', '>= 18.04'
