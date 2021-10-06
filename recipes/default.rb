c63886eed254f949d0921cce5ce1fe9ccccc5fb0
chenwany <chenwany@amazon.com>
2021-10-06 16:47:59 -0700

    Test certificates
    Signed-off-by: chenwany <chenwany@amazon.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,5 +15,4 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-#include_recipe 'aws-parallelcluster-install::install'
-include_recipe "aws-parallelcluster-install::certificates"
+include_recipe 'aws-parallelcluster-install::install'
