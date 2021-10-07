fe463985f9967cd2a96a29339bac21ac0395e561
Francesco De Martino <fdm@amazon.com>
2021-10-07 12:31:07 +0200

    Minor fixes to execute_event_handler resource
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- resources/execute_event_handler.rb
+++ resources/execute_event_handler.rb
@@ -50,17 +50,17 @@ action_class do # rubocop:disable Metrics/BlockLength
     source_cluster_config = node.dig(:cluster, :cluster_config_path)
     raise "Expected cluster configuration file not found in (#{source_cluster_config})" unless ::File.exist?(source_cluster_config)
 
-    target_cluster_config = "#{node['cluster']['byos']['handler_dir']}cluster-config.yaml"
+    target_cluster_config = "#{node['cluster']['byos']['handler_dir']}/cluster-config.yaml"
     FileUtils.cp(source_cluster_config, target_cluster_config)
 
-    source_launch_templates = "#{node['cluster']['byos']['shared_dir']}launch_templates.json"
+    source_launch_templates = "#{node['cluster']['shared_dir']}/launch_templates_config.json"
     raise "Expected launch templates file not found in (#{source_launch_templates})" unless ::File.exist?(source_launch_templates)
 
-    target_launch_templates = "#{node['cluster']['byos']['handler_dir']}launch_templates.json"
+    target_launch_templates = "#{node['cluster']['byos']['handler_dir']}/launch_templates_config.json"
     FileUtils.cp(source_launch_templates, target_launch_templates) if ::File.exist?(source_launch_templates)
 
-    source_byos_substack_outputs = "#{node['cluster']['byos']['shared_dir']}byos_substack_outputs.json"
-    target_byos_substack_outputs = "#{node['cluster']['byos']['handler_dir']}byos_substack_outputs.json"
+    source_byos_substack_outputs = "#{node['cluster']['shared_dir']}/byos_substack_outputs.json"
+    target_byos_substack_outputs = "#{node['cluster']['byos']['handler_dir']}/byos_substack_outputs.json"
     byos_substack_arn = node.dig(:cluster, :byos_substack_arn)
     if byos_substack_arn && !byos_substack_arn.empty?
       Chef::Log.info("Found byos substack (#{byos_substack_arn})")
@@ -83,7 +83,7 @@ action_class do # rubocop:disable Metrics/BlockLength
       FileUtils.cp(source_byos_substack_outputs, target_byos_substack_outputs)
     end
 
-    source_handler_env = "#{node['cluster']['byos']['shared_dir']}handler_env.json"
+    source_handler_env = "#{node['cluster']['shared_dir']}/handler_env.json"
     if ::File.exist?(source_handler_env)
       Chef::Log.info("Found handler environment file (#{source_handler_env})")
       env = JSON.load_file(source_handler_env)
@@ -105,15 +105,15 @@ action_class do # rubocop:disable Metrics/BlockLength
     env = {}
 
     # PCLUSTER_EC2_INSTANCE_TYPE
-    env.merge!(build_hash_from_node('PCLUSTER_EC2_INSTANCE_TYPE', :ec2, :instance_type))
+    env.merge!(build_hash_from_node('PCLUSTER_EC2_INSTANCE_TYPE', true, :ec2, :instance_type))
 
     case node['cluster']['node_type']
     when 'ComputeFleet'
       # PCLUSTER_QUEUE_NAME
-      env.merge!(build_hash_from_node('PCLUSTER_QUEUE_NAME', :cluster, :scheduler_queue_name))
+      env.merge!(build_hash_from_node('PCLUSTER_QUEUE_NAME', true, :cluster, :scheduler_queue_name))
 
       # PCLUSTER_COMPUTE_RESOURCE_NAME
-      env.merge!(build_hash_from_node('PCLUSTER_COMPUTE_RESOURCE_NAME', :cluster, :scheduler_compute_resource_name))
+      env.merge!(build_hash_from_node('PCLUSTER_COMPUTE_RESOURCE_NAME', true, :cluster, :scheduler_compute_resource_name))
     end
 
     env
@@ -130,40 +130,40 @@ action_class do # rubocop:disable Metrics/BlockLength
     env.merge!({ 'PCLUSTER_LAUNCH_TEMPLATES' => target_launch_templates })
 
     # PCLUSTER_CLUSTER_NAME
-    env.merge!(build_hash_from_node('PCLUSTER_CLUSTER_NAME', :cluster, :stack_name))
+    env.merge!(build_hash_from_node('PCLUSTER_CLUSTER_NAME', true, :cluster, :stack_name))
 
     # PCLUSTER_CFN_STACK_ARN
-    env.merge!(build_hash_from_node('PCLUSTER_CFN_STACK_ARN', :cluster, :stack_arn))
+    env.merge!(build_hash_from_node('PCLUSTER_CFN_STACK_ARN', true, :cluster, :stack_arn))
 
     # PCLUSTER_BYOS_CFN_SUBSTACK_ARN
-    env.merge!(build_hash_from_node('PCLUSTER_BYOS_CFN_SUBSTACK_ARN', :cluster, :byos_substack_arn))
+    env.merge!(build_hash_from_node('PCLUSTER_BYOS_CFN_SUBSTACK_ARN', false, :cluster, :byos_substack_arn))
 
     # PCLUSTER_BYOS_CFN_SUBSTACK_OUTPUTS
     env.merge!({ 'PCLUSTER_BYOS_CFN_SUBSTACK_OUTPUTS' => target_byos_substack_outputs }) if ::File.exist?(target_byos_substack_outputs)
 
     # PCLUSTER_SHARED_SCHEDULER_DIR
-    env.merge!(build_hash_from_node('PCLUSTER_SHARED_SCHEDULER_DIR', :byos, :shared_dir))
+    env.merge!(build_hash_from_node('PCLUSTER_SHARED_SCHEDULER_DIR', true, :cluster, :byos, :shared_dir))
 
     # PCLUSTER_LOCAL_SCHEDULER_DIR
-    env.merge!(build_hash_from_node('PCLUSTER_LOCAL_SCHEDULER_DIR', :byos, :local_dir))
+    env.merge!(build_hash_from_node('PCLUSTER_LOCAL_SCHEDULER_DIR', true, :cluster, :byos, :local_dir))
 
     # PCLUSTER_AWS_REGION
-    env.merge!(build_hash_from_node('PCLUSTER_AWS_REGION', :ec2, :region))
+    env.merge!(build_hash_from_node('PCLUSTER_AWS_REGION', true, :ec2, :region))
 
     # PCLUSTER_OS
-    env.merge!(build_hash_from_node('PCLUSTER_OS', :cluster, :config, :Image, :Os))
+    env.merge!(build_hash_from_node('PCLUSTER_OS', true, :cluster, :config, :Image, :Os))
 
     # PCLUSTER_ARCH
-    env.merge!(build_hash_from_node('PCLUSTER_ARCH', :cpu, :architecture))
+    env.merge!(build_hash_from_node('PCLUSTER_ARCH', true, :cpu, :architecture))
 
     # PCLUSTER_VERSION
-    env.merge!(build_hash_from_node('PCLUSTER_VERSION', %i[cluster parallelcluster-version]))
+    env.merge!(build_hash_from_node('PCLUSTER_VERSION', true, :cluster, :'parallelcluster-version'))
 
     # PCLUSTER_HEADNODE_PRIVATE_IP
-    env.merge!(build_hash_from_node('PCLUSTER_HEADNODE_PRIVATE_IP', :ec2, :local_ipv4))
+    env.merge!(build_hash_from_node('PCLUSTER_HEADNODE_PRIVATE_IP', true, :ec2, :local_ipv4))
 
     # PCLUSTER_HEADNODE_HOSTNAME
-    env.merge!(build_hash_from_node('PCLUSTER_HEADNODE_HOSTNAME', :ec2, :hostname))
+    env.merge!(build_hash_from_node('PCLUSTER_HEADNODE_HOSTNAME', true, :hostname))
 
     # PCLUSTER_CLUSTER_CONFIG_OLD
     # TODO: to be implemented
@@ -171,8 +171,9 @@ action_class do # rubocop:disable Metrics/BlockLength
     env
   end
 
-  def build_hash_from_node(name, *path_in_node)
+  def build_hash_from_node(name, raise_if_not_found, *path_in_node)
     var = node.dig(*path_in_node)
+    raise "Unable to find node attribute #{path_in_node}" if (!var || var.empty?) && raise_if_not_found
     var && !var.empty? ? { name => var } : {}
   end
 
