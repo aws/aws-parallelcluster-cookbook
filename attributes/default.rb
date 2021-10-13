b43b4cd23a20ce69805cfb55f4f794385a9f8090
Yulei Wang <yuleiwan@amazon.com>
2021-10-12 18:33:31 -0700

    Bump version to 3.0.1
    Signed-off-by: Yulei Wang <yuleiwan@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -101,9 +101,9 @@ default['cluster']['armpl']['url'] = [
 ].join('/')
 
 # Python packages
-default['cluster']['parallelcluster-version'] = '3.0.0'
-default['cluster']['parallelcluster-cookbook-version'] = '3.0.0'
-default['cluster']['parallelcluster-node-version'] = '3.0.0'
+default['cluster']['parallelcluster-version'] = '3.0.1'
+default['cluster']['parallelcluster-cookbook-version'] = '3.0.1'
+default['cluster']['parallelcluster-node-version'] = '3.0.1'
 default['cluster']['parallelcluster-awsbatch-cli-version'] = '1.0.0'
 
 # URLs to software packages used during install recipes
--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,7 @@ long_description 'Installs/Configures AWS ParallelCluster'
 issues_url 'https://github.com/aws/aws-parallelcluster-cookbook/issues'
 source_url 'https://github.com/aws/aws-parallelcluster-cookbook'
 chef_version '17.2.29'
-version '3.0.0'
+version '3.0.1'
 
 supports 'amazon', '>= 2'
 supports 'centos', '>= 7'
