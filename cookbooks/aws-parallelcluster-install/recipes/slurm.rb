c8066c2853c63e4576af28a63d23cb2c793eb329
Francesco De Martino <fdm@amazon.com>
2021-10-13 18:45:26 +0200

    Remove node type conditions from Slurm install recipe
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- cookbooks/aws-parallelcluster-install/recipes/slurm.rb
+++ cookbooks/aws-parallelcluster-install/recipes/slurm.rb
@@ -42,91 +42,79 @@ user node['cluster']['slurm']['user'] do
   shell '/bin/bash'
 end
 
-case node['cluster']['node_type']
-when 'HeadNode', nil
-  slurm_tarball = "#{node['cluster']['sources_dir']}/slurm-#{node['cluster']['slurm']['version']}.tar.gz"
+slurm_tarball = "#{node['cluster']['sources_dir']}/slurm-#{node['cluster']['slurm']['version']}.tar.gz"
 
-  # Get slurm tarball
-  remote_file slurm_tarball do
-    source node['cluster']['slurm']['url']
-    mode '0644'
+# Get slurm tarball
+remote_file slurm_tarball do
+  source node['cluster']['slurm']['url']
+  mode '0644'
+  retries 3
+  retry_delay 5
+  not_if { ::File.exist?(slurm_tarball) }
+end
+
+# Validate the authenticity of the downloaded archive based on the checksum published by SchedMD
+ruby_block "Validate Slurm Tarball Checksum" do
+  block do
+    require 'digest'
+    checksum = Digest::SHA1.file(slurm_tarball).hexdigest # nosemgrep
+    raise "Downloaded Tarball Checksum #{checksum} does not match expected checksum #{node['cluster']['slurm']['sha1']}" if checksum != node['cluster']['slurm']['sha1']
+  end
+end
+
+# Install Slurm
+bash 'make install' do
+  user 'root'
+  group 'root'
+  cwd Chef::Config[:file_cache_path]
+  code <<-SLURM
+    set -e
+
+    # python3 is required to build slurm >= 20.02
+    source #{node['cluster']['cookbook_virtualenv_path']}/bin/activate
+
+    tar xf #{slurm_tarball}
+    cd slurm-slurm-#{node['cluster']['slurm']['version']}
+    ./configure --prefix=/opt/slurm --with-pmix=/opt/pmix
+    CORES=$(grep processor /proc/cpuinfo | wc -l)
+    make -j $CORES
+    make install
+    make install-contrib
+    deactivate
+  SLURM
+  # TODO: Fix, so it works for upgrade
+  creates '/opt/slurm/bin/srun'
+end
+
+# Copy required licensing files
+directory "#{node['cluster']['license_dir']}/slurm"
+
+bash 'copy license stuff' do
+  user 'root'
+  group 'root'
+  cwd Chef::Config[:file_cache_path]
+  code <<-SLURMLICENSE
+    set -e
+    cd slurm-slurm-#{node['cluster']['slurm']['version']}
+    cp -v COPYING #{node['cluster']['license_dir']}/slurm/COPYING
+    cp -v DISCLAIMER #{node['cluster']['license_dir']}/slurm/DISCLAIMER
+    cp -v LICENSE.OpenSSL #{node['cluster']['license_dir']}/slurm/LICENSE.OpenSSL
+    cp -v README.rst #{node['cluster']['license_dir']}/slurm/README.rst
+  SLURMLICENSE
+  # TODO: Fix, so it works for upgrade
+  creates "#{node['cluster']['license_dir']}/slurm/README.rst"
+end
+
+# Install PerlSwitch
+case node['platform']
+when 'ubuntu'
+  package 'libswitch-perl' do
     retries 3
     retry_delay 5
-    not_if { ::File.exist?(slurm_tarball) }
   end
-
-  # Validate the authenticity of the downloaded archive based on the checksum published by SchedMD
-  ruby_block "Validate Slurm Tarball Checksum" do
-    block do
-      require 'digest'
-      checksum = Digest::SHA1.file(slurm_tarball).hexdigest # nosemgrep
-      raise "Downloaded Tarball Checksum #{checksum} does not match expected checksum #{node['cluster']['slurm']['sha1']}" if checksum != node['cluster']['slurm']['sha1']
-    end
-  end
-
-  # Install Slurm
-  bash 'make install' do
-    user 'root'
-    group 'root'
-    cwd Chef::Config[:file_cache_path]
-    code <<-SLURM
-      set -e
-
-      # python3 is required to build slurm >= 20.02
-      source #{node['cluster']['cookbook_virtualenv_path']}/bin/activate
-
-      tar xf #{slurm_tarball}
-      cd slurm-slurm-#{node['cluster']['slurm']['version']}
-      ./configure --prefix=/opt/slurm --with-pmix=/opt/pmix
-      CORES=$(grep processor /proc/cpuinfo | wc -l)
-      make -j $CORES
-      make install
-      make install-contrib
-      deactivate
-    SLURM
-    # TODO: Fix, so it works for upgrade
-    creates '/opt/slurm/bin/srun'
-  end
-
-  # Copy required licensing files
-  directory "#{node['cluster']['license_dir']}/slurm"
-
-  bash 'copy license stuff' do
-    user 'root'
-    group 'root'
-    cwd Chef::Config[:file_cache_path]
-    code <<-SLURMLICENSE
-      set -e
-      cd slurm-slurm-#{node['cluster']['slurm']['version']}
-      cp -v COPYING #{node['cluster']['license_dir']}/slurm/COPYING
-      cp -v DISCLAIMER #{node['cluster']['license_dir']}/slurm/DISCLAIMER
-      cp -v LICENSE.OpenSSL #{node['cluster']['license_dir']}/slurm/LICENSE.OpenSSL
-      cp -v README.rst #{node['cluster']['license_dir']}/slurm/README.rst
-    SLURMLICENSE
-    # TODO: Fix, so it works for upgrade
-    creates "#{node['cluster']['license_dir']}/slurm/README.rst"
-  end
-
-  # Install PerlSwitch
-  case node['platform']
-  when 'ubuntu'
-    package 'libswitch-perl' do
-      retries 3
-      retry_delay 5
-    end
-  when 'centos', 'amazon'
-    package 'perl-Switch' do
-      retries 3
-      retry_delay 5
-    end
-  end
-
-when 'ComputeFleet'
-  # Created Slurm shared mount point
-  directory "/opt/slurm" do
-    mode '1777'
-    owner 'root'
-    group 'root'
-    action :create
+when 'centos', 'amazon'
+  package 'perl-Switch' do
+    retries 3
+    retry_delay 5
   end
 end
