82154c95b347de908c77d95802dea768af0eb0d9
Hanwen <hanwenli@amazon.com>
2021-10-28 07:09:30 -0700

    Upgrade EFA to version 1.14.1
    Starting from EFA 1.14.0, GDR support is enabled by default. Therefore, this commit also removes the logic to reinstall EFA if GDR is enabled.
    In kitchen test:
    1. EFA test is migrated from pclster 2.11
    2. the GDR check is moved to only check official built AMIs. This does not reduce the coverage of the test, because the check had `efa_gdr_enabled?` as part of the condition to execute. `enable_efa_gdr?` checks `node['cfncluster']['enable_efa_gdr']` in dna.json. However, in .kitchen.yml `enable_efa_gdr` was never provided. So the check was never executed.
    
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- recipes/efa_config.rb
+++ recipes/efa_config.rb
@@ -15,9 +15,6 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Installation recipe must be re-executed at runtime to enable GDR
-include_recipe "aws-parallelcluster::efa_install"
-
 if node['platform'] == 'ubuntu' && node['cluster']['enable_efa'] == 'compute' && node['cluster']['node_type'] == 'ComputeFleet'
   # Disabling ptrace protection is needed for EFA in order to use SHA transfer for intra-node communication.
   replace_or_add "disable ptrace protection" do
