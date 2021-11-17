30f593015bb26cb003dd70a37994e3a6ad1cc713
Enrico Usai <usai@amazon.com>
2021-11-17 11:25:18 +0100

    Bump version to 2.11.4
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -102,9 +102,9 @@ default['cfncluster']['armpl']['url'] = [
 ].join('/')
 
 # Python packages
-default['cfncluster']['cfncluster-version'] = '2.11.3'
-default['cfncluster']['cfncluster-cookbook-version'] = '2.11.3'
-default['cfncluster']['cfncluster-node-version'] = '2.11.3'
+default['cfncluster']['cfncluster-version'] = '2.11.4'
+default['cfncluster']['cfncluster-cookbook-version'] = '2.11.4'
+default['cfncluster']['cfncluster-node-version'] = '2.11.4'
 
 # URLs to software packages used during install recipes
 # Gridengine software
