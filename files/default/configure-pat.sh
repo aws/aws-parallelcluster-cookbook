a4d3c6b2d6a2ee83ec290617cb30ecad7a0cba22
chenwany <chenwany@amazon.com>
2021-11-17 12:18:27 -0800

    Only add more retry for metadata for ssh_target_checker
    Only add more retry for metadata in ssh_target_checker since it only get throthing with some specified cases with ssh.
    
    Signed-off-by: chenwany <chenwany@amazon.com>

--- files/default/configure-pat.sh
+++ files/default/configure-pat.sh
@@ -13,8 +13,8 @@
 
 set -x
 echo "Determining the MAC address"
-TOKEN=$(curl --retry 20 --retry-delay 1 --fail -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 300")
-MAC=$(curl --retry 20 --retry-delay 1 --silent --fail -H "X-aws-ec2-metadata-token: ${TOKEN}" http://169.254.169.254/latest/meta-data/mac)
+TOKEN=$(curl --retry 3 --retry-delay 0 --fail -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 300")
+MAC=$(curl --retry 3 --retry-delay 0 --silent --fail -H "X-aws-ec2-metadata-token: ${TOKEN}" http://169.254.169.254/latest/meta-data/mac)
 if [ $? -ne 0 ] ; then
    echo "Unable to determine MAC address" | logger -t "ec2"
    exit 1
@@ -25,7 +25,7 @@ echo "Found MAC: ${MAC} on the first network device" | logger -t "ec2"
 VPC_CIDR_URI="http://169.254.169.254/latest/meta-data/network/interfaces/macs/${MAC}/vpc-ipv4-cidr-block"
 echo "Metadata location for vpc ipv4 range: ${VPC_CIDR_URI}" | logger -t "ec2"
 
-VPC_CIDR_RANGE=$(curl --retry 20 --retry-delay 1 --silent --fail -H "X-aws-ec2-metadata-token: ${TOKEN}" ${VPC_CIDR_URI})
+VPC_CIDR_RANGE=$(curl --retry 3 --retry-delay 0 --silent --fail -H "X-aws-ec2-metadata-token: ${TOKEN}" ${VPC_CIDR_URI})
 if [ $? -ne 0 ] ; then
    echo "Unable to retrive VPC CIDR range from meta-data. Using 0.0.0.0/0 instead. PAT may not function correctly" | logger -t "ec2"
    VPC_CIDR_RANGE="0.0.0.0/0"
