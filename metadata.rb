249a9676f85f3d8cd18c1bd245fb2e58f228c2d5
Francesco De Martino <fdm@amazon.com>
2022-01-13 19:52:59 +0100

    Bump version to 3.0.3
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,7 @@ long_description 'Installs/Configures AWS ParallelCluster'
 issues_url 'https://github.com/aws/aws-parallelcluster-cookbook/issues'
 source_url 'https://github.com/aws/aws-parallelcluster-cookbook'
 chef_version '17.2.29'
-version '3.0.2'
+version '3.0.3'
 
 supports 'amazon', '>= 2'
 supports 'centos', '>= 7'
