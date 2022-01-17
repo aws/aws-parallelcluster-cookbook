670a24de5c057386966586d7db35fe9a2fe59c9f
Francesco De Martino <fdm@amazon.com>
2022-01-17 16:24:56 +0100

    Improve imds access configuration
    * Execute tests for user access only at AMI build time
    * Skip all configurations on compute nodes where IMDS access is always enabled
    
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- cookbooks/aws-parallelcluster-test/recipes/test_imds.rb
+++ cookbooks/aws-parallelcluster-test/recipes/test_imds.rb
@@ -42,6 +42,7 @@ allowed_users.each { |allowed_user| check_imds_access(allowed_user, true) }
 
 denied_users.each { |denied_user| check_imds_access(denied_user, false) }
 
-check_run_level_script('parallelcluster-iptables', %w(1 2 3 4 5), %w(0 6))
-
-check_iptables_rules_file('/etc/parallelcluster/sysconfig/iptables.rules')
+if node['cluster']['node_type'] == 'HeadNode' && node['cluster']['scheduler'] != 'awsbatch'
+  check_run_level_script('parallelcluster-iptables', %w(1 2 3 4 5), %w(0 6))
+  check_iptables_rules_file('/etc/parallelcluster/sysconfig/iptables.rules')
+end
