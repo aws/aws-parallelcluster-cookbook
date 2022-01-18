841c3e2d00d46ca1fea9eaac6aeea791177941ef
Luca Carrogu <carrogu@amazon.com>
2022-01-18 15:49:03 +0100

    Install scheduler plugin python virtual into a shared path
    This to allow other users (different from scheduler-plugin) to be able to access the virtualenv and activate it
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -140,10 +140,10 @@ default['cluster']['scheduler_plugin']['local_dir'] = "#{node['cluster']['base_d
 default['cluster']['scheduler_plugin']['handler_dir'] = "#{node['cluster']['scheduler_plugin']['local_dir']}/.configs"
 default['cluster']['scheduler_plugin']['scheduler_plugin_substack_outputs_path'] = "#{node['cluster']['shared_dir']}/scheduler-plugin-substack-outputs.json"
 default['cluster']['scheduler_plugin']['python_version'] = '3.9.9'
+default['cluster']['scheduler_plugin']['pyenv_root'] = "#{node['cluster']['scheduler_plugin']['shared_dir']}/pyenv"
 default['cluster']['scheduler_plugin']['virtualenv'] = 'scheduler_plugin_virtualenv'
 default['cluster']['scheduler_plugin']['virtualenv_path'] = [
-  node['cluster']['scheduler_plugin']['home'],
-  '.pyenv',
+  node['cluster']['scheduler_plugin']['pyenv_root'],
   'versions',
   node['cluster']['scheduler_plugin']['python_version'],
   'envs',
