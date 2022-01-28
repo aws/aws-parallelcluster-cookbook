48c3b38ebe46fe78a47487ae77e37f584edb08bd
Luca Carrogu <carrogu@amazon.com>
2022-01-28 15:07:32 +0100

    Change Slurm compute fleet status update logic
    Compute fleet status update is now triggered by the new clusterstatusmgtd.
    This change enable the recipe to perform the compute fleet status update for the Slurm case, which is going to use the new slurm fleet manager program
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

---
+++ cookbooks/aws-parallelcluster-slurm/recipes/update_computefleet_status_head_node.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+#
+# Cookbook:: aws-parallelcluster-slurm
+# Recipe:: update_computefleet_head_node
+#
+# Copyright:: 2013-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the
+# License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+
+bash 'update compute fleet' do
+  user 'root'
+  code <<-UPDATE_COMPUTE_FLEET
+      set -xe
+      #{node['cluster']['cookbook_virtualenv_path']}/bin/supervisorctl stop clustermgtd
+      #{node['cluster']['scripts_dir']}/slurm/slurm_fleet_status_manager -cf #{node['cluster']['computefleet_status_path']}
+      #{node['cluster']['cookbook_virtualenv_path']}/bin/supervisorctl start clustermgtd
+  UPDATE_COMPUTE_FLEET
+end
