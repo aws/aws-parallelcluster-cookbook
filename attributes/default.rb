863b0c2048cf3cc5ea8838880e13dbed91db5f16
Luca Carrogu <carrogu@amazon.com>
2022-02-03 14:07:33 +0100

    Upgrade NVIDIA drivers, CUDA and Fabric Manager
    NVIDIA drivers from 470.82.01 to 470.103.01
    CUDA from 11.4.3 to 11.4.4
    Fabric Manager from 470.82.01 to 470.103.01
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -125,10 +125,10 @@ default['cfncluster']['ganglia_enabled'] = 'no'
 
 # NVIDIA
 default['cfncluster']['nvidia']['enabled'] = 'no'
-default['cfncluster']['nvidia']['driver_version'] = '470.82.01'
-default['cfncluster']['nvidia']['driver_url'] = 'https://us.download.nvidia.com/tesla/470.82.01/NVIDIA-Linux-x86_64-470.82.01.run'
+default['cfncluster']['nvidia']['driver_version'] = '470.103.01'
+default['cfncluster']['nvidia']['driver_url'] = 'https://us.download.nvidia.com/tesla/470.103.01/NVIDIA-Linux-x86_64-470.103.01.run'
 default['cfncluster']['nvidia']['cuda_version'] = '11.4'
-default['cfncluster']['nvidia']['cuda_url'] = 'https://developer.download.nvidia.com/compute/cuda/11.4.3/local_installers/cuda_11.4.3_470.82.01_linux.run'
+default['cfncluster']['nvidia']['cuda_url'] = 'https://developer.download.nvidia.com/compute/cuda/11.4.4/local_installers/cuda_11.4.4_470.82.01_linux.run'
 
 # The package name of Fabric Manager for alinux2 and centos7 is nvidia-fabric-manager-<version>
 # For ubuntu, it is nvidia-fabricmanager-<major-version>_<version>
