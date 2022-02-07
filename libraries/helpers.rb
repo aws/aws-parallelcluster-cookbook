76e682dab92e17886c69fede21d1adbea4ad99dd
Francesco De Martino <fdm@amazon.com>
2022-02-07 10:18:29 +0100

    Use YAML.safe_load when reading cluster config
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -537,7 +537,7 @@ def load_cluster_config
   ruby_block "load cluster configuration" do
     block do
       require 'yaml'
-      config = YAML.load_file(node['cluster']['cluster_config_path'])
+      config = YAML.safe_load(File.read(node['cluster']['cluster_config_path']))
       Chef::Log.debug("Config read #{config}")
       node.override['cluster']['config'].merge! config
     end
