88dfc627e97f34f827109373c72797cf7b83cc01
Hanwen <hanwenli@amazon.com>
2022-02-24 12:08:36 -0800

    Upgrade Slurm to version 21.08.6
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -116,9 +116,9 @@ default['cluster']['parallelcluster-awsbatch-cli-version'] = '1.0.0'
 # URLs to software packages used during install recipes
 # Slurm software
 default['cluster']['slurm_plugin_dir'] = '/etc/parallelcluster/slurm_plugin'
-default['cluster']['slurm']['version'] = '21-08-5-1'
+default['cluster']['slurm']['version'] = '21-08-6-1'
 default['cluster']['slurm']['url'] = "https://github.com/SchedMD/slurm/archive/slurm-#{node['cluster']['slurm']['version']}.tar.gz"
-default['cluster']['slurm']['sha1'] = '1416539a06c866605b8d464daf6c98d881592361'
+default['cluster']['slurm']['sha1'] = 'f2672e03dd4fe63cc682df2333182c5fb3033279'
 default['cluster']['slurm']['user'] = 'slurm'
 default['cluster']['slurm']['user_id'] = node['cluster']['reserved_base_uid'] + 1
 default['cluster']['slurm']['group'] = node['cluster']['slurm']['user']
