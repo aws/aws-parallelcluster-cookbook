8213a2061d78bae5ddf80e33e9fb4ebdd0a3c5f8
Giacomo Marciani <mgiacomo@amazon.com>
2022-03-16 16:54:04 +0100

    Add kitchen test to verify Nvidia drivers and DCV GL
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

--- cookbooks/aws-parallelcluster-test/recipes/tests.rb
+++ cookbooks/aws-parallelcluster-test/recipes/tests.rb
@@ -21,6 +21,8 @@ include_recipe 'aws-parallelcluster-test::test_processes'
 include_recipe 'aws-parallelcluster-test::test_imds'
 include_recipe 'aws-parallelcluster-test::test_sudoers'
 include_recipe 'aws-parallelcluster-test::test_openssh'
+include_recipe 'aws-parallelcluster-test::test_nvidia'
+include_recipe 'aws-parallelcluster-test::test_dcv'
 
 ###################
 # AWS Cli
