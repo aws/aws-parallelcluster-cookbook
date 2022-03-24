a9825af2d7717e50b118455ddf889cd7b0033d92
Giacomo Marciani <mgiacomo@amazon.com>
2022-03-24 11:12:25 +0100

    Bump version to 2.11.6
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>
    
    SQUASH CHANGELOG
    
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -96,9 +96,9 @@ default['cfncluster']['armpl']['url'] = [
 ].join('/')
 
 # Python packages
-default['cfncluster']['cfncluster-version'] = '2.11.5'
-default['cfncluster']['cfncluster-cookbook-version'] = '2.11.5'
-default['cfncluster']['cfncluster-node-version'] = '2.11.5'
+default['cfncluster']['cfncluster-version'] = '2.11.6'
+default['cfncluster']['cfncluster-cookbook-version'] = '2.11.6'
+default['cfncluster']['cfncluster-node-version'] = '2.11.6'
 
 # URLs to software packages used during install recipes
 # Slurm software
