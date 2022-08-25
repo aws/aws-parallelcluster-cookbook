15ad40b0d09e6dfac16ac70812616b40e17886c3
Francesco Giordano <giordafr@amazon.it>
2022-08-25 17:23:53 +0200

    Add kitchen tests to check Neuron driver and repository
    Signed-off-by: Francesco Giordano <giordafr@amazon.it>
    Signed-off-by: Enrico Usai <usai@amazon.com>

---
+++ cookbooks/aws-parallelcluster-test/recipes/test_neuron.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+#
+# Cookbook:: aws-parallelcluster-test
+# Recipe:: test_neuron
+#
+# Copyright:: 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the
+# License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+
+ruby_block "Verify Neuron repository is enabled" do
+  block do
+    unless is_repository_enabled?(node['cluster']['neuron']['repository_name'], node['cluster']['neuron']['base_url'])
+      raise "Missing Neuron repository"
+    end
+  end
+end
+
+bash "Check Neuron module" do
+  cwd Chef::Config[:file_cache_path]
+  code <<-TEST
+    lsmod | grep neuron
+    [[ $? == 0 ]] || { echo "ERROR Installed Neuron driver is not working properly: kernel module not loaded"; exit 1; };
+  TEST
+end
+
+if is_neuron_instance?
+  bash "Check Neuron devices" do
+    cwd Chef::Config[:file_cache_path]
+    code <<-TEST
+      ls -l /dev/neuron*
+      [[ $? == 0 ]] || { echo "ERROR Installed Neuron driver is not working properly: device not found"; exit 1; };
+    TEST
+  end
+end
