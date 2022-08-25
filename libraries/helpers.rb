edde79f77573475ce799a598da44222e5fb41929
Francesco Giordano <giordafr@amazon.it>
2022-08-25 17:23:53 +0200

    Add kitchen tests to check Neuron driver and repository
    Signed-off-by: Francesco Giordano <giordafr@amazon.it>

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -399,6 +399,21 @@ def remove_package_repository(repo_name)
   end
 end
 
+# Check if package repository is enabled
+# NOTE: This helper function defines a Chef resource function to be executed at Converge time
+def repository_enabled?(repo_name, base_url)
+  command = value_for_platform(
+  %w(ubuntu debian) => {
+    'default' => "grep -r -E '^deb +#{base_url}' /etc/apt/sources.list*",
+  },
+  'default' => "yum -v repolist #{repo_name} | grep 'Repo-status  : enabled' || exit 1 && yum -v repolist neuron | grep 'Repo-baseurl : #{base_url}'")
+
+  cmd = Mixlib::ShellOut.new(command, timeout: 60)
+  cmd.run_command
+  # Return false if the repository is not installed
+  cmd.exitstatus == 0
+end
+
 # Get number of nv switches
 def get_nvswitches
   # NVSwitch device id is 10de:1af1
@@ -547,6 +562,11 @@ def neuron_installed?
   cmd.run_command
   # Return false if the package is not installed
   !(cmd.exitstatus != 0)
+end
+
+def neuron_instance?
+  neuron_filter = ["trn1."]
+  node['ec2']['instance_type'].start_with?(*neuron_filter)
 end
 
 # load cluster configuration file into node object
