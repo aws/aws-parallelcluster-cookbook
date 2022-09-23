9c648b0104cc6235a8534ab5a9caca23ae73d2ec
chenwany <chenwany@amazon.com>
2022-09-23 10:37:37 -0700

    Only create shared_storage_data.yaml on headnode
    Signed-off-by: chenwany <chenwany@amazon.com>

--- cookbooks/aws-parallelcluster-config/recipes/head_node_base.rb
+++ cookbooks/aws-parallelcluster-config/recipes/head_node_base.rb
@@ -15,6 +15,12 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
+# generate the shared storages mapping file
+template node['cluster']['shared_storages_mapping_path'] do
+  source 'shared_storages/shared_storages_data.erb'
+  mode '0644'
+end
+
 manage_ebs "add ebs" do
   shared_dir_array node['cluster']['ebs_shared_dirs'].split(',')
   vol_array node['cluster']['volume'].split(',')
