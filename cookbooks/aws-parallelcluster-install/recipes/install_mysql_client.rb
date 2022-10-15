73ac4683f34003b7ea5b2a02d642adfebdff18ae
David Pratt <davprat@amazon.com>
2022-10-14 19:47:01 -0700

    Install MySQL packages from S3
    Instead of installing the MySQL community packages from the
    MySQL repositories, we now install packages from an S3
    bucket where we have uploaded the required packages to.
    
    Signed-off-by: David Pratt <davprat@amazon.com>

--- cookbooks/aws-parallelcluster-install/recipes/install_mysql_client.rb
+++ cookbooks/aws-parallelcluster-install/recipes/install_mysql_client.rb
@@ -15,55 +15,46 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-def install_repository_configuration_package(repository_name, definition_source, local_file, md5_hash)
-  remote_file local_file do
-    source definition_source
-    mode '0644'
+if platform?('ubuntu') && arm_instance?
+  package node['cluster']['mysql']['repository']['packages'] do
     retries 3
     retry_delay 5
-    not_if { ::File.exist?(local_file) }
   end
+else
 
-  ruby_block "Validate Repository Definition Checksum" do
-    block do
-      require 'digest'
-      checksum = Digest::MD5.file(local_file).hexdigest
-
-      if checksum != md5_hash
-        raise "Downloaded file #{local_file} checksum #{checksum} does not match expected checksum #{md5_hash}"
-      end
-    end
-  end
-
-  if platform?('ubuntu')
-    # dpkg_package is used here because 'package' seems to default to using apt_package
-    # which fails on the MySQL package.
-    dpkg_package repository_name do
-      source local_file
-    end
-
-    apt_update 'update' do
-      action :update
-      retries 3
-      retry_delay 5
-    end
-  else
-    package repository_name do
-      source local_file
-    end
-  end
-end
-
-unless platform?('ubuntu') && arm_instance?
-  install_repository_configuration_package(
-    "MySQL Repository",
-    node['cluster']['mysql']['repository']['definition']['url'],
-    "/tmp/#{node['cluster']['mysql']['repository']['definition']['file-name']}",
-    node['cluster']['mysql']['repository']['definition']['md5']
+  package_installer = value_for_platform(
+    'default' => "yum install -y",
+    'ubunutu' => { 'default' => "apt install" }
   )
-end
 
-package node['cluster']['mysql']['repository']['packages'] do
-  retries 3
-  retry_delay 5
+  mysql_source_key = "#{node['cluster']['mysql']['package']['prefix']}/#{node['cluster']['mysql']['package']['file']}"
+
+  mysql_extract_directory = "/tmp/mysql"
+  mysql_tar_file = "/tmp/#{node['cluster']['mysql']['package']['file']}"
+
+  # Remove packages that break MySQL installation
+  package node['cluster']['mysql']['remove']['packages'] do
+    action :remove
+  end
+
+  bash 'Install MySQL packages' do
+    user 'root'
+    group 'root'
+    cwd '/tmp'
+    code <<-MYSQL
+      set -e
+      #{node['cluster']['cookbook_virtualenv_path']}/bin/aws s3api get-object \
+                         --bucket "#{node['cluster']['mysql']['package']['bucket']}" \
+                         --key "#{mysql_source_key}" \
+                         --region "#{node['cluster']['region']}" \
+                         "#{mysql_tar_file}"
+      mkdir "#{mysql_extract_directory}"
+      tar xf "#{mysql_tar_file}" --directory "#{mysql_extract_directory}"
+
+      cat "#{mysql_extract_directory}/install.txt" | while read line; do
+        #{package_installer} "#{mysql_extract_directory}/${line}"
+      done
+    MYSQL
+  end
+
 end
