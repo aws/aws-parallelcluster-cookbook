54deba129e5f4f456cc246b118d5d1c954286adb
Delong Meng <delmeng@amazon.com>
2022-11-16 11:50:52 -0800

    rename error file

--- files/default/event_handler/write_chef_error_handler.rb
+++ files/default/event_handler/write_chef_error_handler.rb
@@ -3,7 +3,7 @@ module WriteChefError
   class WriteChefError < Chef::Handler
     def report
       if run_status.failed?
-        Mixlib::ShellOut.new("echo 'Failed when running chef recipes: #{run_status.formatted_exception}' > /var/log/parallelcluster/chef_error_msg").run_command
+        Mixlib::ShellOut.new("echo 'Failed when running chef recipes: #{run_status.formatted_exception}' > /var/log/parallelcluster/headnode_bootstrap_error_msg").run_command
       end
     end
   end
