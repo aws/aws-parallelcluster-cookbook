9dcb861f7b1ef317e78c5c10f87e69dd67a62a19
Delong Meng <delmeng@amazon.com>
2022-11-17 17:35:53 -0800

    add a check to avoid conflict with BYOS error handling

--- files/default/event_handler/write_chef_error_handler.rb
+++ files/default/event_handler/write_chef_error_handler.rb
@@ -3,7 +3,9 @@ module WriteChefError
   class WriteChefError < Chef::Handler
     def report
       if run_status.failed?
-        Mixlib::ShellOut.new("echo 'Failed when running chef recipes: #{run_status.formatted_exception}' > /var/log/parallelcluster/headnode_bootstrap_error_msg").run_command
+        unless File.exist?('/var/log/parallelcluster/headnode_bootstrap_error_msg')
+          Mixlib::ShellOut.new("echo 'Failed when running chef recipes: #{run_status.formatted_exception}' > /var/log/parallelcluster/headnode_bootstrap_error_msg").run_command
+        end
       end
     end
   end
