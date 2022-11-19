0bd3e79171296cccfb1a203d902ef1e4cc4e0c5d
Delong Meng <delmeng@amazon.com>
2022-11-18 16:57:42 -0800

    rename error file

--- files/default/event_handler/write_chef_error_handler.rb
+++ files/default/event_handler/write_chef_error_handler.rb
@@ -3,7 +3,7 @@ module WriteChefError
   class WriteChefError < Chef::Handler
     def report
       if run_status.failed?
-        error_file = '/var/log/parallelcluster/headnode_bootstrap_error_msg'
+        error_file = '/var/log/parallelcluster/bootstrap_error_msg'
         unless File.exist?(error_file)
           message = "Failed when running chef recipes (If --rollback-on-failure was set to false, more details can be found in /var/log/chef-client.log and /var/log/cloud-init-output.log.):"
           Mixlib::ShellOut.new("echo '#{message}' '#{run_status.formatted_exception}' > '#{error_file}'").run_command
