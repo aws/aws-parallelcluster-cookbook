e23472bc8fd7d6165ed3639a192bbc282e87eafa
Jacopo De Amicis <jdamicis@amazon.it>
2022-12-06 15:44:31 +0100

    Upgrade Slurm to version 22.05.6
    Remove workaround sleep between restart of slurmctld and scontrol reconfigure.
    
    Signed-off-by: Jacopo De Amicis <jdamicis@amazon.it>

--- cookbooks/aws-parallelcluster-slurm/recipes/update_head_node.rb
+++ cookbooks/aws-parallelcluster-slurm/recipes/update_head_node.rb
@@ -189,8 +189,6 @@ service 'slurmctld' do
   not_if { ::File.exist?(node['cluster']['previous_cluster_config_path']) && !are_queues_updated? }
 end
 
-chef_sleep '5'
-
 execute 'reload config for running nodes' do
   command "#{node['cluster']['slurm']['install_dir']}/bin/scontrol reconfigure"
   retries 3
