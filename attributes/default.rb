d5414144b7555131eb93b4c50ae2c7d3fb172077
Jacopo De Amicis <35738823+jdeamicis@users.noreply.github.com>
2022-12-20 19:36:00 +0100

    Upgrade Slurm to version 22.05.7 (#1630)
    Signed-off-by: Jacopo De Amicis <jdamicis@amazon.it>

--- attributes/default.rb
+++ attributes/default.rb
@@ -138,9 +138,9 @@ default['cluster']['cfn_bootstrap']['package'] = "aws-cfn-bootstrap-py3-#{node['
 # URLs to software packages used during install recipes
 # Slurm software
 default['cluster']['slurm_plugin_dir'] = '/etc/parallelcluster/slurm_plugin'
-default['cluster']['slurm']['version'] = '22-05-6-1'
+default['cluster']['slurm']['version'] = '22-05-7-1'
 default['cluster']['slurm']['url'] = "https://github.com/SchedMD/slurm/archive/slurm-#{node['cluster']['slurm']['version']}.tar.gz"
-default['cluster']['slurm']['sha1'] = 'bd2fbc4f6fcf41bfce899eaac0d92d9f09996cd3'
+default['cluster']['slurm']['sha1'] = 'a9208e0d6af8465e417d5a60a6ea82b151b6fc34'
 default['cluster']['slurm']['user'] = 'slurm'
 default['cluster']['slurm']['user_id'] = node['cluster']['reserved_base_uid'] + 1
 default['cluster']['slurm']['group'] = node['cluster']['slurm']['user']
