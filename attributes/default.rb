0905c385931bda366692ca5fe3ab0c9942a630f9
Jacopo De Amicis <jdamicis@amazon.it>
2023-01-13 16:14:01 +0100

    Add comments about Slurm commit hash
    Signed-off-by: Jacopo De Amicis <jdamicis@amazon.it>

--- attributes/default.rb
+++ attributes/default.rb
@@ -102,6 +102,8 @@ default['cluster']['armpl']['url'] = [
 # Slurm software
 default['cluster']['slurm_plugin_dir'] = '/etc/parallelcluster/slurm_plugin'
 default['cluster']['slurm']['version'] = '22-05-8-1'
+# WARNING: specify the commit hash only if a development version of Slurm must be used
+# WARNING: the whole commit hash must be used here (not a shortened version of the hash)
 default['cluster']['slurm']['commit'] = ''
 if slurm_commit_hash?
   default['cluster']['slurm']['tar_name'] = "#{node['cluster']['slurm']['commit']}"
