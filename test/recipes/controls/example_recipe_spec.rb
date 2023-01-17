b513985ba50b943ca99036d73e3bf828d697e9d6
Enrico Usai <usai@amazon.com>
2023-01-17 17:55:35 +0100

    Remove useless example_recipes
    We have sudoers.rb as example now.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- test/recipes/controls/example_recipe_spec.rb
+++
@@ -1,9 +0,0 @@
-# Use the name matching the resource type
-control 'example_recipe' do
-  # describe the resource
-  title 'Example recipe'
-
-  describe file('/tmp/example_file') do
-    it { should exist }
-  end
-end
