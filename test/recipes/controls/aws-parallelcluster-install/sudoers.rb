b22f84802e69c20372d514785739085b5cefafbf
Enrico Usai <usai@amazon.com>
2023-01-17 16:15:30 +0100

    Make sudoers.rb recipe compatible with images without sudo package and redhat8
    * Added sudo package, it is required to permit the recipe to work in base images without it.
    * Removed the ubuntu case to make the more generic and support all the OSes with the same code.
    * Added unit test for the changes performed by the recipe.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

---
+++ test/recipes/controls/aws-parallelcluster-install/sudoers.rb
@@ -0,0 +1,24 @@
+# Copyright:: 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file.
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+control 'sudoers' do
+  title 'Install sudo package and prepare sudoers file'
+
+  describe package('sudo') do
+    it { should be_installed }
+  end
+
+  describe file('/etc/sudoers.d/99-parallelcluster-secure-path') do
+    it { should exist }
+    its('mode') { should cmp '0600' }
+    its('content') { should match(%r{Defaults secure_path = /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin}) }
+  end
+end
