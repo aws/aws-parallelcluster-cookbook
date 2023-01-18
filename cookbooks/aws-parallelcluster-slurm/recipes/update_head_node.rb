08918a738559702d85acbf790da4e8ffc1f1ec6c
David Pratt <davprat@amazon.com>
2023-01-18 13:43:37 -0800

    Remove unnecessary node property
    This change simplifies the logic for enabling compute console
    output logging to be based soley on if CloudWatch logging is
    enabled.
    
    Signed-off-by: David Pratt <davprat@amazon.com>

--- cookbooks/aws-parallelcluster-slurm/recipes/update_head_node.rb
+++ cookbooks/aws-parallelcluster-slurm/recipes/update_head_node.rb
@@ -175,9 +175,8 @@ end
 
 replace_or_add "Update compute console logging" do
   path "/etc/parallelcluster/slurm_plugin/parallelcluster_clustermgtd.conf"
-  pattern "compute_console_logging_enabled*"
-  line "compute_console_logging_enabled = <%= node['cluster']['cw_logging_enabled'] == 'true' && node['cluster']['slurm_console_logging']['enabled'] %>"
-  replace_only true
+  pattern "compute_console_logging_enabled.*"
+  line "compute_console_logging_enabled = #{node['cluster']['cw_logging_enabled']}"
 end
 
 ruby_block "Update Slurm Accounting" do
