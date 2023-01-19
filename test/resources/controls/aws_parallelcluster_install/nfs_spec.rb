f60e85e2dbd12ee96fdce687092139ae628060ec
Enrico Usai <usai@amazon.com>
2023-01-19 17:13:12 +0100

    Improve NFS resource tests
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- test/resources/controls/aws_parallelcluster_install/nfs_spec.rb
+++ test/resources/controls/aws_parallelcluster_install/nfs_spec.rb
@@ -8,8 +8,11 @@ control 'nfs' do
     end
 
     # Check version of NFS
-    describe command("rpcinfo -p localhost") do
-      its('stdout') { should match(/4.*tcp.*2049.*nfs/) }
+    describe "Verify installed NFS version is 4\n" do
+      nfs_version = command("rpcinfo -p localhost | awk '{print $5$2}' | grep nfs4")
+      describe nfs_version do
+        its('stdout') { should match "nfs4" }
+      end
     end
   end
 end
