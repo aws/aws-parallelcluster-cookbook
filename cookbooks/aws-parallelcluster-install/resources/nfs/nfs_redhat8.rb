789b167d1df1f45c35b00a42c44b07fc0acbb52e
Enrico Usai <usai@amazon.com>
2023-01-19 16:50:23 +0100

    Create a nfs resource for each OS and add support for redhat8
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-install/resources/nfs/nfs_redhat8.rb
+++ cookbooks/aws-parallelcluster-install/resources/nfs/nfs_redhat8.rb
@@ -13,7 +13,9 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-provides :nfs
+provides :nfs, platform: 'redhat' do |node|
+  node['platform_version'].to_i == 8
+end
 unified_mode true
 
 use 'partial/_install_nfs4_and_disable'
