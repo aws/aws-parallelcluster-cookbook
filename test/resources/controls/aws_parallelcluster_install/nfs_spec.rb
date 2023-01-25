accdac3ca144c85b75ab282a911fe77d765cbbe5
Tatiana Litvinova <litvit@amazon.com>
2023-01-26 00:54:57 +0100

    Replace if with only_if in nfs_spec.rb

--- test/resources/controls/aws_parallelcluster_install/nfs_spec.rb
+++ test/resources/controls/aws_parallelcluster_install/nfs_spec.rb
@@ -1,18 +1,18 @@
 control 'nfs' do
   title 'NFS resource'
 
-  if virtualization.system != 'docker'
-    # Check nfsd process is running
-    describe command('ps aux') do
-      its('stdout') { should match(/nfsd/) }
-    end
+  only_if { !os_properties.docker? }
 
-    # Check version of NFS
-    describe "Verify installed NFS version is 4\n" do
-      nfs_version = command("rpcinfo -p localhost | awk '{print $5$2}' | grep nfs4")
-      describe nfs_version do
-        its('stdout') { should match "nfs4" }
-      end
+  # Check nfsd process is running
+  describe command('ps aux') do
+    its('stdout') { should match(/nfsd/) }
+  end
+
+  # Check version of NFS
+  describe "Verify installed NFS version is 4\n" do
+    nfs_version = command("rpcinfo -p localhost | awk '{print $5$2}' | grep nfs4")
+    describe nfs_version do
+      its('stdout') { should match "nfs4" }
     end
   end
 end
