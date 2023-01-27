3f6778db2a6700d4858386396582de31bf6f2956
Jacopo De Amicis <jdamicis@amazon.it>
2023-01-27 14:29:55 +0100

    Install MySQL packages from OS repos for Ubuntu x86_64
    Remove the previous infrastructure to install MySQL packages for
    Ubuntu 18.04 and 20.04 x86_64 from S3.
    Now Ubuntu x86_64 is treated like Ubuntu arm.
    
    Signed-off-by: Jacopo De Amicis <jdamicis@amazon.it>

--- cookbooks/aws-parallelcluster-install/recipes/install_mysql_client.rb
+++ cookbooks/aws-parallelcluster-install/recipes/install_mysql_client.rb
@@ -15,7 +15,7 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-if platform?('ubuntu') && arm_instance?
+if platform?('ubuntu')
   package node['cluster']['mysql']['repository']['packages'] do
     retries 3
     retry_delay 5
