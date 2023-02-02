d54eef927b01e28945c2566a84dde6af158ca2a4
Enrico Usai <usai@amazon.com>
2023-02-02 13:34:15 +0100

    Add test for ami_cleanup file creation
    Signed-off-by: Enrico Usai <usai@amazon.com>

---
+++ cookbooks/aws-parallelcluster-install/recipes/ami_cleanup.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+#
+# Cookbook:: aws-parallelcluster-install
+# Recipe:: ami_cleanup
+#
+# Copyright:: 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file.
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+# Create AMI cleanup script
+cookbook_file "ami_cleanup.sh" do
+  source 'base/ami_cleanup.sh'
+  path '/usr/local/sbin/ami_cleanup.sh'
+  owner "root"
+  group "root"
+  mode "0755"
+end
