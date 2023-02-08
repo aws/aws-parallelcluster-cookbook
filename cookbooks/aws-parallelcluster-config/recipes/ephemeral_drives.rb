12cf2effb41b72816a339b771661c616948c1364
Enrico Usai <usai@amazon.com>
2023-02-08 15:59:56 +0100

    Move ephemeral_drives config logic into a new recipe
    Added tests to verify service is enabled
    Tested it by passing driver / instance_type: m5d.xlarge for EC2 tests.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

---
+++ cookbooks/aws-parallelcluster-config/recipes/ephemeral_drives.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+#
+# Cookbook:: aws-parallelcluster-config
+# Recipe:: ephemeral_drives
+#
+# Copyright:: 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the
+# License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Mount the ephemeral drive unless there is a mountpoint collision with shared drives
+shared_dir_array = node['cluster']['ebs_shared_dirs'].split(',') + \
+                   node['cluster']['efs_shared_dirs'].split(',') + \
+                   node['cluster']['fsx_shared_dirs'].split(',') + \
+                   [ node['cluster']['raid_shared_dir'] ]
+
+unless shared_dir_array.include? node['cluster']['ephemeral_dir']
+  service "setup-ephemeral" do
+    supports restart: false
+    action :enable
+  end
+
+  # Execution timeout 3600 seconds
+  unless virtualized?
+    execute "Setup of ephemeral drives" do
+      user "root"
+      command "/usr/local/sbin/setup-ephemeral-drives.sh"
+    end
+  end
+end
