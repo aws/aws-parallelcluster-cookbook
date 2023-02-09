ca84ce1c3c5294f9db387c2305e783cf94da92d8
Ermanno Moser <ermann@amazon.com>
2023-02-09 23:31:18 +0100

    Convert stunnel install to recipe

---
+++ test/resources/controls/aws_parallelcluster_install/stunnel_spec.rb
@@ -0,0 +1,25 @@
+# Copyright:: 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file.
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+control 'stunnel_installed' do
+  title "Check that the correct version of stunnel has been installed"
+
+  # In AL2 stunnel comes as part of the aws-efs-utils package.
+  # In RHEL8 on Docker we disable the installation of base packages, so stunnel cannot be built.
+  only_if { !os_properties.alinux2? && !os_properties.redhat_ubi? }
+
+  stunnel_version = '5.67'
+
+  describe bash("/bin/stunnel -version 2>&1 | awk '/^stunnel / { printf $2 }'") do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should eq stunnel_version }
+  end
+end
