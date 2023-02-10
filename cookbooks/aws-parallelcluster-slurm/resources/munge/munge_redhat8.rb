47561badf11f945e79965f726bebf5c396254705
Nicola Sirena <nssirena@amazon.com>
2023-02-10 09:22:02 +0100

    Transform install_munge recipe into a resource

---
+++ cookbooks/aws-parallelcluster-slurm/resources/munge/munge_redhat8.rb
@@ -0,0 +1,24 @@
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file.
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+provides :munge, platform: 'redhat' do |node|
+  node['platform_version'].to_i == 8
+end
+unified_mode true
+
+default_action :setup
+
+use 'partial/_munge_actions'
+
+action_class do
+  def munge_libdir
+    '/usr/lib64'
+  end
+end
