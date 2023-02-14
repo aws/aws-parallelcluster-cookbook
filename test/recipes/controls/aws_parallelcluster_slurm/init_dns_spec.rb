0ff13cdc0a1307bcba1bc15fc9a2574ff973aa92
Nicola Sirena <nssirena@amazon.com>
2023-02-14 17:24:10 +0100

    Adds test for init_dns recipe

--- test/recipes/controls/aws_parallelcluster_slurm/init_dns_spec.rb
+++ test/recipes/controls/aws_parallelcluster_slurm/init_dns_spec.rb
@@ -9,19 +9,21 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-control 'dns_domain_configured' do
-  title "Checks that the DNS search domain is configured"
+control 'hostname_configured' do
+  title 'Checks hostname is properly set'
 
-  dns_domain_string = 'test-domain'
-  config_file = '/etc/dhcp/dhclient.conf'
-  if os_properties.debian_family?
-    config_file = '/etc/systemd/resolved.conf'
-  end
-
-  describe file(config_file) do
+  describe file('/etc/hostname') do
     it { should exist }
     its('content') do
-      should match(dns_domain_string)
+      should match("^ip-[0-9]*-[0-9]*-[0-9]*-[0-9]*")
+    end
+  end
+
+  describe file('/etc/hosts') do
+    it { should exist }
+    its('content') do
+      should match("^[0-9]*.[0-9]*.[0-9]*.[0-9]*")
+      should match("ip-[0-9]*-[0-9]*-[0-9]*-[0-9]*")
     end
   end
 end
