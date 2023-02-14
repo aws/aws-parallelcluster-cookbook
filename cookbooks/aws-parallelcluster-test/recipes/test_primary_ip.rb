6dbe74e1a4e317e0fe9758bd8bf61e92857b911c
nse Obot <nseobot@amazon.com>
2023-02-13 21:16:29 -0500

    move exit 1 outside for loop

--- cookbooks/aws-parallelcluster-test/recipes/test_primary_ip.rb
+++ cookbooks/aws-parallelcluster-test/recipes/test_primary_ip.rb
@@ -20,11 +20,11 @@ bash "check primary ip" do
   code <<-TEST
   TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
   macs=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/network/interfaces/macs`
-  for mac in ${macs}
+  for mac in $macs
   do
     device_number=`curl -H "X-aws-ec2-metadata-token: $TOKEN" "http://169.254.169.254/latest/meta-data/network/interfaces/macs/${mac}/device-number"`
     network_card=`curl -H "X-aws-ec2-metadata-token: $TOKEN" "http://169.254.169.254/latest/meta-data/network/interfaces/macs/${mac}/network-card"`
-    if [[ ${device_number} == '0' && ${network_card} == '0' ]]
+    if [[ $device_number == '0' && $network_card == '0' ]]
     then
       IP_HOSTS="$(grep "$HOSTNAME" /etc/hosts | awk '{print $1}')"
       mac_ip=`curl -H "X-aws-ec2-metadata-token: $TOKEN" "http://169.254.169.254/latest/meta-data/network/interfaces/macs/${mac}/local-ipv4s"`
@@ -36,8 +36,8 @@ bash "check primary ip" do
         fi
       done
     fi
-    echo Error: Route53 IP does not match host IP
-    exit 1
   done
+  echo Error: Route53 IP does not match host IP
+  exit 1
   TEST
 end
