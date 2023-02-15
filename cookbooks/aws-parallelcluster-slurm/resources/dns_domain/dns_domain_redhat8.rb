94d6a7ca349585b6eed732d079860cdcdffa4d27
Nicola Sirena <nssirena@amazon.com>
2023-02-15 11:40:42 +0100

    Skip tests on docker for init-dns recipe and dns-update resource

--- cookbooks/aws-parallelcluster-slurm/resources/dns_domain/dns_domain_redhat8.rb
+++ cookbooks/aws-parallelcluster-slurm/resources/dns_domain/dns_domain_redhat8.rb
@@ -18,6 +18,8 @@ default_action :configure
 # Configure custom dns domain (only if defined) by appending the Route53 domain created within the cluster
 # ($CLUSTER_NAME.pcluster) and be listed as a "search" domain in the resolv.conf file.
 action :configure do
+  return if virtualized?
+
   Chef::Log.info("Appending search domain '#{node['cluster']['dns_domain']}' to /etc/dhcp/dhclient.conf")
   # Configure dhclient to automatically append Route53 search domain in resolv.conf
   # - on CentOS7 and Alinux2 resolv.conf is managed by NetworkManager + dhclient,
