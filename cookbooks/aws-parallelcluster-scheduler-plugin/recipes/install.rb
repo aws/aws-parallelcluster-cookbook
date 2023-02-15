942639b60dce6cfac9abd6c514ba867706019815
Enrico Usai <usai@amazon.com>
2023-02-15 11:20:50 +0100

    Skip unsupported install recipes when building RHEL8 images
    AWS Batch and Scheduler Plugin won't be supported at all.
    DCV and EFS will be supported soon.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-scheduler-plugin/recipes/install.rb
+++ cookbooks/aws-parallelcluster-scheduler-plugin/recipes/install.rb
@@ -15,6 +15,8 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
+return if redhat8?
+
 # setup the user accounts
 include_recipe "aws-parallelcluster-scheduler-plugin::install_user"
 
