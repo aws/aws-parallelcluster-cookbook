522eff1d06afeaf700ac5df80d8d7e919ac95d8b
Enrico Usai <usai@amazon.com>
2023-02-16 17:40:32 +0100

    Remove environment-modules from base_packages list
    This packages is only required by EFA and installed as EFA requirement.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-install/attributes/default_ubuntu18.rb
+++ cookbooks/aws-parallelcluster-install/attributes/default_ubuntu18.rb
@@ -10,7 +10,7 @@ default['cluster']['base_packages'] = %w(vim ksh tcsh zsh libssl-dev ncurses-dev
                                          libgcrypt20-dev libevent-dev iproute2 python3 python3-pip
                                          libatlas-base-dev libglvnd-dev iptables libcurl4-openssl-dev
                                          coreutils moreutils sssd sssd-tools sssd-ldap curl
-                                         python-pip python-parted environment-modules)
+                                         python-pip python-parted)
 
 default['cluster']['kernel_headers_pkg'] = "linux-headers-#{node['kernel']['release']}"
 
