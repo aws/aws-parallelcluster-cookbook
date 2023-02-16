b2b092106b7e51782ab1d6a40f68bd61c4266bfc
Enrico Usai <usai@amazon.com>
2023-02-16 17:42:53 +0100

    Remove lvm2 package from base_packages list
    This is only required by ephemeral_drives recipe execution
    and explicitly installed in the aws-parallelcluster-install/ephemeral_drives.rb
    
    Move test for installation of this package into the ephemeral_drives_spec.rb
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-install/attributes/default_redhat8.rb
+++ cookbooks/aws-parallelcluster-install/attributes/default_redhat8.rb
@@ -5,7 +5,7 @@ return unless platform?('redhat') && node['platform_version'].to_i == 8
 # Removed libssh2-devel from base_packages since is not shipped by RedHat 8 and in conflict with package libssh-0.9.6-3.el8.x86_64
 default['cluster']['base_packages'] = %w(vim ksh tcsh zsh openssl-devel ncurses-devel pam-devel net-tools openmotif-devel
                                              libXmu-devel hwloc-devel libdb-devel tcl-devel automake autoconf pyparted libtool
-                                             httpd boost-devel redhat-lsb mlocate lvm2 R atlas-devel
+                                             httpd boost-devel redhat-lsb mlocate R atlas-devel
                                              blas-devel libffi-devel dkms libedit-devel
                                              libical-devel postgresql-devel postgresql-server sendmail libxml2-devel libglvnd-devel
                                              mdadm python2 python2-pip libgcrypt-devel libevent-devel glibc-static bind-utils
