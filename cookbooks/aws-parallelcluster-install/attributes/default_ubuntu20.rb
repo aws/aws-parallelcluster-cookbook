9937ff0025ca1c3347c448a8764e191afe693eca
Ermanno Moser <ermann@amazon.com>
2023-02-16 20:17:50 +0100

    Add IMDS test

--- cookbooks/aws-parallelcluster-install/attributes/default_ubuntu20.rb
+++ cookbooks/aws-parallelcluster-install/attributes/default_ubuntu20.rb
@@ -3,6 +3,7 @@
 return unless platform?('ubuntu') && node['platform_version'] == "20.04"
 
 # environment-modules required by EFA, Intel MPI and ARM PL
+# iptables needed for IMDS setup
 default['cluster']['base_packages'] = %w(vim ksh tcsh zsh libssl-dev ncurses-dev libpam-dev net-tools libhwloc-dev dkms
                                          tcl-dev automake autoconf libtool librrd-dev libapr1-dev libconfuse-dev
                                          apache2 libboost-dev libdb-dev libncurses5-dev libpam0g-dev libxt-dev
