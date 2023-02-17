79e77bc895a3eedc7676372d6683fdc99cffd07d
Enrico Usai <usai@amazon.com>
2023-02-17 09:30:22 +0100

    Restore environment-modules in the base_packages list
    This reverts commit 7d00d32ef89299f209738678d9ee1a3668039a64
    
    This package is required by both EFA resource, Intel MPI and ARM PL recipes.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-install/attributes/default_ubuntu20.rb
+++ cookbooks/aws-parallelcluster-install/attributes/default_ubuntu20.rb
@@ -2,6 +2,7 @@
 
 return unless platform?('ubuntu') && node['platform_version'] == "20.04"
 
+# environment-modules required by EFA, Intel MPI and ARM PL
 default['cluster']['base_packages'] = %w(vim ksh tcsh zsh libssl-dev ncurses-dev libpam-dev net-tools libhwloc-dev dkms
                                          tcl-dev automake autoconf libtool librrd-dev libapr1-dev libconfuse-dev
                                          apache2 libboost-dev libdb-dev libncurses5-dev libpam0g-dev libxt-dev
@@ -9,7 +10,7 @@ default['cluster']['base_packages'] = %w(vim ksh tcsh zsh libssl-dev ncurses-dev
                                          r-base libblas-dev libffi-dev libxml2-dev mdadm
                                          libgcrypt20-dev libevent-dev iproute2 python3 python3-pip
                                          libatlas-base-dev libglvnd-dev iptables libcurl4-openssl-dev
-                                         coreutils moreutils sssd sssd-tools sssd-ldap curl python3-parted)
+                                         coreutils moreutils sssd sssd-tools sssd-ldap curl python3-parted environment-modules)
 
 default['cluster']['kernel_headers_pkg'] = "linux-headers-#{node['kernel']['release']}"
 
