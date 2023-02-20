60f988ace9a483a4a3c56386c5266c9cb35ce41f
Francesco Giordano <giordafr@amazon.it>
2023-02-20 16:54:42 +0100

    Fix chrony recipe and spec in case of redhat_ubi
    Signed-off-by: Francesco Giordano <giordafr@amazon.it>

--- cookbooks/aws-parallelcluster-install/recipes/chrony.rb
+++ cookbooks/aws-parallelcluster-install/recipes/chrony.rb
@@ -15,6 +15,8 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
+return if redhat_ubi?
+
 # Install Amazon Time Sync
 package %w(ntp ntpdate ntp*) do
   action :remove
@@ -23,7 +25,7 @@ end
 package %w(chrony) do
   retries 3
   retry_delay 5
-end unless redhat_ubi?
+end
 
 append_if_no_line "add configuration to chrony.conf" do
   path node['cluster']['chrony']['conf']
