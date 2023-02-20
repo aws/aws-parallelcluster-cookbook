1fe36b4c7d7cb65ad44e800340b9d2d14ddfab8c
Francesco Giordano <giordafr@amazon.it>
2023-02-20 16:01:20 +0100

    Moved efa_system_settings_configured control in config directory
    Signed-off-by: Francesco Giordano <giordafr@amazon.it>

--- test/resources/controls/aws_parallelcluster_install/efa_spec.rb
+++ test/resources/controls/aws_parallelcluster_install/efa_spec.rb
@@ -59,13 +59,3 @@ control 'efa_installed' do
     end
   end
 end
-
-control 'efa_system_settings_configured' do
-  title 'Check system is correctly configured for EFA'
-
-  only_if { os.debian? && !os_properties.virtualized? }
-
-  describe kernel_parameter('kernel.yama.ptrace_scope') do
-    its('value') { should eq 0 }
-  end
-end
