fe167227d642a0bab18587eca798e350bacd90aa
Tatiana Litvinova <litvit@amazon.com>
2023-02-23 14:24:05 +0100

    ChefSpec test for efa resource

--- cookbooks/aws-parallelcluster-common/spec/spec_helper.rb
+++ cookbooks/aws-parallelcluster-common/spec/spec_helper.rb
@@ -1,1 +1,16 @@
 require 'chefspec'
+
+module ChefSpec
+  class Runner
+    # Allows to converge a dynamic code block
+    # For instance, it can be used to invoke actions on resources
+    def converge_dsl(*recipes, &block)
+      cookbook_name = 'any'
+      recipe_name = 'any'
+      converge(*recipes) do
+        recipe = Chef::Recipe.new(cookbook_name, recipe_name, @run_context)
+        recipe.instance_eval(&block)
+      end
+    end
+  end
+end
