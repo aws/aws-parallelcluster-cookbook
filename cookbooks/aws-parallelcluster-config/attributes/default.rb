c3834770d6968740f1ad00a2e1381cad5aef6d3b
Francesco Giordano <giordafr@amazon.it>
2023-02-23 15:56:21 +0100

    Move attributes in the corresponding default files
    Signed-off-by: Francesco Giordano <giordafr@amazon.it>

---
+++ cookbooks/aws-parallelcluster-config/attributes/default.rb
@@ -0,0 +1,5 @@
+# aws-parallelcluster-config attributes
+
+default['cluster']['raid_shared_dir'] = ''
+default['cluster']['exported_raid_shared_dir'] = node['cluster']['raid_shared_dir']
+default['cluster']['exported_intel_dir'] = '/opt/intel'
