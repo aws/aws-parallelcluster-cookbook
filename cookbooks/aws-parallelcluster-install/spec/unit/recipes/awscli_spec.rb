f8465646a8c2eb669b392fbea6f3e75f260b5d2c
Tatiana Litvinova <litvit@amazon.com>
2023-02-28 20:02:36 +0100

    Fix for_all_oses function

--- cookbooks/aws-parallelcluster-install/spec/unit/recipes/awscli_spec.rb
+++ cookbooks/aws-parallelcluster-install/spec/unit/recipes/awscli_spec.rb
@@ -6,33 +6,35 @@ describe 'aws-parallelcluster-install::awscli' do
   end
 
   for_all_oses do |platform, version|
-    let(:chef_run) do
-      ChefSpec::Runner.new(platform: platform, version: version)
-    end
-
-    context 'when aws cli exists' do
-      before do
-        mock_file_exists("/usr/local/bin/aws", true)
-        chef_run.converge(described_recipe)
+    context "on #{platform}#{version}" do
+      let(:chef_run) do
+        ChefSpec::Runner.new(platform: platform, version: version)
       end
 
-      it "should not install it" do
-        is_expected.not_to run_bash("install awscli")
-      end
-    end
+      context 'when aws cli exists' do
+        before do
+          mock_file_exists("/usr/local/bin/aws", true)
+          chef_run.converge(described_recipe)
+        end
 
-    context 'when aws cli does not exist' do
-      before do
-        mock_file_exists("/usr/local/bin/aws", false)
-        chef_run.converge(described_recipe)
-      end
-
-      it "should install unzip" do
-        is_expected.to install_package("unzip")
+        it "should not install it" do
+          is_expected.not_to run_bash("install awscli")
+        end
       end
 
-      it "should install awscli" do
-        is_expected.to run_bash("install awscli")
+      context 'when aws cli does not exist' do
+        before do
+          mock_file_exists("/usr/local/bin/aws", false)
+          chef_run.converge(described_recipe)
+        end
+
+        it "should install unzip" do
+          is_expected.to install_package("unzip")
+        end
+
+        it "should install awscli" do
+          is_expected.to run_bash("install awscli")
+        end
       end
     end
   end
