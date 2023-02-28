5f539c1b1043070b590816d66c689d771dd5678b
Tatiana Litvinova <litvit@amazon.com>
2023-02-28 20:02:36 +0100

    Fix for_all_oses function

--- cookbooks/aws-parallelcluster-install/spec/unit/recipes/ami_cleanup_spec.rb
+++ cookbooks/aws-parallelcluster-install/spec/unit/recipes/ami_cleanup_spec.rb
@@ -2,21 +2,23 @@ require 'spec_helper'
 
 describe 'aws-parallelcluster-install::ami_cleanup' do
   for_all_oses do |platform, version|
-    let(:chef_run) do
-      ChefSpec::Runner.new(platform: platform, version: version)
-    end
+    context "on #{platform}#{version}" do
+      let(:chef_run) do
+        ChefSpec::Runner.new(platform: platform, version: version)
+      end
 
-    before do
-      chef_run.converge(described_recipe)
-    end
+      before do
+        chef_run.converge(described_recipe)
+      end
 
-    it 'Creates ami_cleanup.sh under /usr/local/sbin' do
-      is_expected.to create_cookbook_file("ami_cleanup.sh")
-        .with(source: 'base/ami_cleanup.sh')
-        .with(path: '/usr/local/sbin/ami_cleanup.sh')
-        .with(owner: 'root')
-        .with(group: 'root')
-        .with(mode: '0755')
+      it 'Creates ami_cleanup.sh under /usr/local/sbin' do
+        is_expected.to create_cookbook_file("ami_cleanup.sh")
+          .with(source: 'base/ami_cleanup.sh')
+          .with(path: '/usr/local/sbin/ami_cleanup.sh')
+          .with(owner: 'root')
+          .with(group: 'root')
+          .with(mode: '0755')
+      end
     end
   end
 end
