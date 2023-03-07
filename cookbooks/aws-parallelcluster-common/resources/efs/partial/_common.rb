9e87f0cd283ac0ece5cac94b8d8796ed1e5517c3
Tatiana Litvinova <litvit@amazon.com>
2023-03-07 16:51:23 +0100

    Cleanup efs:install_utils

--- cookbooks/aws-parallelcluster-common/resources/efs/partial/_common.rb
+++ cookbooks/aws-parallelcluster-common/resources/efs/partial/_common.rb
@@ -1,3 +1,7 @@
+unified_mode true
+
+default_action :install_utils
+
 def already_installed?(package_name, expected_version)
   Gem::Version.new(get_package_version(package_name)) >= Gem::Version.new(expected_version)
 end
