c6e2a2c0c96519fa3e2304968ea68c4a7c31106d
Tatiana Litvinova <litvit@amazon.com>
2023-03-09 00:45:43 +0100

    Fix nfs ChefSpec

--- cookbooks/aws-parallelcluster-common/spec/unit/resources/nfs_spec.rb
+++ cookbooks/aws-parallelcluster-common/spec/unit/resources/nfs_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 class ConvergeNfs
   def self.setup(chef_run)
-    chef_run.converge_dsl('nfs::server', 'nfs::server4', 'nfs::_common') do
+    chef_run.converge_dsl do
       nfs 'setup' do
         action :setup
       end
@@ -21,47 +21,81 @@ end
 describe 'nfs:setup' do
   for_all_oses do |platform, version|
     context "on #{platform}#{version}" do
-      let(:chef_run) do
-        ChefSpec::Runner.new(
+      cached(:disabled_service) { 'disable_service' }
+      cached(:chef_run) do
+        runner = ChefSpec::Runner.new(
           platform: platform, version: version,
           step_into: ['nfs']
-        )
-      end
-
-      # cached(:node) { chef_run.node }
-      cached(:nfs_service) { platform == 'ubuntu' ? 'nfs-kernel-server.service' : 'nfs-server.service' }
-
-      before :each do
-        block_stepping_into_recipe
+        ) do |node|
+          node.override['nfs']['service']['server'] = disabled_service
+        end
+        ConvergeNfs.setup(runner)
       end
 
       if %w(amazon centos redhat).include?(platform)
-        # It doesn't work because nfs::server fails to use `template` resource
-        # It should skip the entire recipe but it doesn't work.
-        #
-        # TODO: find a way to fix
-        #
-        # it 'installs nfs::server4' do
-        #   expect_to_include_recipe_from_resource('nfs::server')
-        #   expect_to_include_recipe_from_resource('nfs::server4')
-        #   ConvergeNfs.setup(chef_run)
-        # end
-        #
-        # it 'disables service at boot' do
-        #   ConvergeNfs.setup(chef_run)
-        #   is_expected.to disable_service(nfs_service)
-        # end
+        it 'installs nfs::server4' do
+          expect_to_include_recipe_from_resource('nfs::server4')
+          chef_run
+        end
 
-      elsif %w(ubuntu).include?(platform)
-        it 'installs nfs::server and disables service start at boot' do
+      elsif platform == 'ubuntu'
+        it 'installs nfs::server and nfs:server4' do
           expect_to_include_recipe_from_resource('nfs::server')
           expect_to_include_recipe_from_resource('nfs::server4')
-          ConvergeNfs.setup(chef_run)
-          expect(chef_run).to disable_service(nfs_service)
+          chef_run
         end
 
       else
-        pending "Implement for #{platform}"
+        pending "to be implemented"
+      end
+
+      it 'disables service at boot' do
+        is_expected.to disable_service(disabled_service)
+      end
+    end
+  end
+end
+
+describe 'nfs:configure' do
+  for_all_oses do |platform, version|
+    context "on #{platform}#{version}" do
+      cached(:threads) { 10 }
+      cached(:server_template) { 'server_template' }
+      cached(:nfs_service) { 'nfs_service' }
+      cached(:chef_run) do
+        runner = ChefSpec::Runner.new(
+          platform: platform, version: version,
+          step_into: ['nfs']
+        ) do |node|
+          node.override['cluster']['nfs']['threads'] = threads
+          node.override['nfs']['config']['server_template'] = server_template
+          node.override['nfs']['service']['server'] = nfs_service
+        end
+        ConvergeNfs.configure(runner)
+      end
+
+      if %w(amazon centos ubuntu).include?(platform)
+        it 'overrides nfs config with custom template' do
+          is_expected.to create_template(server_template)
+            .with(source: 'nfs/nfs.conf.erb')
+            .with(cookbook: 'aws-parallelcluster-common')
+        end
+
+      elsif platform == 'redhat'
+        it 'uses nfs config template shipped with nfs cookbook' do
+          is_expected.to create_template(server_template)
+            .with(source: "#{server_template}.erb")
+            .with(cookbook: 'nfs')
+        end
+
+      else
+        pending "to be implemented"
+      end
+
+      it 'enables and restarts service' do
+        is_expected.to restart_service(nfs_service)
+          .with(action: %i(restart enable))
+          .with(supports: { restart: true })
       end
     end
   end
