b0f8d39c068f3fd26e0607c5f92211bad49b7c8c
Francesco Giordano <giordafr@amazon.it>
2023-03-10 15:25:14 +0100

    Add template to configure setup-ephemeral.service in order to wait after network-online.target for redhat8
    https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/configuring_and_managing_networking/systemd-network-targets-and-services_configuring-and-managing-networking
    
    Signed-off-by: Francesco Giordano <giordafr@amazon.it>

--- cookbooks/aws-parallelcluster-install/resources/ephemeral_drivers/partial/_ephemeral_drives_common.rb
+++ cookbooks/aws-parallelcluster-install/resources/ephemeral_drivers/partial/_ephemeral_drives_common.rb
@@ -16,24 +16,30 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-package "install Logical Volume Manager 2 utilities" do
-  package_name "lvm2"
-  retries 3
-  retry_delay 5
-end
+unified_mode true
+default_action :setup
 
-cookbook_file 'setup-ephemeral-drives.sh' do
-  source 'base/setup-ephemeral-drives.sh'
-  path '/usr/local/sbin/setup-ephemeral-drives.sh'
-  owner 'root'
-  group 'root'
-  mode '0744'
-end
+action :setup do
+  package "install Logical Volume Manager 2 utilities" do
+    package_name "lvm2"
+    retries 3
+    retry_delay 5
+  end
 
-cookbook_file 'setup-ephemeral.service' do
-  source 'base/setup-ephemeral.service'
-  path '/etc/systemd/system/setup-ephemeral.service'
-  owner 'root'
-  group 'root'
-  mode '0644'
+  cookbook_file 'setup-ephemeral-drives.sh' do
+    source 'base/setup-ephemeral-drives.sh'
+    path '/usr/local/sbin/setup-ephemeral-drives.sh'
+    owner 'root'
+    group 'root'
+    mode '0744'
+  end
+
+  template 'setup-ephemeral.service' do
+    source 'base/setup-ephemeral.service.erb'
+    path '/etc/systemd/system/setup-ephemeral.service'
+    owner 'root'
+    group 'root'
+    mode '0644'
+    variables(network_target: network_target)
+  end
 end
