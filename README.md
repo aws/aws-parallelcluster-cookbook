f8155393c3e8e281b28e9f12ee8e5132b1aaf98a
Paolo Sacconier <psacc@amazon.it>
2023-03-13 12:41:09 +0100

    tox codecov + report upload in ci (#1874)
    * tox codecov + report upload in ci
    
    * linting fix
    
    ---------
    
    Signed-off-by: Paolo Sacconier <psacc@amazon.it>

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 AWS ParallelCluster Cookbook
 ============================
 
-![Build Status](https://github.com/aws/aws-parallelcluster-cookbook/actions/workflows/ci.yml/badge.svg?event=push)
+[![codecov](https://codecov.io/gh/aws/aws-parallelcluster-cookbook/branch/develop/graph/badge.svg)](https://codecov.io/gh/aws/aws-parallelcluster-cookbook)
+[![Build Status](https://github.com/aws/aws-parallelcluster-cookbook/actions/workflows/ci.yml/badge.svg?event=push)](https://github.com/aws/aws-parallelcluster-cookbook/actions)
 
 This repo contains the AWS ParallelCluster Chef cookbook used in AWS ParallelCluster.
--- cookbooks/aws-parallelcluster-install/files/default/cloudwatch_agent/verify_cloudwatch_agent_public_key_fingerprint.py
+++ cookbooks/aws-parallelcluster-install/files/default/cloudwatch_agent/verify_cloudwatch_agent_public_key_fingerprint.py
@@ -2,6 +2,7 @@
 """Verify the fingerprint for the CloudWatch agent's public key is as expected."""
 
 import sys
+
 import gnupg
 
 # This value comes from the CloudWatch agent's documentation:
@@ -11,7 +12,7 @@ EXPECTED_FINGERPRINT = "937616F3450B7D806CBD9725D58167303B789C72"
 
 def get_key_fingerprint():
     """Get identifier for CloudWatch agent's key."""
-    keys = gnupg.GPG().list_keys(keys='Amazon CloudWatch Agent')
+    keys = gnupg.GPG().list_keys(keys="Amazon CloudWatch Agent")
     for key in keys:
         if "Amazon CloudWatch Agent" in key.get("uids"):
             return key.get("fingerprint")
@@ -23,11 +24,10 @@ def main():
     fingerprint = get_key_fingerprint()
     if fingerprint != EXPECTED_FINGERPRINT:
         sys.exit(
-            'Observed fingerprint of cloudwatch agent public key ({observed}) '
-            'does not match expected fingerprint ({expected}).'
-            .format(observed=fingerprint, expected=EXPECTED_FINGERPRINT)
+            f"Observed fingerprint of cloudwatch agent public key ({fingerprint}) "
+            f"does not match expected fingerprint ({EXPECTED_FINGERPRINT})."
         )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
