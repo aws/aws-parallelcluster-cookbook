303a88b0811244cd01be147fbfe2c54ac68ed95b
Tatiana Litvinova <litvit@amazon.com>
2023-03-13 18:54:41 +0100

    Enable kitchen lifecycle hooks for recipes
    https://kitchen.ci/docs/reference/lifecycle-hooks/
    You can create a script under test/recipes/hooks/<suite_name> with the name
    corresponding to the lifecycle event name (for instance, post_create.sh).
    The script will be run accordingly.
    
    Currently supported events: post_create pre_converge pre_destroy
    
    You can add another event to lifecycle section of kitchen.recipes.yml to the need.
    Fix root volume id retrieval in integration tests

--- kitchen.docker.sh
+++ kitchen.docker.sh
@@ -11,5 +11,6 @@
 export KITCHEN_LOCAL_YAML="kitchen.$1.yml"; shift;
 export KITCHEN_YAML=kitchen.docker.yml
 export KITCHEN_GLOBAL_YAML=kitchen.global.yml
+export KITCHEN_DRIVER=dokken
 
 kitchen "$@"
--- kitchen.ec2.sh
+++ kitchen.ec2.sh
@@ -50,6 +50,7 @@
 export KITCHEN_LOCAL_YAML="kitchen.$1.yml"; shift;
 export KITCHEN_YAML=kitchen.ec2.yml
 export KITCHEN_GLOBAL_YAML=kitchen.global.yml
+export KITCHEN_DRIVER=ec2
 
 THIS_DIR=$(dirname "$0")
 if [ -e "${THIS_DIR}/.kitchen.env.sh" ]
@@ -57,5 +58,63 @@ then
   echo "*** Apply local environment"
   source "${THIS_DIR}/.kitchen.env.sh"
 fi
+
+: "${KITCHEN_AWS_REGION:=${AWS_DEFAULT_REGION:-eu-west-1}}"
+: "${KITCHEN_KEY_NAME:=kitchen}"
+: "${KITCHEN_SSH_KEY_PATH:="~/.ssh/${KITCHEN_KEY_NAME}-${KITCHEN_AWS_REGION}.pem"}"
+: "${KITCHEN_AVAILABILITY_ZONE:=a}"
+
+# Subnet
+if [ -z "${KITCHEN_SUBNET_ID}" ]; then
+  echo "** KITCHEN_SUBNET_ID not explicitly set: looking for subnet tagged Kitchen=true"
+
+  KITCHEN_SUBNET_ID=$(aws ec2 describe-subnets --region "${KITCHEN_AWS_REGION}" \
+      --filters "Name=tag:Kitchen,Values=true" \
+                "Name=availability-zone,Values=${KITCHEN_AWS_REGION}${KITCHEN_AVAILABILITY_ZONE}" \
+      --query 'Subnets[0].SubnetId' --output text)
+
+  echo "** KITCHEN_SUBNET_ID: ${KITCHEN_SUBNET_ID}"
+
+  if [ "${KITCHEN_SUBNET_ID}" = "None" ]; then
+    echo "Subnet tagged Kitchen=true not found in AZ ${KITCHEN_AWS_REGION}${KITCHEN_AVAILABILITY_ZONE}"
+    exit 1
+  fi
+fi
+
+# VPC
+KITCHEN_VPC_ID=$(aws ec2 describe-subnets --region "${KITCHEN_AWS_REGION}" \
+    --subnet-ids "${KITCHEN_SUBNET_ID}" \
+    --query 'Subnets[0].VpcId' --output text)
+
+echo "** KITCHEN_VPC_ID: ${KITCHEN_VPC_ID}"
+
+# Security Group
+if [ -z "${KITCHEN_SECURITY_GROUP_ID}" ]; then
+  echo "** KITCHEN_SECURITY_GROUP_ID not explicitly set"
+
+  if [ -z "${KITCHEN_SECURITY_GROUP_NAME}" ]; then
+    echo "** KITCHEN_SECURITY_GROUP_NAME not explicitly set, looking for tag Kitchen=true"
+
+    KITCHEN_SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --region "${KITCHEN_AWS_REGION}" \
+                --filters "Name=tag:Kitchen,Values=true" "Name=vpc-id,Values=${KITCHEN_VPC_ID}" \
+                --query 'SecurityGroups[0].GroupId' --output text)
+  else
+    echo "** Looking for SG named ${KITCHEN_SECURITY_GROUP_NAME}"
+
+    KITCHEN_SECURITY_GROUP_ID=$(aws ec2 describe-security-groups --region "${KITCHEN_AWS_REGION}" \
+                --filters "Name=vpc-id,Values=${KITCHEN_VPC_ID}" "Name=group-name,Values=${KITCHEN_SECURITY_GROUP_NAME}" \
+                --query 'SecurityGroups[0].GroupId' --output text)
+  fi
+
+  echo "** KITCHEN_SECURITY_GROUP_ID: ${KITCHEN_SECURITY_GROUP_ID}"
+
+fi
+
+export KITCHEN_AWS_REGION
+export KITCHEN_KEY_NAME
+export KITCHEN_SSH_KEY_PATH
+export KITCHEN_SUBNET_ID
+export KITCHEN_VPC_ID
+export KITCHEN_SECURITY_GROUP_ID
 
 kitchen "$@"
