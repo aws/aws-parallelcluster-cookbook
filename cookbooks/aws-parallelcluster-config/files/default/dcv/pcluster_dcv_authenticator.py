345029efd56fdac673b3f7d7acab6d8df166f81b
Giacomo Marciani <mgiacomo@amazon.com>
2023-03-13 12:03:28 +0100

    [Code Quality] Fix linter error R1710 in DCv Authenticator. R1710: Either all return statements in a function should return an expression, or none of them should. (inconsistent-return-statements)
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

--- cookbooks/aws-parallelcluster-config/files/default/dcv/pcluster_dcv_authenticator.py
+++ cookbooks/aws-parallelcluster-config/files/default/dcv/pcluster_dcv_authenticator.py
@@ -39,7 +39,7 @@ LOG_FILE_PATH = "/var/log/parallelcluster/pcluster_dcv_authenticator.log"
 logger = logging.getLogger(__name__)
 
 
-def retry(func, func_args, attempts=1, wait=0):  # pylint: disable=R1710
+def retry(func, func_args, attempts=1, wait=0):
     """
     Call function and re-execute it if it raises an Exception.
 
@@ -59,6 +59,7 @@ def retry(func, func_args, attempts=1, wait=0):  # pylint: disable=R1710
 
             logger.info("%s, retrying in %s seconds..", e, wait)
             time.sleep(wait)
+    return None
 
 
 def generate_random_token(token_length):
