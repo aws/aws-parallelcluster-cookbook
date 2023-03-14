70308a82fdb7bf33012d9f06842a40fd4cd44533
Francesco Giordano <giordafr@amazon.it>
2023-03-14 12:49:33 +0100

    Fix RedHat8 environment-modules config path
    Since RedHat8 have environment-modules version 4.x and in contrary of ubuntu does not link the `/usr/share/modules/init/.modulespath` to `/etc/environment-modules/modulespath` we had to change the module configuration file path
    Signed-off-by: Francesco Giordano <giordafr@amazon.it>

--- cookbooks/aws-parallelcluster-common/attributes/common_redhat8.rb
+++ cookbooks/aws-parallelcluster-common/attributes/common_redhat8.rb
@@ -6,6 +6,7 @@ return unless platform?('redhat') && node['platform_version'].to_i == 8
 default['cluster']['modulefile_dir'] = "/usr/share/Modules/modulefiles"
 # MODULESHOME
 default['cluster']['moduleshome'] = "/usr/share/Modules"
-default['cluster']['modulepath_config_file'] = "#{node['cluster']['moduleshome']}/init/.modulespath"
+default['cluster']['modulesconfig'] = "/etc/environment-modules"
+default['cluster']['modulepath_config_file'] = "#{node['cluster']['modulesconfig']}/modulespath"
 
 default['cluster']['chrony']['service'] = "chronyd"
