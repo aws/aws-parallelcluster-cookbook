6cdf40d0ad5f41af70761ce70b71dd7da8f320d0
Delong Meng <delmeng@amazon.com>
2023-03-15 15:37:06 -0700

    try to solve ChefSpec error on the chef handler

--- cookbooks/aws-parallelcluster-slurm/recipes/init.rb
+++ cookbooks/aws-parallelcluster-slurm/recipes/init.rb
@@ -15,7 +15,6 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 # Ensure slurm plugin directory is in place
 # Directory will contain slurm_nodename file used to identify current compute node in computemgtd
 directory node['cluster']['slurm_plugin_dir'] do
