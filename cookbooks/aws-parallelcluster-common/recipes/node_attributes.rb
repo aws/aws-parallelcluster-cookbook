d106b5f219bbbbe7a8c3b7aba811b03f846187b8
Tatiana Litvinova <litvit@amazon.com>
2023-03-16 20:46:10 +0100

    Transform node_attributes to a common recipe
    node_attributes was mistakenly created as a resource.
    We transform it into a recipe and move to aws-parallelcluster-common cookbook
    so that it can be used everywhere.

--- cookbooks/aws-parallelcluster-common/recipes/node_attributes.rb
+++ cookbooks/aws-parallelcluster-common/recipes/node_attributes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+#
+# Cookbook:: aws-parallelcluster-install
+# Recipe:: node_attributes
+#
 # Copyright:: 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License").
@@ -12,15 +16,8 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-provides :node_attributes
-unified_mode true
-default_action :generate_json
-
-action :generate_json do
-  json_content = Chef::JSONCompat.to_json_pretty(node)
-  file "/etc/chef/node_attributes.json" do
-    content "#{json_content}"
-    owner 'root'
-    mode '0644'
-  end
+file "/etc/chef/node_attributes.json" do
+  content Chef::JSONCompat.to_json_pretty(node)
+  owner 'root'
+  mode '0644'
 end
