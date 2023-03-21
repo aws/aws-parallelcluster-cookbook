c8c04b09a88b67f3c4bf74130a4a39bab3d2c457
Tatiana Litvinova <litvit@amazon.com>
2023-03-22 00:53:24 +0100

    Move awsbatch tests from tests.rb recipe to InSpec
    Remove them from aws-parallelcluster-test cookbook, add missing tests to
    InSpec controls and add tag:config to control names in order to pick them
    during final instance validation.

---
+++ test/recipes/controls/aws_parallelcluster_awsbatch/awsbatch_spec.rb
@@ -0,0 +1,27 @@
+# Copyright:: 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file.
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+control 'tag:config_awsbatch_correctly_configured' do
+  only_if { node['cluster']['scheduler'] == 'awsbatch' }
+
+  # Test that batch commands can be accessed without absolute path
+  %w(awsbkill awsbqueues awsbsub awsbhosts awsbout awsbstat).each do |cli_commmand|
+    describe "#{cli_commmand} can be accessed without absolute path" do
+      subject { bash("sudo -u #{node['cluster']['cluster_user']} -c '. ~/.bash_profile; #{cli_commmand} -h'") }
+      its('exit_status') { should eq 0 }
+    end
+  end
+
+  describe 'clusterstatusmgtd is configured to be executed by supervisord' do
+    subject { bash("#{node['cluster']['cookbook_virtualenv_path']}/bin/supervisorctl status clusterstatusmgtd") }
+    its('stdout') { should match /RUNNING/ }
+  end
+end
