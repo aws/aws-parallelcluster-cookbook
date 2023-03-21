bff1b31e97176881e270804f34e6dbf1014c263b
Tatiana Litvinova <litvit@amazon.com>
2023-03-21 20:56:56 +0100

    Move chrony tests from tests.rb recipe to InSpec
    Remove them from aws-parallelcluster-test cookbook, add missing tests to
    InSpec controls and add tag:config to control names in order to pick them
    during final instance validation.

--- test/recipes/controls/aws_parallelcluster_config/chrony_spec.rb
+++ test/recipes/controls/aws_parallelcluster_config/chrony_spec.rb
@@ -9,12 +9,12 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-control 'chrony_configured' do
+control 'tag:config_chrony_service_configured' do
   title 'Check that chrony is correctly configured'
 
   only_if { !os_properties.virtualized? }
 
-  chrony_service = os_properties.debian_family? ? 'chrony' : 'chronyd'
+  chrony_service = node['cluster']['chrony']['service']
 
   describe service(chrony_service) do
     it { should be_installed }
@@ -24,5 +24,13 @@ control 'chrony_configured' do
 
   describe bash("systemctl show #{chrony_service} | grep 'Restart=no'") do
     its('exit_status') { should eq(0) }
+  end
+
+  describe bash("journalctl -u #{chrony_service}") do
+    its('exit_status') { should eq 0 }
+  end
+
+  describe bash("sudo -u #{node['cluster']['cluster_user']} chronyc waitsync 30; chronyc tracking | grep -i reference | grep 169.254.169.123") do
+    its('exit_status') { should eq 0 }
   end
 end
