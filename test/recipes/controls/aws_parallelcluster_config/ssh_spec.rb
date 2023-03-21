6ea53065ad7bd590de9cd2210b72c9848536f44a
Tatiana Litvinova <litvit@amazon.com>
2023-03-21 18:33:08 +0100

    Move tests from test_openssh.rb recipe to InSpec
    Remove them from aws-parallelcluster-test cookbook, add missing tests to
    InSpec controls and add tag:config to control names in order to pick them
    during final instance validation.

--- test/recipes/controls/aws_parallelcluster_config/ssh_spec.rb
+++ test/recipes/controls/aws_parallelcluster_config/ssh_spec.rb
@@ -9,7 +9,19 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-control 'ssh_target_checker_contains_correct_vpc_cidr_list' do
+control 'ssh_target_checker_script_created' do
+  title 'Check that ssh_target_checker.sh is created correctly'
+
+  describe file('/usr/bin/ssh_target_checker.sh') do
+    it { should exist }
+    its('mode') { should cmp '0755' }
+    its('owner') { should eq 'root' }
+    its('group') { should eq 'root' }
+    its('content') { should match /VPC_CIDR_LIST=\(cidr1 cidr2\)/ }
+  end
+end
+
+control 'tag:config_ssh_target_checker_contains_correct_vpc_cidr_list' do
   ssh_target_checker_script = '/usr/bin/ssh_target_checker.sh'
 
   vpc_cidr_list = node['ec2']['network_interfaces_macs'][node['ec2']['mac']]['vpc_ipv4_cidr_blocks']
@@ -18,5 +30,11 @@ control 'ssh_target_checker_contains_correct_vpc_cidr_list' do
   describe file(ssh_target_checker_script) do
     it { should exist }
     its('content') { should match /VPC_CIDR_LIST=\(#{vpc_cidr_list.join(' ')}\)/ }
+  end
+end
+
+control 'tag:config_ssh_is_correctly_configured' do
+  describe file('/etc/ssh/ssh_config') do
+    its('content') { should match %r{Match exec "ssh_target_checker.sh %h"\n  StrictHostKeyChecking no\n  UserKnownHostsFile /dev/null} }
   end
 end
