3c8a0cee6e3f068826b67e1eb303817233e1b613
Tatiana Litvinova <litvit@amazon.com>
2023-03-21 18:59:10 +0100

    Move tests from test_nvidia.rb recipe to InSpec
    Remove them from aws-parallelcluster-test cookbook, add missing tests to
    InSpec controls and add tag:config to control names in order to pick them
    during final instance validation.

--- cookbooks/aws-parallelcluster-common/test/common/libraries/instance.rb
+++ cookbooks/aws-parallelcluster-common/test/common/libraries/instance.rb
@@ -16,7 +16,37 @@ class Instance < Inspec.resource(1)
     inspec.node['cluster']['node_type'] == 'ComputeFleet'
   end
 
+  def dcv_gpu_accel_supported?
+    unsupported_gpu_accel_list = ["g5g."]
+    !inspec.node['ec2']['instance_type'].start_with?(*unsupported_gpu_accel_list)
+  end
+
+  def efa_supported?
+    !inspec.os_properties.arm? || !node['cluster']['efa']['unsupported_aarch64_oses'].include?(node['cluster']['base_os'])
+  end
+
+  def imds_token
+    @imds_token = inspec.http('http://169.254.169.254/latest/api/token', method: 'PUT', headers: {
+      "X-aws-ec2-metadata-token-ttl-seconds": 900,
+    }).body unless @imds_token
+    @imds_token
+  end
+
+  def imds(what)
+    inspec.http("http://169.254.169.254/latest/#{what}", headers: {
+      "X-aws-ec2-metadata-token": imds_token,
+    }).body
+  end
+
   def graphic?
     !inspec.command("lspci | grep -i -o 'NVIDIA'").stdout.strip.empty?
+  end
+
+  def custom_ami?
+    inspec.node['cluster']['os'] && inspec.node['cluster']['os'].end_with?("-custom")
+  end
+
+  def nvidia_installed?
+    inspec.file('/usr/bin/nvidia-smi').exist?
   end
 end
