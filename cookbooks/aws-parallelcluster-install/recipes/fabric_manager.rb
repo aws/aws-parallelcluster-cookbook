922286bb56fe7e0227a307f759eed1aaa307eddd
Francesco Giordano <giordafr@amazon.it>
2023-03-23 14:38:09 +0100

    Transform Nvidia driver installation in a resource
    Signed-off-by: Francesco Giordano <giordafr@amazon.it>

--- cookbooks/aws-parallelcluster-install/recipes/fabric_manager.rb
+++ cookbooks/aws-parallelcluster-install/recipes/fabric_manager.rb
@@ -15,7 +15,6 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 return unless node['cluster']['nvidia']['enabled'] == 'yes' || node['cluster']['nvidia']['enabled'] == true
 
 # NVIDIA Fabric Manager not present on ARM
