9300753f949e4511244b572328a275e1ed950a96
Francesco Giordano <giordafr@amazon.it>
2023-03-24 15:55:04 +0100

    Change fabric_manager from recipe to a resource
    Signed-off-by: Francesco Giordano <giordafr@amazon.it>

---
+++ cookbooks/aws-parallelcluster-install/resources/fabric_manager/partial/_fabric_manager_common.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+#
+# Copyright:: 2013-2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file.
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+unified_mode true
+default_action :setup
+
+action :setup do
+  # NVIDIA Fabric Manager not present on ARM
+  return if arm_instance? || !(node['cluster']['nvidia']['enabled'] == 'yes' || node['cluster']['nvidia']['enabled'] == true)
+  action_install_package
+end
