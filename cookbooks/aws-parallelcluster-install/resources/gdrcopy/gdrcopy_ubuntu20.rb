33c311b6fe93cbe4053261625ef00bd6aa46bd37
Francesco Giordano <giordafr@amazon.it>
2023-03-28 09:06:06 +0200

    Change gdrcopy from a recipe to a resource

---
+++ cookbooks/aws-parallelcluster-install/resources/gdrcopy/gdrcopy_ubuntu20.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Copyright:: 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file.
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+provides :gdrcopy, platform: 'ubuntu', platform_version: '20.04'
+
+use 'partial/_gdrcopy_common.rb'
+use 'partial/_gdrcopy_common_debian.rb'
+
+action_class do
+  def platform
+    'Ubuntu20_04'
+  end
+end
