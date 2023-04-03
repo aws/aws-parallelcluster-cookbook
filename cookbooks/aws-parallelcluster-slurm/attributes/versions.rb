36dfaa3269913adf63bf6c3a9d0dab2bd03ab608
Jacopo De Amicis <jdamicis@amazon.it>
2023-04-03 17:18:16 +0200

    Upgrade munge to version 0.5.15
    Signed-off-by: Jacopo De Amicis <jdamicis@amazon.it>

--- cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
+++ cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
@@ -1,4 +1,4 @@
 # Slurm
 default['cluster']['slurm']['version'] = '23-02-1-1'
 # Munge
-default['cluster']['munge']['munge_version'] = '0.5.14'
+default['cluster']['munge']['munge_version'] = '0.5.15'
