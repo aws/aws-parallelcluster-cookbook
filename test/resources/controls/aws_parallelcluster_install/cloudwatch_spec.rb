46a5d748432c10229bf806d1c978c19eea2c5708
Francesco Giordano <giordafr@amazon.it>
2023-04-07 16:20:26 +0200

    Remove verify_cloudwatch_agent_public_key_fingerprint.py in favour of a simpler command
    Signed-off-by: Francesco Giordano <giordafr@amazon.it>

--- test/resources/controls/aws_parallelcluster_install/cloudwatch_spec.rb
+++ test/resources/controls/aws_parallelcluster_install/cloudwatch_spec.rb
@@ -1,10 +1,6 @@
 control 'cloudwatch_installation_files' do
   title "Check cloudwatch installation files"
 
-  describe file("/usr/local/bin/verify_cloudwatch_agent_public_key_fingerprint.py") do
-    it { should exist }
-  end
-
   package_extension = os_properties.debian_family? ? 'deb' : 'rpm'
 
   signature_path = "#{node['cluster']['sources_dir']}/amazon-cloudwatch-agent.#{package_extension}.sig"
@@ -20,7 +16,8 @@ control 'cloudwatch_installation_files' do
   describe 'Check the presence of the cloudwatch package gpg key'
   # In Ubuntu 20.04 due to environment variable the keyring is placed under home of the user ubuntu with the permission of root
   keyring = os_properties.ubuntu2004? && !os_properties.virtualized? ? '--keyring /home/ubuntu/.gnupg/pubring.kbx' : ''
-  describe bash("sudo gpg --list-keys #{keyring}") do
+  sudo = os_properties.redhat_ubi? ? '' : 'sudo'
+  describe bash("#{sudo} gpg --list-keys #{keyring}") do
     its('exit_status') { should eq 0 }
     its('stdout') { should match /3B789C72/ }
     its('stdout') { should match /Amazon CloudWatch Agent/ }
