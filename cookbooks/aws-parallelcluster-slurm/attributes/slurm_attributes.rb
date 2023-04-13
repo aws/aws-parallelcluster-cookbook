e17dd2471f8e16f07cc3b6075174572a13cad48d
Jacopo De Amicis <35738823+jdeamicis@users.noreply.github.com>
2023-04-13 16:12:33 +0200

    Upgrade Slurm to version 23.02.1 and munge to version 0.5.15 (#1997)
    Signed-off-by: Jacopo De Amicis <jdamicis@amazon.it>

--- cookbooks/aws-parallelcluster-slurm/attributes/slurm_attributes.rb
+++ cookbooks/aws-parallelcluster-slurm/attributes/slurm_attributes.rb
@@ -1,4 +1,4 @@
 # Slurm attributes shared between install_slurm and configure_slurm_accounting
 default['cluster']['slurm']['commit'] = ''
-default['cluster']['slurm']['sha256'] = '8c8f6a26a5d51e6c63773f2e02653eb724540ee8b360125c8d7732314ce737d6'
+default['cluster']['slurm']['sha256'] = 'd827553496ee9158bbf6a862b563cfd48566e6d815ad2f8349950fe6f04934da'
 default['cluster']['slurm']['install_dir'] = '/opt/slurm'
