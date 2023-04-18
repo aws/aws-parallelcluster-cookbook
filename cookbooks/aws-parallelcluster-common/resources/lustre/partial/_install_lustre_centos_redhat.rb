941a683be50473d7539e91a7119e927fb4a89895
Francesco Giordano <giordafr@amazon.it>
2023-04-18 17:01:13 +0200

    Fix lustre client for RedHat 8.7
    Signed-off-by: Francesco Giordano <giordafr@amazon.it>

--- cookbooks/aws-parallelcluster-common/resources/lustre/partial/_install_lustre_centos_redhat.rb
+++ cookbooks/aws-parallelcluster-common/resources/lustre/partial/_install_lustre_centos_redhat.rb
@@ -35,3 +35,10 @@ action :install_lustre do
 
   kernel_module 'lnet' unless redhat_ubi?
 end
+
+def find_kernel_patch_version
+  # kernel release is in the form 3.10.0-1127.8.2.el7.x86_64
+  kernel_patch_version = node['cluster']['kernel_release'].match(/^\d+\.\d+\.\d+-(\d+)\..*$/)
+  raise "Unable to retrieve the kernel patch version from #{node['cluster']['kernel_release']}." unless kernel_patch_version
+  kernel_patch_version[1]
+end
