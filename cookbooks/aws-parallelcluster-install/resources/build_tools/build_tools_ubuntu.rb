743c9e0bbaa308abff39cde38c564eb172022f52
Tatiana Litvinova <litvit@amazon.com>
2023-04-25 03:50:38 +0200

    Create build_tools resource installing prerequisites to run make command.

---
+++ cookbooks/aws-parallelcluster-install/resources/build_tools/build_tools_ubuntu.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Copyright:: 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file.
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+provides :build_tools, platform: 'ubuntu'
+
+unified_mode true
+default_action :setup
+
+action :setup do
+  package %w(build-essential) do
+    retries 10
+    retry_delay 5
+  end
+end
