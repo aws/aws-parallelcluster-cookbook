02112055e672dff6b15b7cdfc96e74d3bb5dade9
Tatiana Litvinova <litvit@amazon.com>
2023-05-06 20:26:47 +0200

    Allow running Kitchen tests for a specific cookbook

--- kitchen.docker.sh
+++ kitchen.docker.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-export KITCHEN_LOCAL_YAML="kitchen.$1.yml"; shift;
-export KITCHEN_YAML=kitchen.docker.yml
-export KITCHEN_GLOBAL_YAML=kitchen.global.yml
+source kitchen.local-yml.sh
+
+THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export KITCHEN_YAML="${THIS_DIR}/kitchen.docker.yml"
+export KITCHEN_GLOBAL_YAML="${THIS_DIR}/kitchen.global.yml"
 export KITCHEN_DRIVER=dokken
 
-kitchen "$@"
+echo "** KITCHEN_LOCAL_YAML: $KITCHEN_LOCAL_YAML"
+echo "** KITCHEN_YAML: $KITCHEN_YAML"
+echo "** KITCHEN_GLOBAL_YAML: $KITCHEN_GLOBAL_YAML"
+echo "kitchen $*"
+
+cd "${KITCHEN_COOKBOOK_PATH}" && kitchen "$@"
