518775173552c6654870fc90aae79ef540241f17
Tatiana Litvinova <litvit@amazon.com>
2023-05-07 20:01:48 +0200

    Move IntelMPI-related attributes to aws-parallelcluster-install

--- cookbooks/aws-parallelcluster-install/attributes/default.rb
+++ cookbooks/aws-parallelcluster-install/attributes/default.rb
@@ -4,3 +4,10 @@
 default['cluster']['sysctl']['ipv4']['gc_thresh1'] = 0
 default['cluster']['sysctl']['ipv4']['gc_thresh2'] = 15_360
 default['cluster']['sysctl']['ipv4']['gc_thresh3'] = 16_384
+
+# Intel MPI
+default['conditions']['intel_mpi_supported'] = !arm_instance?
+default['cluster']['intelmpi']['version'] = '2021.9.0'
+default['cluster']['intelmpi']['full_version'] = "#{node['cluster']['intelmpi']['version']}.43482"
+default['cluster']['intelmpi']['modulefile'] = "/opt/intel/mpi/#{node['cluster']['intelmpi']['version']}/modulefiles/mpi"
+default['cluster']['intelmpi']['qt_version'] = '6.4.2'
