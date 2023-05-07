150a221076504c9dc04b47eeb9072834fe649860
Tatiana Litvinova <litvit@amazon.com>
2023-05-07 02:32:06 +0200

    Move disable_services from aws-parallelcluster-install to aws-parallelcluster-platform
    This PR enables removal of Log4jPatcher step from Test phase of image build.
    Add InSpec tests for recipes and platform to AMI validation

--- cookbooks/aws-parallelcluster-platform/test/controls/disable_services_spec.rb
+++ cookbooks/aws-parallelcluster-platform/test/controls/disable_services_spec.rb
@@ -9,7 +9,7 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-control 'tag:config_services_disabled_on_debian_family' do
+control 'tag:testami_tag:config_services_disabled_on_debian_family' do
   title 'Test that DLAMI multi eni helper is disabled and masked on debian family'
 
   only_if { os_properties.debian_family? && !os_properties.virtualized? }
@@ -25,7 +25,7 @@ control 'tag:config_services_disabled_on_debian_family' do
   end
 end
 
-control 'tag:config_services_disabled_on_amazon_family' do
+control 'tag:testami_tag:config_services_disabled_on_amazon_family' do
   title 'Test that log4j-cve-2021-44228-hotpatch is disabled and masked on amazon family'
 
   only_if { os_properties.amazon_family? && !os_properties.virtualized? }
@@ -38,5 +38,10 @@ control 'tag:config_services_disabled_on_amazon_family' do
   describe bash('systemctl list-unit-files --state=masked --no-legend') do
     its(:exit_status) { should eq 0 }
     its(:stdout) { should match /log4j-cve-2021-44228-hotpatch.service\s*masked/ }
+  end
+
+  describe bash('systemctl show -p LoadState log4j-cve-2021-44228-hotpatch') do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match /LoadState=masked/ }
   end
 end
