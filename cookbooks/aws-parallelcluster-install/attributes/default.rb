b3416eb9fa4d7eba7d02a86c8e0d91cb337e0612
Tatiana Litvinova <litvit@amazon.com>
2023-05-07 20:06:29 +0200

    Move stunnel-related attributes to aws-parallelcluster-install

--- cookbooks/aws-parallelcluster-install/attributes/default.rb
+++ cookbooks/aws-parallelcluster-install/attributes/default.rb
@@ -11,3 +11,9 @@ default['cluster']['intelmpi']['version'] = '2021.9.0'
 default['cluster']['intelmpi']['full_version'] = "#{node['cluster']['intelmpi']['version']}.43482"
 default['cluster']['intelmpi']['modulefile'] = "/opt/intel/mpi/#{node['cluster']['intelmpi']['version']}/modulefiles/mpi"
 default['cluster']['intelmpi']['qt_version'] = '6.4.2'
+
+# stunnel
+default['cluster']['stunnel']['version'] = '5.67'
+default['cluster']['stunnel']['url'] = lazy { "#{node['cluster']['artifacts_s3_url']}/stunnel/stunnel-#{node['cluster']['stunnel']['version']}.tar.gz" }
+default['cluster']['stunnel']['sha256'] = '3086939ee6407516c59b0ba3fbf555338f9d52f459bcab6337c0f00e91ea8456'
+default['cluster']['stunnel']['tarball_path'] = "#{node['cluster']['sources_dir']}/stunnel-#{node['cluster']['stunnel']['version']}.tar.gz"
