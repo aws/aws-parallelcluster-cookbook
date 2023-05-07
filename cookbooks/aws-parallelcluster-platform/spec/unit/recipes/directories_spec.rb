ea789d9f81650fe002db78345e541f74535bdec5
Tatiana Litvinova <litvit@amazon.com>
2023-05-07 21:16:04 +0200

    Move directories from aws-parallelcluster-install to aws-parallelcluster-platform

---
+++ cookbooks/aws-parallelcluster-platform/spec/unit/recipes/directories_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe 'aws-parallelcluster-platform::directories' do
+  for_all_oses do |platform, version|
+    context "on #{platform}#{version}" do
+      cached(:chef_run) do
+        ChefSpec::Runner.new(platform: platform, version: version).converge(described_recipe)
+      end
+      cached(:node) { chef_run.node }
+
+      it 'creates parallelcluster directory' do
+        is_expected.to create_directory('/etc/parallelcluster')
+      end
+
+      it 'creates base directory' do
+        is_expected.to create_directory(node['cluster']['base_dir'])
+      end
+
+      it 'creates sources directory' do
+        is_expected.to create_directory(node['cluster']['sources_dir'])
+      end
+
+      it 'creates scripts directory' do
+        is_expected.to create_directory(node['cluster']['scripts_dir'])
+      end
+
+      it 'creates license directory' do
+        is_expected.to create_directory(node['cluster']['license_dir'])
+      end
+
+      it 'creates config directory' do
+        is_expected.to create_directory(node['cluster']['configs_dir'])
+      end
+
+      it 'creates shared directory' do
+        is_expected.to create_directory(node['cluster']['shared_dir'])
+      end
+
+      it 'creates log directory' do
+        is_expected.to create_directory('/var/log/parallelcluster/').with(
+          owner: 'root',
+          mode: '1777',
+          recursive: true
+        )
+      end
+    end
+  end
+end
---
+++ cookbooks/aws-parallelcluster-shared/attributes/directories.rb
@@ -0,0 +1,6 @@
+default['cluster']['base_dir'] = '/opt/parallelcluster'
+default['cluster']['sources_dir'] = "#{node['cluster']['base_dir']}/sources"
+default['cluster']['scripts_dir'] = "#{node['cluster']['base_dir']}/scripts"
+default['cluster']['license_dir'] = "#{node['cluster']['base_dir']}/licenses"
+default['cluster']['configs_dir'] = "#{node['cluster']['base_dir']}/configs"
+default['cluster']['shared_dir'] = "#{node['cluster']['base_dir']}/shared"
