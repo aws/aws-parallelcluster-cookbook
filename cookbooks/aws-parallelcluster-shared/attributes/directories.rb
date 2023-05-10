9e0bc4a4bb5a77d11a290b23c137f03fed51704a
Tatiana Litvinova <litvit@amazon.com>
2023-05-10 14:48:59 +0200

    Move python attributes to shared cookbook

--- cookbooks/aws-parallelcluster-shared/attributes/directories.rb
+++ cookbooks/aws-parallelcluster-shared/attributes/directories.rb
@@ -4,3 +4,6 @@ default['cluster']['scripts_dir'] = "#{node['cluster']['base_dir']}/scripts"
 default['cluster']['license_dir'] = "#{node['cluster']['base_dir']}/licenses"
 default['cluster']['configs_dir'] = "#{node['cluster']['base_dir']}/configs"
 default['cluster']['shared_dir'] = "#{node['cluster']['base_dir']}/shared"
+
+# plcuster-specific pyenv system installation root
+default['cluster']['system_pyenv_root'] = "#{node['cluster']['base_dir']}/pyenv"
