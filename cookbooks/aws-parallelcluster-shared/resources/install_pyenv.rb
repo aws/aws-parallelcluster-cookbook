c75fd2da7c013f7ff1cb36c0b36c303fb6ca2b91
Tatiana Litvinova <litvit@amazon.com>
2023-05-10 14:41:44 +0200

    Remove attribute set from install_pyenv
    Since install_pyenv can be called with arbitrary property values, setting node attributes from those values is wrong.
    Remove fallback to default values as well: node attributes must exist.

--- cookbooks/aws-parallelcluster-shared/resources/install_pyenv.rb
+++ cookbooks/aws-parallelcluster-shared/resources/install_pyenv.rb
@@ -13,7 +13,7 @@ property :user, String
 default_action :run
 
 action :run do
-  python_version = new_resource.python_version || node['cluster']['python-version'] || '3.9.16'
+  python_version = new_resource.python_version || node['cluster']['python-version']
 
   if new_resource.user_only
     raise "user property is required for resource install_pyenv when user_only is set to true" unless new_resource.user
@@ -23,7 +23,7 @@ action :run do
       user_prefix new_resource.prefix if new_resource.prefix
     end
   else
-    prefix = new_resource.prefix || node['cluster']['system_pyenv_root'] || "#{node['cluster']['base_dir']}/pyenv"
+    prefix = new_resource.prefix || node['cluster']['system_pyenv_root']
 
     directory prefix do
       recursive true
@@ -49,9 +49,4 @@ action :run do
     git_url 'https://github.com/pyenv/pyenv-virtualenv'
     user new_resource.user if new_resource.user_only
   end
-
-  node.default['cluster']['system_pyenv_root'] = prefix
-  node.default['cluster']['python-version'] = python_version
-
-  node_attributes "dump node attributes"
 end
