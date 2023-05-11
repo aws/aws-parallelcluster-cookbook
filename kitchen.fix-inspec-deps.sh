d5da296ef6b42cf1892c8b9ffac234fa3b9ca097
Tatiana Litvinova <litvit@amazon.com>
2023-05-12 00:40:44 +0200

    Handle Kitchen dependencies
    Kitchen resolves relative dependencies based on the directory from which it was launched. Therefore, dependencies that are resolved correctly when launched from a sub-cookbook will not work when launched from the main cookbook.
    
    To avoid changing the codebase, dependencies are copied to a temporary folder, where the paths are adjusted based on the launching directory.

---
+++ kitchen.fix-inspec-deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "*** Fix relative paths in dependent cookbooks"
+rm -rf /tmp/cookbooks
+mkdir -p /tmp/cookbooks/aws-parallelcluster-common/test
+mkdir -p /tmp/cookbooks/aws-parallelcluster-platform
+mkdir -p /tmp/cookbooks/aws-parallelcluster-environment
+mkdir -p /tmp/cookbooks/aws-parallelcluster-computefleet
+cp -r cookbooks/aws-parallelcluster-common/test/common /tmp/cookbooks/aws-parallelcluster-common/test
+cp -r cookbooks/aws-parallelcluster-platform/test /tmp/cookbooks/aws-parallelcluster-platform
+cp -r cookbooks/aws-parallelcluster-environment/test /tmp/cookbooks/aws-parallelcluster-environment
+cp -r cookbooks/aws-parallelcluster-computefleet/test /tmp/cookbooks/aws-parallelcluster-computefleet
+sed -i.bak "s#path: ../aws-parallelcluster#path: /tmp/cookbooks/aws-parallelcluster#g" /tmp/cookbooks/aws-parallelcluster-*/test/inspec.yml
