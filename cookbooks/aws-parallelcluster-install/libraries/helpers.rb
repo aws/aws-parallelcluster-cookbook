743991b576befd653a6fd6677a1ca0e0c6e8ad73
Francesco Giordano <giordafr@amazon.it>
2023-05-12 18:48:18 +0200

    Transform remove_package_repository into an action of package_repos
    Signed-off-by: Francesco Giordano <giordafr@amazon.it>

--- cookbooks/aws-parallelcluster-install/libraries/helpers.rb
+++ cookbooks/aws-parallelcluster-install/libraries/helpers.rb
@@ -70,21 +70,3 @@ def append_if_not_present_grub_cmdline(attributes, grub_variable)
     end
   end
 end
-
-# Remove an external package repository from the OS's package manager
-# NOTE: This helper function defines a Chef resource function to be executed at Converge time
-def remove_package_repository(repo_name)
-  case node['platform_family']
-  when 'rhel', 'amazon'
-    yum_repository repo_name do
-      action :remove
-    end
-  when 'debian'
-    apt_repository repo_name do
-      action :remove
-    end
-    apt_update
-  else
-    raise "platform not supported: #{node['platform_family']}"
-  end
-end
