a1b02537e6098210491e574ff1a1da398ebff85b
Tatiana Litvinova <litvit@amazon.com>
2023-05-15 08:03:43 +0200

    Move gc_thresh_values from install to platform cookbook

--- cookbooks/aws-parallelcluster-install/attributes/default.rb
+++ cookbooks/aws-parallelcluster-install/attributes/default.rb
@@ -1,10 +1,5 @@
 # aws-parallelcluster-install attributes
 
-# Default gc_thresh values for performance at scale
-default['cluster']['sysctl']['ipv4']['gc_thresh1'] = 0
-default['cluster']['sysctl']['ipv4']['gc_thresh2'] = 15_360
-default['cluster']['sysctl']['ipv4']['gc_thresh3'] = 16_384
-
 # Intel MPI
 default['conditions']['intel_mpi_supported'] = !arm_instance?
 default['cluster']['intelmpi']['version'] = '2021.9.0'
--- cookbooks/aws-parallelcluster-platform/attributes/platform.rb
+++ cookbooks/aws-parallelcluster-platform/attributes/platform.rb
@@ -1,2 +1,7 @@
 # ulimit settings
 default['cluster']['filehandle_limit'] = 10_000
+
+# Default gc_thresh values for performance at scale
+default['cluster']['sysctl']['ipv4']['gc_thresh1'] = 0
+default['cluster']['sysctl']['ipv4']['gc_thresh2'] = 15_360
+default['cluster']['sysctl']['ipv4']['gc_thresh3'] = 16_384
