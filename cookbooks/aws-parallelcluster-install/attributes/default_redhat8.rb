6162dbb7dd40b0c32c2d8a6ab97c9dced39a61db
eantonin <83351137+eantonin@users.noreply.github.com>
2023-05-15 17:54:03 +0200

    [3.x] Removal of tests.rb (#2122)
    * Move clusterstatusmgtd test to other_spec.rb
    * Move awscli test to other_spec.rb
    * Move systemd default settings test to other_spec.rb
    * Remove scheduler plugin test from tests.rb as it's deprecated
    * Remove aws-parallelcluster-cookbook/tests.rb
    * Move SSH headnode test to other_spec.rb
    * Move Bridge network interface test to other_spec.rb
    * Move jq test to other_spec.rb
    * Delete tests.rb file
    * Execute awsbatch spec sourcing the correct file
    * Remove awscli test from here as added in https://github.com/aws/aws-parallelcluster-cookbook/pull/2129
    * Adding jq as a package to be installed in all oses (was already in alinux2) this is needed for docker tests to pass

--- cookbooks/aws-parallelcluster-install/attributes/default_redhat8.rb
+++ cookbooks/aws-parallelcluster-install/attributes/default_redhat8.rb
@@ -8,7 +8,7 @@ return unless platform?('redhat') && node['platform_version'].to_i == 8
 default['cluster']['base_packages'] = %w(vim ksh tcsh zsh openssl-devel ncurses-devel pam-devel net-tools openmotif-devel
                                              libXmu-devel hwloc-devel libdb-devel tcl-devel automake autoconf pyparted libtool
                                              httpd boost-devel redhat-lsb mlocate R atlas-devel
-                                             blas-devel libffi-devel dkms libedit-devel
+                                             blas-devel libffi-devel dkms libedit-devel jq
                                              libical-devel postgresql-devel postgresql-server sendmail libxml2-devel libglvnd-devel
                                              mdadm python2 python2-pip libgcrypt-devel libevent-devel glibc-static bind-utils
                                              iproute NetworkManager-config-routing-rules python3 python3-pip iptables libcurl-devel yum-plugin-versionlock
--- test/recipes/controls/other_spec.rb
+++ test/recipes/controls/other_spec.rb
@@ -18,6 +18,15 @@ control 'tag:config_enough_space_on_root_volume' do
   end
 end
 
+control 'tag:config_clusterstatusmgtd_is_running' do
+  only_if { instance.head_node? && node['cluster']['scheduler'] != 'awsbatch' && !os_properties.on_docker? }
+
+  describe 'clusterstatusmgtd is configured to be executed by supervisord' do
+    subject { bash("#{node['cluster']['cookbook_virtualenv_path']}/bin/supervisorctl status clusterstatusmgtd | grep RUNNING") }
+    its('exit_status') { should eq 0 }
+  end
+end
+
 control 'tag:config_no_mpich_packages' do
   describe bash('ls 2>/dev/null /usr/lib64/mpich*') do
     its('stdout') { should be_empty }
@@ -25,6 +34,28 @@ control 'tag:config_no_mpich_packages' do
 
   describe bash('ls 2>/dev/null /usr/lib/mpich*') do
     its('stdout') { should be_empty }
+  end
+end
+
+control 'tag:config_check_non_graphical_systemd_settings' do
+  only_if do
+    (
+      !instance.head_node? ||
+      !node['conditions']['dcv_supported'] ||
+      node['cluster']['dcv_enabled'] != "head_node"
+    ) && node['conditions']['ami_bootstrapped']
+  end
+  describe 'check systemd default runlevel' do
+    subject { bash("systemctl get-default | grep -i multi-user.target") }
+    its('exit_status') { should eq 0 }
+  end
+
+  if os_properties.ubuntu1804? || os_properties.alinux2?
+    describe service('gdm') do
+      it { should be_installed }
+      it { should be_enabled }
+      it { should_not be_running }
+    end
   end
 end
 
@@ -37,5 +68,31 @@ control 'tag:config_no_fftw_packages' do
 
   describe bash('ls 2>/dev/null /usr/lib/libfftw*') do
     its('stdout') { should be_empty }
+  end
+end
+
+control 'tag:config_jq_version_new_enough' do
+  only_if { !instance.custom_ami? && !os_properties.redhat_ubi? }
+
+  describe bash("jq --argfile") do
+    its('stderr') { should match /jq: --argfile takes two parameters/ }
+  end
+end
+
+control 'tag:config_ssh_localhost' do
+  only_if { instance.head_node? && !os_properties.on_docker? }
+
+  hostname = bash("hostname").stdout
+
+  describe bash("su #{node['cluster']['cluster_user']} -c 'ssh localhost hostname'") do
+    its('stdout') { should eq hostname }
+  end
+end
+
+control 'tag:config_bridge_network_interface_presence' do
+  only_if { os_properties.centos? }
+
+  describe bash("[ $(brctl show | awk 'FNR == 2 {print $1}') ] && exit 1 || exit 0") do
+    its('exit_status') { should eq 0 }
   end
 end
