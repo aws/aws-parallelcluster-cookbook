97041ea75636551b3cc3afc0072870d786c832bf
Enrico Usai <usai@amazon.com>
2023-05-23 11:38:20 +0200

    Automate startup of Colima when running docker
    If Colima software is available, start it and export DOCKER_HOST variable
    
    ### Tests
    
    Tested with:
    * colima stopped
    * colima running
    * with another command in place of colima (to simulate colima not found case)
    
    ### References
    
    * https://github.com/abiosoft/colima
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- kitchen.docker.sh
+++ kitchen.docker.sh
@@ -7,6 +7,12 @@ export KITCHEN_YAML="${THIS_DIR}/kitchen.docker.yml"
 export KITCHEN_GLOBAL_YAML="${THIS_DIR}/kitchen.global.yml"
 export KITCHEN_DRIVER=dokken
 
+# Export DOCKER_HOST variable when using Colima
+if [[ $(command -v colima) ]]; then
+  colima status || colima start
+  export DOCKER_HOST=unix://${HOME}/.colima/default/docker.sock
+fi
+
 echo "** KITCHEN_LOCAL_YAML: $KITCHEN_LOCAL_YAML"
 echo "** KITCHEN_YAML: $KITCHEN_YAML"
 echo "** KITCHEN_GLOBAL_YAML: $KITCHEN_GLOBAL_YAML"
