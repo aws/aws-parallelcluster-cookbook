d8a9713a8ad2bd77bfaf1b1629048e6b7acc5047
Ryan Anderson <ndry@amazon.com>
2023-05-24 12:32:00 -0400

    Update the NFS resource for Ubuntu 22
    Split the ubuntu NFS resource into three, one for each version of Ubuntu we
    support, 18, 20, and 22.  Since the difference is with 20 and 18 will soon be removed.
    Ubuntu 22 uses a new config file for NFS see https://ubuntu.com/server/docs/service-nfs
    for details.  Upgrading the nfs third party resource to 5.0.1+ resulted in failures similar
    to the known issue https://github.com/sous-chefs/nfs/issues/93.  Setting the server
    template in the :setup action will preserve the default nfs.conf file and allow
    for us to override any values we need without removing the history of the config
    for users.  Also created more useful names for the two different types of
    config files used for NFS.

--- cookbooks/aws-parallelcluster-environment/resources/nfs/nfs_ubuntu22+.rb
+++ cookbooks/aws-parallelcluster-environment/resources/nfs/nfs_ubuntu22+.rb
@@ -13,7 +13,9 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-provides :nfs, platform: 'ubuntu'
+provides :nfs, platform: 'ubuntu' do |node|
+  node['platform_version'].to_i >= 22
+end
 
 unified_mode true
 
@@ -27,12 +29,13 @@ action :setup do
   action_install_nfs
   action_install_nfs4
   action_disable_start_at_boot
+  node.default['nfs']['config']['server_template'] = '/etc/nfs.conf.d/parallelcluster-nfs.conf'
 end
 
 action_class do
   def override_server_template
     edit_resource(:template, node['nfs']['config']['server_template']) do
-      source 'nfs/nfs.conf.erb'
+      source 'nfs/nfs-ubuntu22+.conf.erb'
       cookbook 'aws-parallelcluster-environment'
     end
   end
