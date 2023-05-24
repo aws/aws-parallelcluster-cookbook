1ffb4df26bbcee2bb415adc59328088a2152a0d1
Enrico Usai <usai@amazon.com>
2023-05-24 18:25:16 +0200

    Move efa resource to environment cookbook

--- cookbooks/aws-parallelcluster-environment/resources/efa/efa_redhat8.rb
+++ cookbooks/aws-parallelcluster-environment/resources/efa/efa_redhat8.rb
@@ -1,7 +1,5 @@
-
 # frozen_string_literal: true
 
-#
 # Copyright:: 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License").
@@ -12,27 +10,37 @@
 #
 # or in the "LICENSE.txt" file accompanying this file.
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
-# See the License for the specific language governing permissions and limitations under the License
-provides :efa, platform: 'centos' do |node|
-  node['platform_version'].to_i == 7
+# See the License for the specific language governing permissions and limitations under the License.
+
+provides :efa, platform: 'redhat' do |node|
+  node['platform_version'].to_i == 8
 end
 unified_mode true
 default_action :setup
 
-use 'partial/_setup'
-
-action :configure do
-  # do nothing
-end
+use 'partial/_common'
 
 action_class do
+  def efa_supported?
+    if node['platform_version'].to_f < 8.4
+      log "EFA is not supported in this RHEL version #{node['platform_version']}, supported versions are >= 8.4" do
+        level :warn
+      end
+      false
+    else
+      true
+    end
+  end
+
   def conflicting_packages
     %w(openmpi-devel openmpi)
   end
-end
 
-action_class do
   def prerequisites
-    %w(environment-modules)
+    if redhat_ubi?
+      %w(environment-modules)
+    else
+      %w(environment-modules libibverbs-utils librdmacm-utils rdma-core-devel)
+    end
   end
 end
--- test/resources/controls/aws_parallelcluster_config/efa_spec.rb
+++
@@ -1,9 +0,0 @@
-control 'efa_debian_system_settings_configured' do
-  title 'Check debian system is correctly configured for EFA'
-
-  only_if { os.debian? && !os_properties.virtualized? }
-
-  describe kernel_parameter('kernel.yama.ptrace_scope') do
-    its('value') { should eq 0 }
-  end
-end
