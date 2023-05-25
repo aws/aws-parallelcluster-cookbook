f54cb67166fd256ab8ccc6afe23fcc9da0055fbe
Tatiana Litvinova <litvit@amazon.com>
2023-05-25 20:32:03 +0200

    Replace virtualized with on_docker

--- cookbooks/aws-parallelcluster-environment/test/controls/cloudwatch_spec.rb
+++ cookbooks/aws-parallelcluster-environment/test/controls/cloudwatch_spec.rb
@@ -16,7 +16,7 @@ control 'tag:install_cloudwatch_installation_files' do
   describe 'Check the presence of the cloudwatch package gpg key'
   # In Ubuntu 20.04 due to environment variable the keyring is placed under home of the user ubuntu with the permission of root
   ubuntu2004 = os_properties.ubuntu2004?
-  keyring = os_properties.ubuntu2004? && !os_properties.virtualized? ? '--keyring /home/ubuntu/.gnupg/pubring.kbx' : ''
+  keyring = os_properties.ubuntu2004? && !os_properties.on_docker? ? '--keyring /home/ubuntu/.gnupg/pubring.kbx' : ''
   sudo = os_properties.redhat_ubi? ? '' : 'sudo'
   describe bash("#{sudo} gpg --list-keys #{keyring}") do
     # Don't check exit status for Ubuntu20 because it returns 2 when executed in the validate phase of a created AMI
