8949ec7772161638fc8d2fb81331473d81d1b2f2
Tatiana Litvinova <litvit@amazon.com>
2023-05-27 23:14:02 +0200

    Move jwt_dependencies tests to slurm cookbook
    * bash kitchen.ec2.sh slurm-install verify jwt-dependencies -c 6: OK
    * bash kitchen.docker.sh slurm-install verify jwt-dependencies -c 6: OK

--- cookbooks/aws-parallelcluster-slurm/test/controls/jwt_dependencies_spec.rb
+++ cookbooks/aws-parallelcluster-slurm/test/controls/jwt_dependencies_spec.rb
@@ -9,7 +9,7 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-control 'jwt_dependencies_installed' do
+control 'tag:install_jwt_dependencies_installed' do
   title "JWT dependencies are installed"
 
   packages = []
