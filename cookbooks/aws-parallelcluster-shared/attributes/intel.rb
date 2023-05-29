600e0cd15d82463459ea94057b2884d3953549dd
Enrico Usai <usai@amazon.com>
2023-05-29 15:17:22 +0200

    Move Intel attributes from main cookbook to aws-parallelcluster-shared
    This is a temporary change to make these attributes available to both code and inspec tests.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-shared/attributes/intel.rb
+++ cookbooks/aws-parallelcluster-shared/attributes/intel.rb
@@ -1,1 +1,24 @@
+# Intel Packages
+default['cluster']['psxe']['version'] = '2020.4-17'
+default['cluster']['psxe']['noarch_packages'] = %w(intel-tbb-common-runtime intel-mkl-common-runtime intel-psxe-common-runtime
+                                                   intel-ipp-common-runtime intel-ifort-common-runtime intel-icc-common-runtime
+                                                   intel-daal-common-runtime intel-comp-common-runtime)
+default['cluster']['psxe']['archful_packages']['i486'] = %w(intel-tbb-runtime intel-tbb-libs-runtime intel-comp-runtime
+                                                            intel-daal-runtime intel-icc-runtime intel-ifort-runtime
+                                                            intel-ipp-runtime intel-mkl-runtime intel-openmp-runtime)
+default['cluster']['psxe']['archful_packages']['x86_64'] = node['cluster']['psxe']['archful_packages']['i486'] + %w(intel-mpi-runtime)
+
+default['cluster']['intelhpc']['platform_name'] = value_for_platform(
+  'centos' => {
+    '~>7' => 'el7',
+  }
+)
 default['cluster']['intelhpc']['dependencies'] = %w(compat-libstdc++-33 nscd nss-pam-ldapd openssl098e)
+
+default['cluster']['intelhpc']['packages'] = %w(intel-hpc-platform-core-intel-runtime-advisory intel-hpc-platform-compat-hpc-advisory
+                                                intel-hpc-platform-core intel-hpc-platform-core-advisory intel-hpc-platform-hpc-cluster
+                                                intel-hpc-platform-compat-hpc intel-hpc-platform-core-intel-runtime)
+default['cluster']['intelhpc']['version'] = '2018.0-7'
+
+default['cluster']['intelpython2']['version'] = '2019.4-088'
+default['cluster']['intelpython3']['version'] = '2020.2-902'
