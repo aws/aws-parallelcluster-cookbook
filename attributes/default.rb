f1bae97d44b594e4fb85898b64c5cb49a2870b53
Enrico Usai <usai@amazon.com>
2023-05-29 15:17:22 +0200

    Move Intel attributes from main cookbook to aws-parallelcluster-shared
    This is a temporary change to make these attributes available to both code and inspec tests.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -29,27 +29,6 @@ default['cluster']['cluster_config_version'] = nil
 default['cluster']['change_set_s3_key'] = nil
 default['cluster']['instance_types_data_s3_key'] = nil
 
-# Intel Packages
-default['cluster']['psxe']['version'] = '2020.4-17'
-default['cluster']['psxe']['noarch_packages'] = %w(intel-tbb-common-runtime intel-mkl-common-runtime intel-psxe-common-runtime
-                                                   intel-ipp-common-runtime intel-ifort-common-runtime intel-icc-common-runtime
-                                                   intel-daal-common-runtime intel-comp-common-runtime)
-default['cluster']['psxe']['archful_packages']['i486'] = %w(intel-tbb-runtime intel-tbb-libs-runtime intel-comp-runtime
-                                                            intel-daal-runtime intel-icc-runtime intel-ifort-runtime
-                                                            intel-ipp-runtime intel-mkl-runtime intel-openmp-runtime)
-default['cluster']['psxe']['archful_packages']['x86_64'] = node['cluster']['psxe']['archful_packages']['i486'] + %w(intel-mpi-runtime)
-default['cluster']['intelhpc']['platform_name'] = value_for_platform(
-  'centos' => {
-    '~>7' => 'el7',
-  }
-)
-default['cluster']['intelhpc']['packages'] = %w(intel-hpc-platform-core-intel-runtime-advisory intel-hpc-platform-compat-hpc-advisory
-                                                intel-hpc-platform-core intel-hpc-platform-core-advisory intel-hpc-platform-hpc-cluster
-                                                intel-hpc-platform-compat-hpc intel-hpc-platform-core-intel-runtime)
-default['cluster']['intelhpc']['version'] = '2018.0-7'
-default['cluster']['intelpython2']['version'] = '2019.4-088'
-default['cluster']['intelpython3']['version'] = '2020.2-902'
-
 # URLs to software packages used during install recipes
 default['cluster']['slurm_plugin_dir'] = '/etc/parallelcluster/slurm_plugin'
 default['cluster']['slurm']['fleet_config_path'] = "#{node['cluster']['slurm_plugin_dir']}/fleet-config.json"
