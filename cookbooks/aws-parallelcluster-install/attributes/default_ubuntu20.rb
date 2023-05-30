a5d9ebea8cd400ba65969037d2615dacceb3cc0e
Tatiana Litvinova <litvit@amazon.com>
2023-05-30 13:27:10 +0200

    Cleanup unused attributes in install cookbooks
    Tests:
    * `./kitchen.ec2.sh environment-install verify nfs -c 6`: OK
    * `./kitchen.ec2.sh environment-config verify nfs -c 6`: OK

--- cookbooks/aws-parallelcluster-install/attributes/default_ubuntu20.rb
+++
@@ -1,9 +0,0 @@
-# Ubuntu 20 default attributes for aws-parallelcluster-install
-
-return unless platform?('ubuntu') && node['platform_version'] == "20.04"
-
-default['nfs']['service_provider']['idmap'] = Chef::Provider::Service::Systemd
-default['nfs']['service_provider']['portmap'] = Chef::Provider::Service::Systemd
-default['nfs']['service_provider']['lock'] = Chef::Provider::Service::Systemd
-default['nfs']['service']['lock'] = 'rpc-statd'
-default['nfs']['service']['idmap'] = 'nfs-idmapd'
