6d8518de19398ccc16410315607e4ea1f5567724
Luca Carrogu <carrogu@amazon.com>
2023-05-30 16:00:24 +0200

    Rename group used to read SLURM_RESUME_FILE
    Rename group used to read SLURM_RESUME_FILE. Name is now more generic
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- cookbooks/aws-parallelcluster-shared/attributes/users.rb
+++ cookbooks/aws-parallelcluster-shared/attributes/users.rb
@@ -4,14 +4,14 @@ default['cluster']['cluster_admin_user'] = 'pcluster-admin'
 default['cluster']['cluster_admin_user_id'] = node['cluster']['reserved_base_uid']
 default['cluster']['cluster_admin_group'] = node['cluster']['cluster_admin_user']
 default['cluster']['cluster_admin_group_id'] = node['cluster']['cluster_admin_user_id']
+default['cluster']['cluster_admin_slurm_share_group'] = 'pcluster-slurm-share'
+default['cluster']['cluster_admin_slurm_share_group_id'] = node['cluster']['reserved_base_uid'] + 5
 
 # Slurm
 default['cluster']['slurm']['user'] = 'slurm'
 default['cluster']['slurm']['user_id'] = node['cluster']['reserved_base_uid'] + 1
 default['cluster']['slurm']['group'] = node['cluster']['slurm']['user']
 default['cluster']['slurm']['group_id'] = node['cluster']['slurm']['user_id']
-default['cluster']['slurm']['resume_group'] = 'pcluster-slurm-resume'
-default['cluster']['slurm']['resume_group_id'] = node['cluster']['reserved_base_uid'] + 5
 
 # Munge
 default['cluster']['munge']['user'] = 'munge'
