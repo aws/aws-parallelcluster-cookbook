5a9445fd6d31679bc781d2040668931ef010a714
Enrico Usai <usai@amazon.com>
2023-05-31 16:46:49 +0200

    Use on_docker for sticky_bits test
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-platform/test/controls/sticky_bits_spec.rb
+++ cookbooks/aws-parallelcluster-platform/test/controls/sticky_bits_spec.rb
@@ -1,7 +1,7 @@
 control 'tag:config_sticky_bits_configured' do
   title 'Check sticky bits configuration'
 
-  if (os_properties.ubuntu2004? || os_properties.ubuntu2204?) && !os_properties.virtualized?
+  if (os_properties.ubuntu2004? || os_properties.ubuntu2204?) && !os_properties.on_docker?
     # This test passes on Mac but doesn't work as GitHub action.
     describe kernel_parameter('fs.protected_regular') do
       its('value') { should eq 0 }
