651a2517043069e1682437bd8e204ab34ba97a2d
Tatiana Litvinova <litvit@amazon.com>
2023-05-31 14:39:26 +0200

    Avoid stepping into the cookbook to kitchen-test it
    This way, Berksfile in the root directory will be used to build the cookbooks Kitchen tests will have visibility on.
    Berksfiles inside the cookbooks will be used only for ChefSpec tests.
    
    All the Berksfiles have been reviewed and cleaned up.
    Paths in inspec.yml and kitchen config files have been fixed as well.

--- kitchen.local-yml.sh
+++ kitchen.local-yml.sh
@@ -4,8 +4,6 @@ KITCHEN_SCOPE=$1; shift;
 KITCHEN_SUBJECT=$(echo $KITCHEN_SCOPE | awk -F- '{print $1}')
 KITCHEN_PHASE=$(echo $KITCHEN_SCOPE | awk -F- '{print $2}')
 
-export KITCHEN_LOCAL_YAML="kitchen.${KITCHEN_SCOPE}.yml"
-
 if [[ "-recipes-resources-validate-" =~ ${KITCHEN_SUBJECT} ]]; then
   # ./kitchen.$driver.sh recipes-install ... will run kitchen in aws-parallelcluster-cookbook dir
   export KITCHEN_COOKBOOK_PATH='.'
@@ -14,4 +12,5 @@ else
   export KITCHEN_COOKBOOK_PATH="cookbooks/aws-parallelcluster-${KITCHEN_SUBJECT}"
 fi
 
+export KITCHEN_LOCAL_YAML="${KITCHEN_COOKBOOK_PATH}/kitchen.${KITCHEN_SCOPE}.yml"
 export KITCHEN_PHASE
