65321a10c96eda639bb02adbf080af20b6d6e351
Enrico Usai <usai@amazon.com>
2023-06-01 14:32:19 +0200

    Move directory_service.rb to environment cookbook
    * Moved inspec tests
    * Moved templates
    * Moved suite definition
    * Moved parse_arn function to the recipe where it is used
    * Checked attributes used by the recipe, they have been already moved to environment cookbook
    * Added on_docker conditions to recipe and inspec tests to permit to run them on Docker
    
    ### Tests
    
    * `bash kitchen.ec2.sh environment-config test directory-service -c 5` (all oses except U22) -> OK
    * `bash kitchen.docker.sh environment-config test directory-service -c 5` (all oses except U22) -> OK
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-config/libraries/helpers.rb
+++ cookbooks/aws-parallelcluster-config/libraries/helpers.rb
@@ -12,24 +12,6 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-# Parse an ARN.
-# ARN format: arn:PARTITION:SERVICE:REGION:ACCOUNT_ID:RESOURCE.
-# ARN examples:
-#   1. arn:aws:secretsmanager:eu-west-1:12345678910:secret:PasswordName
-#   2. arn:aws:ssm:eu-west-1:12345678910:parameter/PasswordName
-def parse_arn(arn_string)
-  parts = arn_string.nil? ? [] : arn_string.split(':', 6)
-  raise TypeError if parts.size < 6
-
-  {
-    partition: parts[1],
-    service: parts[2],
-    region: parts[3],
-    account_id: parts[4],
-    resource: parts[5],
-  }
-end
-
 # load shared storages data into node object
 def load_shared_storages_mapping
   ruby_block "load shared storages mapping during cluster update" do
