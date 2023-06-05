aws/aws-parallelcluster-cookbook

commit 9467c928784703d94ca9010c08215192adaeab65

Move dcv resource to platform cookbook
Tests:
* ./kitchen.ec2.sh platform-install verify dcv -c 6 : OK for all the OSes but Ubuntu22
* ./kitchen.ec2.sh platform-config verify dcv -c 6 : OK for all the OSes but Ubuntu22
* not working on Docker: it didn't work even before this change, to be fixed separately

cookbooks/aws-parallelcluster-config/recipes/log_rotation.rb
@@ -20,4 +20,5 @@ template node['cluster']['pcluster_log_rotation_path'] do
   source 'log_rotation/parallelcluster_log_rotation.erb'
   mode '0644'
   only_if { node['cluster']['log_rotation_enabled'] == 'true' }
+  variables(dcv_configured: ::File.exist?("/etc/dcv/dcv.conf"))
 end

cookbooks/aws-parallelcluster-config/recipes/sudo.rb
@@ -32,6 +32,7 @@ template '/etc/parallelcluster/parallelcluster_supervisord.conf' do
   mode '0644'
   variables(
     region: region,
-    aws_ca_bundle: region.start_with?('us-iso') ? "/etc/pki/#{region}/certs/ca-bundle.pem" : ''
+    aws_ca_bundle: region.start_with?('us-iso') ? "/etc/pki/#{region}/certs/ca-bundle.pem" : '',
+    dcv_configured: ::File.exist?("/etc/dcv/dcv.conf")
   )
 end

cookbooks/aws-parallelcluster-platform/resources/dcv/partial/_dcv_common.rb
@@ -29,12 +29,12 @@ action_class do
 
   # Function to install and activate a Python virtual env to run the the External authenticator daemon
   def install_ext_auth_virtual_env
-    return if ::File.exist?("#{node['cluster']['dcv']['authenticator']['virtualenv_path']}/bin/activate")
+    return if ::File.exist?("#{dcvauth_virtualenv_path}/bin/activate")
 
     install_pyenv 'pyenv for default python version'
 
-    activate_virtual_env node['cluster']['dcv']['authenticator']['virtualenv'] do
-      pyenv_path node['cluster']['dcv']['authenticator']['virtualenv_path']
+    activate_virtual_env dcvauth_virtualenv do
+      pyenv_path dcvauth_virtualenv_path
       python_version node['cluster']['python-version']
     end
   end
@@ -44,7 +44,7 @@ action_class do
     # Override default GSettings to disable lock screen for all the users
     cookbook_file "/usr/share/glib-2.0/schemas/10_org.gnome.desktop.screensaver.gschema.override" do
       source 'dcv/10_org.gnome.desktop.screensaver.gschema.override'
-      cookbook 'aws-parallelcluster-common'
+      cookbook 'aws-parallelcluster-platform'
       owner 'root'
       group 'root'
       mode '0755'
@@ -54,10 +54,6 @@ action_class do
     execute 'Compile gsettings schema' do
       command "glib-compile-schemas /usr/share/glib-2.0/schemas/"
     end
-  end
-
-  def dcv_url
-    "https://d1uj6qtbmh3dt5.cloudfront.net/#{node['cluster']['dcv']['version'].split('-')[0]}/Servers/#{dcv_package}.tgz"
   end
 
   def post_install
@@ -107,17 +103,30 @@ action :setup do
   return if ::File.exist?("/etc/dcv/dcv.conf")
   return if redhat_ubi?
 
+  # share values with InSpec tests
+  node.default['conditions']['dcv_supported'] = dcv_supported?
+  node.default['cluster']['dcv']['authenticator']['virtualenv_path'] = dcvauth_virtualenv_path
+  node_attributes 'dump node attributes'
+
+  directory node['cluster']['scripts_dir'] do
+    recursive true
+  end
+
+  directory node['cluster']['sources_dir'] do
+    recursive true
+  end
+
   # Install pcluster_dcv_connect.sh script in all the OSes to use it for error handling
   cookbook_file "#{node['cluster']['scripts_dir']}/pcluster_dcv_connect.sh" do
     source 'dcv/pcluster_dcv_connect.sh'
-    cookbook 'aws-parallelcluster-common'
+    cookbook 'aws-parallelcluster-platform'
     owner 'root'
     group 'root'
     mode '0755'
     action :create_if_missing
   end
 
-  if node['conditions']['dcv_supported']
+  if dcv_supported?
     # Setup dcv authenticator group
     group node['cluster']['dcv']['authenticator']['group'] do
       comment 'NICE DCV External Authenticator group'
@@ -137,8 +146,6 @@ action :setup do
       shell '/bin/bash'
     end
 
-    dcv_tarball = "#{node['cluster']['sources_dir']}/dcv-#{node['cluster']['dcv']['version']}.tgz"
-
     pre_install
 
     disable_lock_screen
@@ -147,18 +154,10 @@ action :setup do
     unless ::File.exist?(dcv_tarball)
       remote_file dcv_tarball do
         source dcv_url
+        checksum dcv_sha256sum
         mode '0644'
         retries 3
         retry_delay 5
-      end
-
-      # Verify checksum of dcv package
-      ruby_block "verify dcv checksum" do
-        block do
-          require 'digest'
-          checksum = Digest::SHA256.file(dcv_tarball).hexdigest
-          raise "Downloaded DCV package checksum #{checksum} does not match expected checksum #{dcv_sha256sum}" if checksum != dcv_sha256sum
-        end
       end
 
       bash 'extract dcv packages' do
@@ -189,8 +188,13 @@ action :setup do
 end
 
 action :configure do
-  if node['conditions']['dcv_supported'] && node['cluster']['node_type'] == "HeadNode"
-    if graphic_instance? && nvidia_installed? && dcv_gpu_accel_supported?
+  # share values with InSpec tests
+  node.default['conditions']['dcv_supported'] = dcv_supported?
+  node.default['cluster']['dcv']['authenticator']['virtualenv_path'] = dcvauth_virtualenv_path
+  node_attributes 'dump node attributes'
+
+  if dcv_supported? && node['cluster']['node_type'] == "HeadNode"
+    if dcv_gpu_accel_supported?
       # Enable graphic acceleration in dcv conf file for graphic instances.
       allow_gpu_acceleration
     else
@@ -209,7 +213,7 @@ action :configure do
     # Install utility file to generate HTTPs certificates for the DCV external authenticator and generate a new one
     cookbook_file "/etc/parallelcluster/generate_certificate.sh" do
       source 'dcv/generate_certificate.sh'
-      cookbook 'aws-parallelcluster-common'
+      cookbook 'aws-parallelcluster-platform'
       owner 'root'
       mode '0700'
     end
@@ -231,6 +235,7 @@ action :configure do
     template "/etc/dcv/dcv.conf" do
       action :create
       source 'dcv/dcv.conf.erb'
+      cookbook 'aws-parallelcluster-platform'
       owner 'root'
       group 'root'
       mode '0755'
@@ -246,7 +251,7 @@ action :configure do
     # Install DCV external authenticator
     cookbook_file "#{node['cluster']['dcv']['authenticator']['user_home']}/pcluster_dcv_authenticator.py" do
       source 'dcv/pcluster_dcv_authenticator.py'
-      cookbook 'aws-parallelcluster-common'
+      cookbook 'aws-parallelcluster-platform'
       owner node['cluster']['dcv']['authenticator']['user']
       mode '0700'
     end
@@ -256,4 +261,37 @@ action :configure do
       action %i(enable start)
     end
   end
+end
+
+def dcv_supported?
+  true
+end
+
+def dcv_pkg_arch
+  arm_instance? ? 'arm64' : 'amd64'
+end
+
+def dcv_url_arch
+  arm_instance? ? 'aarch64' : 'x86_64'
+end
+
+def dcv_gpu_accel_supported?
+  unsupported_gpu_accel_list = ["g5g."]
+  nvidia_installed? && !node['ec2']['instance_type'].start_with?(*unsupported_gpu_accel_list)
+end
+
+def dcv_url
+  "https://d1uj6qtbmh3dt5.cloudfront.net/#{node['cluster']['dcv']['version'].split('-')[0]}/Servers/#{dcv_package}.tgz"
+end
+
+def dcv_tarball
+  "#{node['cluster']['sources_dir']}/dcv-#{node['cluster']['dcv']['version']}.tgz"
+end
+
+def dcvauth_virtualenv
+  'dcv_authenticator_virtualenv'
+end
+
+def dcvauth_virtualenv_path
+  "#{node['cluster']['system_pyenv_root']}/versions/#{node['cluster']['python-version']}/envs/#{dcvauth_virtualenv}"
 end

cookbooks/aws-parallelcluster-common/attributes/common.rb
@@ -11,44 +11,10 @@ default['cluster']['shared_dir_head'] = node['cluster']['shared_dir']
 default['cluster']['ebs_shared_dirs'] = '/shared'
 default['cluster']['exported_ebs_shared_dirs'] = node['cluster']['ebs_shared_dirs']
 
-# NICE DCV
-default['cluster']['dcv_port'] = 8443
-default['cluster']['dcv']['installed'] = 'yes'
-default['cluster']['dcv']['version'] = '2023.0-15022'
-if arm_instance?
-  default['cluster']['dcv']['supported_os'] = %w(centos7 ubuntu18 amazon2 redhat8)
-  default['cluster']['dcv']['url_architecture_id'] = 'aarch64'
-else
-  default['cluster']['dcv']['supported_os'] = %w(centos7 ubuntu18 ubuntu20 amazon2 redhat8)
-  default['cluster']['dcv']['url_architecture_id'] = 'x86_64'
-end
-default['cluster']['dcv']['server']['version'] = '2023.0.15022-1'
-default['cluster']['dcv']['xdcv']['version'] = '2023.0.547-1'
-default['cluster']['dcv']['gl']['version'] = '2023.0.1027-1'
-default['cluster']['dcv']['web_viewer']['version'] = '2023.0.15022-1'
-# DCV external authenticator configuration
-default['cluster']['dcv']['authenticator']['user'] = "dcvextauth"
-default['cluster']['dcv']['authenticator']['user_id'] = node['cluster']['reserved_base_uid'] + 3
-default['cluster']['dcv']['authenticator']['group'] = node['cluster']['dcv']['authenticator']['user']
-default['cluster']['dcv']['authenticator']['group_id'] = node['cluster']['dcv']['authenticator']['user_id']
-default['cluster']['dcv']['authenticator']['user_home'] = "/home/#{node['cluster']['dcv']['authenticator']['user']}"
-default['cluster']['dcv']['authenticator']['certificate'] = "/etc/parallelcluster/ext-auth-certificate.pem"
-default['cluster']['dcv']['authenticator']['private_key'] = "/etc/parallelcluster/ext-auth-private-key.pem"
-default['cluster']['dcv']['authenticator']['virtualenv'] = "dcv_authenticator_virtualenv"
-default['cluster']['dcv']['authenticator']['virtualenv_path'] = [
-  node['cluster']['system_pyenv_root'],
-  'versions',
-  node['cluster']['python-version'],
-  'envs',
-  node['cluster']['dcv']['authenticator']['virtualenv'],
-].join('/')
-
-default['conditions']['dcv_supported'] = platform_supports_dcv?
-
 # IMDS
 default['cluster']['head_node_imds_secured'] = 'true'
 default['cluster']['head_node_imds_allowed_users'] = ['root', node['cluster']['cluster_admin_user'], node['cluster']['cluster_user'] ]
-default['cluster']['head_node_imds_allowed_users'].append('dcv') if node['cluster']['dcv_enabled'] == 'head_node' && platform_supports_dcv?
+default['cluster']['head_node_imds_allowed_users'].append('dcv') if node['cluster']['dcv_enabled'] == 'head_node'
 default['cluster']['head_node_imds_allowed_users'].append(lazy { node['cluster']['scheduler_plugin']['user'] }) if node['cluster']['scheduler'] == 'plugin'
 
 # Default NFS mount options

cookbooks/aws-parallelcluster-common/libraries/helpers.rb
@@ -56,18 +56,3 @@ def format_directory(dir)
   format_dir = "/#{format_dir}" unless format_dir.start_with?('/')
   format_dir
 end
-
-#
-# Check if GPU acceleration is supported by DCV
-#
-def dcv_gpu_accel_supported?
-  unsupported_gpu_accel_list = ["g5g."]
-  !node['ec2']['instance_type'].start_with?(*unsupported_gpu_accel_list)
-end
-
-#
-# Check if DCV is supported on this OS
-#
-def platform_supports_dcv?
-  node['cluster']['dcv']['supported_os'].include?("#{node['platform']}#{node['platform_version'].to_i}")
-end

cookbooks/aws-parallelcluster-config/templates/default/base/parallelcluster_supervisord.conf.erb
@@ -30,7 +30,7 @@ redirect_stderr = true
 stdout_logfile = /var/log/parallelcluster/clusterstatusmgtd
 stdout_logfile_maxbytes = 0
 <% end -%>
-<% if node['cluster']['dcv_enabled'] == "head_node" && node['conditions']['dcv_supported'] -%>
+<% if @dcv_configured -%>
 [program:pcluster_dcv_authenticator]
 command = <%= node['cluster']['dcv']['authenticator']['virtualenv_path'] %>/bin/python <%= node['cluster']['dcv']['authenticator']['user_home'] %>/pcluster_dcv_authenticator.py
       --port <%= Integer(node['cluster']['dcv_port']) + 1 %>

cookbooks/aws-parallelcluster-config/templates/default/log_rotation/parallelcluster_log_rotation.erb
@@ -53,7 +53,7 @@
   copytruncate
   nodateext
 }
-<% if node['cluster']['dcv_enabled'] == "head_node" && node['conditions']['dcv_supported'] -%>
+<% if @dcv_configured -%>
 <%# dcv log %>
 /var/log/dcv/server.log
 /var/log/dcv/sessionlauncher.log

cookbooks/aws-parallelcluster-platform/attributes/platform.rb
@@ -12,3 +12,20 @@ default['conditions']['arm_pl_supported'] = arm_instance?
 # NVidia
 default['cluster']['nvidia']['enabled'] = 'no'
 default['cluster']['nvidia']['driver_version'] = '470.182.03'
+
+# DCV
+default['cluster']['dcv']['authenticator']['user'] = "dcvextauth"
+default['cluster']['dcv']['authenticator']['user_id'] = node['cluster']['reserved_base_uid'] + 3
+default['cluster']['dcv']['authenticator']['group'] = node['cluster']['dcv']['authenticator']['user']
+default['cluster']['dcv']['authenticator']['group_id'] = node['cluster']['dcv']['authenticator']['user_id']
+default['cluster']['dcv']['authenticator']['user_home'] = "/home/#{node['cluster']['dcv']['authenticator']['user']}"
+default['cluster']['dcv']['authenticator']['certificate'] = "/etc/parallelcluster/ext-auth-certificate.pem"
+default['cluster']['dcv']['authenticator']['private_key'] = "/etc/parallelcluster/ext-auth-private-key.pem"
+default['cluster']['dcv']['version'] = '2023.0-15022'
+default['cluster']['dcv_port'] = 8443
+default['cluster']['dcv']['installed'] = 'yes'
+
+default['cluster']['dcv']['server']['version'] = '2023.0.15022-1'
+default['cluster']['dcv']['xdcv']['version'] = '2023.0.547-1'
+default['cluster']['dcv']['gl']['version'] = '2023.0.1027-1'
+default['cluster']['dcv']['web_viewer']['version'] = '2023.0.15022-1'

commit f9e595e31c88fde16f4a3db0bc28f1b754dc91e7

Do not use DCV file exist to verify if DCV is configured or not
ParallelCluster recipes are creating the DCV configuration file from a template file
at instance bootstrap time if DCV is enabled, but the file is created at DCV setup.

Check the file exists is not enough, we need to verify if DCV is currently enabled.

This issue was causing the addition of dcv authenticator daemon in the supervisord template
even if DCV was not enable and this was causing failures in the recipe execution
because dcv_port was set to NONE by CDK generation so it was not possible to convert it to Integer.

### Tests

Created a unit test to verify supervisord template creation.

### References

Issue introduced with: 9467c928784703d94ca9010c08215192adaeab65

Signed-off-by: Enrico Usai <usai@amazon.com>

cookbooks/aws-parallelcluster-config/recipes/log_rotation.rb
@@ -20,5 +20,5 @@ template node['cluster']['pcluster_log_rotation_path'] do
   source 'log_rotation/parallelcluster_log_rotation.erb'
   mode '0644'
   only_if { node['cluster']['log_rotation_enabled'] == 'true' }
-  variables(dcv_configured: ::File.exist?("/etc/dcv/dcv.conf"))
+  variables(dcv_configured: node['cluster']['dcv_enabled'] == "head_node" && node['conditions']['dcv_supported'])
 end

cookbooks/aws-parallelcluster-config/recipes/sudo.rb
@@ -33,6 +33,6 @@ template '/etc/parallelcluster/parallelcluster_supervisord.conf' do
   variables(
     region: region,
     aws_ca_bundle: region.start_with?('us-iso') ? "/etc/pki/#{region}/certs/ca-bundle.pem" : '',
-    dcv_configured: ::File.exist?("/etc/dcv/dcv.conf")
+    dcv_configured: node['cluster']['dcv_enabled'] == "head_node" && node['conditions']['dcv_supported']
   )
 end

cookbooks/aws-parallelcluster-platform/resources/dcv/partial/_dcv_common.rb
@@ -103,7 +103,7 @@ action :setup do
   return if ::File.exist?("/etc/dcv/dcv.conf")
   return if redhat_ubi?
 
-  # share values with InSpec tests
+  # share values with InSpec tests and configuration recipes
   node.default['conditions']['dcv_supported'] = dcv_supported?
   node.default['cluster']['dcv']['authenticator']['virtualenv_path'] = dcvauth_virtualenv_path
   node_attributes 'dump node attributes'
@@ -188,7 +188,7 @@ action :setup do
 end
 
 action :configure do
-  # share values with InSpec tests
+  # share values with InSpec tests and configuration recipes
   node.default['conditions']['dcv_supported'] = dcv_supported?
   node.default['cluster']['dcv']['authenticator']['virtualenv_path'] = dcvauth_virtualenv_path
   node_attributes 'dump node attributes'

cookbooks/aws-parallelcluster-config/spec/unit/recipes/sudo_config_spec.rb
@@ -1,0 +1,81 @@
+require 'spec_helper'
+
+describe 'aws-parallelcluster-config::sudo' do
+  for_all_oses do |platform, version|
+    context "on #{platform}#{version}" do
+      cached(:chef_run) do
+        ChefSpec::Runner.new(platform: platform, version: version).converge(described_recipe)
+      end
+
+      it 'creates the sudoers template with the correct attributes' do
+        is_expected.to create_template('/etc/sudoers.d/99-parallelcluster-user-tty').with(
+          owner: 'root',
+          group: 'root',
+          mode:  '0600'
+        )
+      end
+
+      it 'creates the supervisord template with the correct attributes' do
+        is_expected.to create_template('/etc/parallelcluster/parallelcluster_supervisord.conf').with(
+          owner: 'root',
+          group: 'root',
+          mode:  '0644'
+        )
+      end
+
+      context "when head node and dcv configured" do
+        cached(:chef_run) do
+          runner = ChefSpec::Runner.new(platform: platform, version: version) do |node|
+            node.override['cluster']['node_type'] = 'HeadNode'
+            node.override['cluster']['dcv_enabled'] = 'head_node'
+            node.override['conditions']['dcv_supported'] = true
+          end
+          runner.converge(described_recipe)
+        end
+        cached(:node) { chef_run.node }
+
+        it 'has the correct content' do
+          is_expected.to render_file('/etc/parallelcluster/parallelcluster_supervisord.conf')
+            .with_content("[program:clusterstatusmgtd]")
+            .with_content("[program:pcluster_dcv_authenticator]")
+            .with_content("--port 8444")
+        end
+
+        context "when head node and dcv not configured" do
+          cached(:chef_run) do
+            runner = ChefSpec::Runner.new(platform: platform, version: version) do |node|
+              node.override['cluster']['node_type'] = 'HeadNode'
+              node.override['cluster']['dcv_enabled'] = 'NONE'
+              node.override['conditions']['dcv_supported'] = true
+            end
+            runner.converge(described_recipe)
+          end
+          cached(:node) { chef_run.node }
+
+          it 'has the correct content' do
+            is_expected.to render_file('/etc/parallelcluster/parallelcluster_supervisord.conf')
+              .with_content("[program:clusterstatusmgtd]")
+
+            is_expected.not_to render_file('/etc/parallelcluster/parallelcluster_supervisord.conf')
+              .with_content("[program:pcluster_dcv_authenticator]")
+          end
+        end
+      end
+
+      context "when compute fleet" do
+        cached(:chef_run) do
+          runner = ChefSpec::Runner.new(platform: platform, version: version) do |node|
+            node.override['cluster']['node_type'] = 'ComputeFleet'
+          end
+          runner.converge(described_recipe)
+        end
+        cached(:node) { chef_run.node }
+
+        it 'has the correct content' do
+          is_expected.to render_file('/etc/parallelcluster/parallelcluster_supervisord.conf')
+            .with_content("[program:computemgtd]")
+        end
+      end
+    end
+  end
+end