a8c44afeff28dd71f2e32ee5dbfaf09024db2bda
Enrico Usai <usai@amazon.com>
2023-06-06 18:01:34 +0200

    Move 'raid_shared_dir' attribute to environment cookbook
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-config/attributes/default.rb
+++ cookbooks/aws-parallelcluster-config/attributes/default.rb
@@ -1,6 +1,5 @@
 # aws-parallelcluster-config attributes
 
-default['cluster']['raid_shared_dir'] = ''
 default['cluster']['exported_raid_shared_dir'] = node['cluster']['raid_shared_dir']
 
 default['cluster']['scheduler_slots'] = 'vcpus'
