51371a728a370ffb3d8269e702ee5a6b3b059121
Enrico Usai <usai@amazon.com>
2023-06-07 10:37:46 +0200

    Move head_node_raid recipe to environment
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-config/recipes/head_node_base.rb
+++ cookbooks/aws-parallelcluster-config/recipes/head_node_base.rb
@@ -48,7 +48,7 @@ nfs_export "/opt/intel" do
 end unless on_docker?
 
 # Setup RAID array on head node
-include_recipe 'aws-parallelcluster-config::head_node_raid'
+include_recipe 'aws-parallelcluster-environment::head_node_raid'
 
 # Setup cluster user
 user node['cluster']['cluster_user'] do
