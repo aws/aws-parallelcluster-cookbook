b1c9b807831262aace02670efbc9f5facea13d9c
Enrico Usai <usai@amazon.com>
2023-06-08 10:58:31 +0200

    Move network_interface_macs function to environment
    This is only used by network_interfaces and slurm (that depends by environment).
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-shared/libraries/helpers.rb
+++ cookbooks/aws-parallelcluster-shared/libraries/helpers.rb
@@ -57,12 +57,3 @@ def get_metadata_with_token(token, uri)
   metadata = res.body if res.code == '200'
   metadata
 end
-
-#
-# Retrieve list of macs of network interfaces
-#
-def network_interface_macs(token)
-  uri = URI("http://169.254.169.254/latest/meta-data/network/interfaces/macs")
-  res = get_metadata_with_token(token, uri)
-  res.delete("/").split("\n")
-end
