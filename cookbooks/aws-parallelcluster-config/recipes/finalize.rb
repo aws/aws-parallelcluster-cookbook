7e6f2b62b09cc4b8e3712936fc1da81977892f6b
Enrico Usai <usai@amazon.com>
2023-06-12 17:09:31 +0200

    Move enable_chef_error_handler to platform cookbook
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-config/recipes/finalize.rb
+++ cookbooks/aws-parallelcluster-config/recipes/finalize.rb
@@ -15,7 +15,7 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe "aws-parallelcluster-config::enable_chef_error_handler"
+include_recipe "aws-parallelcluster-platform::enable_chef_error_handler"
 
 fetch_config 'Fetch and load cluster configs'
 
--- cookbooks/aws-parallelcluster-platform/recipes/config/enable_chef_error_handler.rb
+++ cookbooks/aws-parallelcluster-platform/recipes/config/enable_chef_error_handler.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 #
-# Cookbook:: aws-parallelcluster-config
-# Recipe:: enable_chef_error_handler
-#
-# Copyright:: 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright:: 2022-2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the
 # License. A copy of the License is located at
