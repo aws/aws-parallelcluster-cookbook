7a38a1a7d7f96a9ffb571b91391d7c1716ac18ba
Enrico Usai <usai@amazon.com>
2023-06-13 18:54:54 +0200

    Remove useless converge step
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-environment/spec/unit/libraries/volume_spec.rb
+++ cookbooks/aws-parallelcluster-environment/spec/unit/libraries/volume_spec.rb
@@ -1,14 +1,6 @@
 require 'spec_helper'
 
 describe 'aws-parallelcluster-environment:libraries:format_directory' do
-  cached(:chef_run) do
-    ChefSpec::SoloRunner.new.converge_dsl do
-      volume 'nothing' do
-        action :nothing
-      end
-    end
-  end
-
   it 'adds / at the beginning if not starting with /' do
     expect(format_directory('any')).to eq('/any')
   end
