ef2a4a96dac521d3c39f73b4ca4e847d66054dfa
Himani Anil Deshpande <79726937+himani2411@users.noreply.github.com>
2023-06-13 13:38:30 -0400

    Correcting the cookbook for mount_shared recipe (#2288)
    Co-authored-by: Himani Deshpande <himanidp@amazon.com>

--- cookbooks/aws-parallelcluster-config/recipes/init.rb
+++ cookbooks/aws-parallelcluster-config/recipes/init.rb
@@ -24,7 +24,7 @@ validate_os_type
 raise "Init package #{node['init_package']} not supported." unless systemd? || on_docker?
 
 include_recipe "aws-parallelcluster-environment::cfnconfig_mixed"
-include_recipe "aws-parallelcluster-config::mount_shared" if node['cluster']['node_type'] == "ComputeFleet"
+include_recipe "aws-parallelcluster-environment::mount_shared" if node['cluster']['node_type'] == "ComputeFleet"
 
 fetch_config 'Fetch and load cluster configs'
 
