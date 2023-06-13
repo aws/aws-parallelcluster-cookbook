dd28fefc9709c902f29cc137006fb056ac1581fd
Enrico Usai <usai@amazon.com>
2023-06-13 17:20:41 +0200

    Move base.rb code into config.rb
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-config/recipes/config.rb
+++ cookbooks/aws-parallelcluster-config/recipes/config.rb
@@ -17,7 +17,65 @@
 
 include_recipe "aws-parallelcluster-platform::enable_chef_error_handler"
 
-include_recipe 'aws-parallelcluster-config::base'
+include_recipe "aws-parallelcluster-shared::setup_envars"
+include_recipe 'aws-parallelcluster-platform::openssh'
+
+sticky_bits "setup sticky bits"
+
+nfs "Configure NFS" do
+  action :configure
+end
+
+include_recipe 'aws-parallelcluster-environment::ephemeral_drives'
+
+include_recipe 'aws-parallelcluster-platform::networking'
+
+# Amazon Time Sync
+chrony 'enable chrony' do
+  action :enable
+end
+
+# Configure Nvidia driver
+include_recipe "aws-parallelcluster-platform::nvidia_config"
+
+# EFA runtime configuration
+efa 'Configure system for EFA' do
+  action :configure
+end
+
+case node['cluster']['node_type']
+when 'HeadNode'
+  include_recipe 'aws-parallelcluster-platform::cluster_user'
+
+  # generate the shared storages mapping file
+  include_recipe 'aws-parallelcluster-environment::fs_update'
+
+  include_recipe 'aws-parallelcluster-environment::ebs'
+  include_recipe 'aws-parallelcluster-environment::shared_storages'
+  include_recipe 'aws-parallelcluster-environment::raid'
+
+  include_recipe 'aws-parallelcluster-platform::dcv'
+
+  include_recipe 'aws-parallelcluster-computefleet::head_node_fleet_status'
+
+when 'ComputeFleet'
+  include_recipe 'aws-parallelcluster-platform::cluster_user'
+  include_recipe 'aws-parallelcluster-environment::raid'
+  include_recipe 'aws-parallelcluster-environment::shared_storages'
+  include_recipe 'aws-parallelcluster-environment::ebs'
+else
+  raise "node_type must be HeadNode or ComputeFleet"
+end
+
+include_recipe "aws-parallelcluster-platform::sudo_config"
+
+# Mount EFS, FSx
+include_recipe "aws-parallelcluster-environment::fs_mount"
+
+# Intel Runtime Libraries
+intel_hpc 'Configure Intel HPC' do
+  action :configure
+end
 
 fetch_config 'Fetch and load cluster configs'
 
