59e5c8133c4c080debc49435662c6b98dcdc6b9d
Enrico Usai <usai@amazon.com>
2023-06-13 17:31:52 +0200

    Unify head node and compute fleet flow
    The guards to skip the execution or execute different behaviours
    according to the node type are now into the recipes
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-config/recipes/config.rb
+++ cookbooks/aws-parallelcluster-config/recipes/config.rb
@@ -43,30 +43,17 @@ efa 'Configure system for EFA' do
   action :configure
 end
 
-case node['cluster']['node_type']
-when 'HeadNode'
-  include_recipe 'aws-parallelcluster-platform::cluster_user'
+include_recipe 'aws-parallelcluster-platform::cluster_user'
 
-  # generate the shared storages mapping file
-  include_recipe 'aws-parallelcluster-environment::fs_update'
+# generate the shared storages mapping file
+include_recipe 'aws-parallelcluster-environment::fs_update'
+include_recipe 'aws-parallelcluster-environment::ebs'
+include_recipe 'aws-parallelcluster-environment::shared_storages'
+include_recipe 'aws-parallelcluster-environment::raid'
 
-  include_recipe 'aws-parallelcluster-environment::ebs'
-  include_recipe 'aws-parallelcluster-environment::shared_storages'
-  include_recipe 'aws-parallelcluster-environment::raid'
+include_recipe 'aws-parallelcluster-platform::dcv'
 
-  include_recipe 'aws-parallelcluster-platform::dcv'
-
-  include_recipe 'aws-parallelcluster-computefleet::fleet_status'
-
-when 'ComputeFleet'
-  include_recipe 'aws-parallelcluster-platform::cluster_user'
-
-  include_recipe 'aws-parallelcluster-environment::ebs'
-  include_recipe 'aws-parallelcluster-environment::shared_storages'
-  include_recipe 'aws-parallelcluster-environment::raid'
-else
-  raise "node_type must be HeadNode or ComputeFleet"
-end
+include_recipe 'aws-parallelcluster-computefleet::fleet_status'
 
 include_recipe "aws-parallelcluster-platform::sudo_config"
 
