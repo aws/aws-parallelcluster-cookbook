8216ea014427a5d91ac5868aa3ce8b39074251e8
Himani Anil Deshpande <79726937+himani2411@users.noreply.github.com>
2023-06-13 10:59:13 -0400

    Move Mount_shared recipe to environment cookbook (#2274)
    Co-authored-by: Himani Deshpande <himanidp@amazon.com>

--- cookbooks/aws-parallelcluster-environment/attributes/environment.rb
+++ cookbooks/aws-parallelcluster-environment/attributes/environment.rb
@@ -42,3 +42,9 @@ default['cluster']['raid_vol_ids'] = ''
 default['cluster']['raid_shared_dir'] = ''
 default['cluster']['ephemeral_dir'] = '/scratch'
 default['cluster']['scheduler_slots'] = 'vcpus'
+
+default['cluster']['head_node_home_path'] = '/home'
+default['cluster']['shared_dir_compute'] = node['cluster']['shared_dir']
+default['cluster']['shared_dir_head'] = node['cluster']['shared_dir']
+
+default['cluster']['head_node_private_ip'] = nil
--- cookbooks/aws-parallelcluster-environment/recipes/config/mount_shared.rb
+++ cookbooks/aws-parallelcluster-environment/recipes/config/mount_shared.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-#
-# Cookbook:: aws-parallelcluster
-# Recipe:: mount_shared
-#
 # Copyright:: 2013-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the
@@ -15,7 +11,7 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-return if virtualized?
+return if on_docker?
 
 # Mount /home over NFS
 mount '/home' do
