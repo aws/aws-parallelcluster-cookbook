7fc11ecbcd9462f877fa7200069b435aeb501b33
Enrico Usai <usai@amazon.com>
2023-06-13 17:43:24 +0200

    Move scheduler guards in the config recipes of the schedulers cookbooks
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-slurm/recipes/config.rb
+++ cookbooks/aws-parallelcluster-slurm/recipes/config.rb
@@ -15,6 +15,8 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
+return unless node['cluster']['scheduler'] == 'slurm'
+
 case node['cluster']['node_type']
 when 'HeadNode'
   include_recipe 'aws-parallelcluster-slurm::config_head_node'
