20bc6021fb2bba8da55e7f2c9d1e6d44f16fd235
Enrico Usai <usai@amazon.com>
2023-06-13 15:47:31 +0200

    Delete unused graphic_instance function
    This is defined in platform libraries too and used by nvidia, dcv, slurm recipes.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-common/libraries/helpers.rb
+++ cookbooks/aws-parallelcluster-common/libraries/helpers.rb
@@ -3,16 +3,6 @@ def virtualized?
   node.include?('virtualized') and node['virtualized']
 end
 
-#
-# Check if the instance has a GPU
-#
-def graphic_instance?
-  has_gpu = Mixlib::ShellOut.new("lspci | grep -i -o 'NVIDIA'")
-  has_gpu.run_command
-
-  !has_gpu.stdout.strip.empty?
-end
-
 def format_directory(dir)
   format_dir = dir.strip
   format_dir = "/#{format_dir}" unless format_dir.start_with?('/')
