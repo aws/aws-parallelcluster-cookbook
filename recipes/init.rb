f7ab98100c354f59b8321e30effd9ae8b5696a8b
Enrico Usai <usai@amazon.com>
2023-06-14 14:34:19 +0200

    Move init recipe from config cookbook to entrypoints
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- recipes/init.rb
+++ recipes/init.rb
@@ -15,4 +15,4 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe "aws-parallelcluster-config::init"
+include_recipe "aws-parallelcluster-entrypoints::init"
