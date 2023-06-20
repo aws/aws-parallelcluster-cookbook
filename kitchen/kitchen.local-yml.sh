8d5c9c2bf552b014c1a88c16dc1e7933d362cc4a
Enrico Usai <usai@amazon.com>
2023-06-20 13:30:01 +0200

    Minor cleanup to kitchen utility file
    recipes-resources-validate file does not exist anymore.
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- kitchen/kitchen.local-yml.sh
+++ kitchen/kitchen.local-yml.sh
@@ -4,13 +4,8 @@ KITCHEN_SCOPE=$1; shift;
 KITCHEN_SUBJECT=$(echo $KITCHEN_SCOPE | awk -F- '{print $1}')
 KITCHEN_PHASE=$(echo $KITCHEN_SCOPE | awk -F- '{print $2}')
 
-if [[ "-recipes-resources-validate-" =~ ${KITCHEN_SUBJECT} ]]; then
-  # ./kitchen.$driver.sh recipes-install ... will run kitchen in aws-parallelcluster-cookbook dir
-  export KITCHEN_COOKBOOK_PATH='.'
-else
-  # ./kitchen.$driver.sh platform-install ... will run kitchen in aws-parallelcluster-cookbook/cookbooks/aws-parallelcluster-platform dir
-  export KITCHEN_COOKBOOK_PATH="cookbooks/aws-parallelcluster-${KITCHEN_SUBJECT}"
-fi
+# ./kitchen.$driver.sh platform-install ... will run kitchen in aws-parallelcluster-cookbook/cookbooks/aws-parallelcluster-platform dir
+export KITCHEN_COOKBOOK_PATH="cookbooks/aws-parallelcluster-${KITCHEN_SUBJECT}"
 
 export KITCHEN_LOCAL_YAML="${KITCHEN_COOKBOOK_PATH}/kitchen.${KITCHEN_SCOPE}.yml"
 export KITCHEN_PHASE
