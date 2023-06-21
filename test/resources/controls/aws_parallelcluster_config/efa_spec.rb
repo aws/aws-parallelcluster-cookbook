dec56c06c5e567cad622c40a9011707775a1f274
Ryan Anderson <ndry@amazon.com>
2023-06-21 10:14:27 -0400

    Fix the kitchen test condition for ptrace_scope to switch on node type and add an efa kitchen test suite for the head node

--- test/resources/controls/aws_parallelcluster_config/efa_spec.rb
+++ test/resources/controls/aws_parallelcluster_config/efa_spec.rb
@@ -3,7 +3,21 @@ control 'efa_debian_system_settings_configured' do
 
   only_if { os.debian? && !os_properties.virtualized? }
 
+  ptrace_scope = instance.head_node? ? 1 : 0
+  if ptrace_scope == 1
+    describe 'Verify ptrace config file is not present' do
+      subject { file('/etc/sysctl.d/99-chef-kernel.yama.ptrace_scope.conf') }
+      it { should_not exist }
+    end
+  else
+    describe 'Verify ptrace config file is present' do
+      subject { file('/etc/sysctl.d/99-chef-kernel.yama.ptrace_scope.conf') }
+      it { should exist }
+      its('content') { should match /kernel.yama.ptrace_scope = #{ptrace_scope}/ }
+    end
+  end
+
   describe kernel_parameter('kernel.yama.ptrace_scope') do
-    its('value') { should eq 0 }
+    its('value') { should eq ptrace_scope }
   end
 end
