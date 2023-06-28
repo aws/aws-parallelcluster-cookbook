4429d9ee554b2deb17e1391a003d01a385b5b7f2
Nicola Sirena <nssirena@amazon.com>
2023-06-28 14:16:51 +0200

    Define current and previous config path for LoginNodes
    HeadNode retrieve the cluster config from S3 and shares it with
    ComputeNodes using the "shared" folder and with LoginNodes using
    the "shared_login_nodes" folder.
    
    Signed-off-by: Nicola Sirena <nssirena@amazon.com>

--- cookbooks/aws-parallelcluster-shared/attributes/cluster.rb
+++ cookbooks/aws-parallelcluster-shared/attributes/cluster.rb
@@ -4,7 +4,7 @@ default['cluster']['scripts_dir'] = "#{node['cluster']['base_dir']}/scripts"
 default['cluster']['license_dir'] = "#{node['cluster']['base_dir']}/licenses"
 default['cluster']['configs_dir'] = "#{node['cluster']['base_dir']}/configs"
 default['cluster']['shared_dir'] = "#{node['cluster']['base_dir']}/shared"
-default['cluster']['shared_login_nodes_dir'] = "#{node['cluster']['base_dir']}/shared_login_nodes"
+default['cluster']['shared_dir_login_nodes'] = "#{node['cluster']['base_dir']}/shared_login_nodes"
 default['cluster']['log_base_dir'] = '/var/log/parallelcluster'
 default['cluster']['etc_dir'] = '/etc/parallelcluster'
 
@@ -20,6 +20,8 @@ default['cluster']['system_pyenv_root'] = "#{node['cluster']['base_dir']}/pyenv"
 
 default['cluster']['cluster_config_path'] = "#{node['cluster']['shared_dir']}/cluster-config.yaml"
 default['cluster']['previous_cluster_config_path'] = "#{node['cluster']['shared_dir']}/previous-cluster-config.yaml"
+default['cluster']['login_cluster_config_path'] = "#{node['cluster']['shared_dir_login_nodes']}/cluster-config.yaml"
+default['cluster']['login_previous_cluster_config_path'] = "#{node['cluster']['shared_dir_login_nodes']}/previous-cluster-config.yaml"
 default['cluster']['change_set_path'] = "#{node['cluster']['shared_dir']}/change-set.json"
 default['cluster']['instance_types_data_path'] = "#{node['cluster']['shared_dir']}/instance-types-data.json"
 
