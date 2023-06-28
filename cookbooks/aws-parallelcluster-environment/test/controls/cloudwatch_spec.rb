b27ac946351e13700f307af7b1a206ac28987596
Ryan Anderson <ndry@amazon.com>
2023-06-28 17:10:04 -0400

    Skip the failing features for the Ubuntu 22 AMI build in kitchen tests as well (#2338)
    Skip exit status for cloudwatch on Ubuntu 22

--- cookbooks/aws-parallelcluster-environment/test/controls/cloudwatch_spec.rb
+++ cookbooks/aws-parallelcluster-environment/test/controls/cloudwatch_spec.rb
@@ -16,11 +16,13 @@ control 'tag:install_cloudwatch_installation_files' do
   describe 'Check the presence of the cloudwatch package gpg key'
   # In Ubuntu >20.04 due to environment variable the keyring is placed under home of the user ubuntu with the permission of root
   ubuntu2004 = os_properties.ubuntu2004?
-  keyring = (os_properties.ubuntu2004? || os_properties.ubuntu2204?) && !os_properties.on_docker? ? '--keyring /home/ubuntu/.gnupg/pubring.kbx' : ''
+  ubuntu2204 = os_properties.ubuntu2204?
+  keyring = (ubuntu2004 || ubuntu2204) && !os_properties.on_docker? ? '--keyring /home/ubuntu/.gnupg/pubring.kbx' : ''
   sudo = os_properties.redhat_on_docker? ? '' : 'sudo'
   describe bash("#{sudo} gpg --list-keys #{keyring}") do
     # Don't check exit status for Ubuntu20 because it returns 2 when executed in the validate phase of a created AMI
-    its('exit_status') { should eq 0 } unless ubuntu2004
+    # os_properties cannot be used in the describe block level.  It can be used within an it{} block
+    its('exit_status') { should eq 0 } unless ubuntu2004 || ubuntu2204
     its('stdout') { should match /3B789C72/ }
     its('stdout') { should match /Amazon CloudWatch Agent/ }
   end
