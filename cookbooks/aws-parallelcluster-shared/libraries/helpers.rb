e24b5fb6f0c02dd434d2c6248aedd7119e67b000
Nicola Sirena <nssirena@amazon.com>
2023-06-28 17:39:57 +0200

    LoadClusterConfig take the config path as parameter
    HeadNode and ComputeNodes read the config from the "shared" folder.
    LoginNodes instead read it from "shared_login_nodes" folder.
    
    Signed-off-by: Nicola Sirena <nssirena@amazon.com>

--- cookbooks/aws-parallelcluster-shared/libraries/helpers.rb
+++ cookbooks/aws-parallelcluster-shared/libraries/helpers.rb
@@ -61,11 +61,12 @@ end
 #
 # load cluster configuration file into node object
 #
-def load_cluster_config
+def load_cluster_config(config_path)
   ruby_block "load cluster configuration" do
     block do
       require 'yaml'
-      config = YAML.safe_load(File.read(node['cluster']['cluster_config_path']))
+      Chef::Log.info("Reading Config from #{config_path}")
+      config = YAML.safe_load(File.read(config_path))
       Chef::Log.debug("Config read #{config}")
       node.override['cluster']['config'].merge! config
     end
