111488b2c0539ef9e0d99711bb65c28cbe866810
Francesco Giordano <giordafr@amazon.it>
2023-06-29 14:37:23 +0200

    Replace nvidia-persistenced with parallelcluster_nvidia service
    `parallelcluster_nvidia` service ensures the creation of the block devices `/dev/nvidia0`
    and it is needed by the `slurmd` service.
    
    `parallelcluster_nvidia` starts the `nvidia-persistenced` or runs `nvidia-smi`
    to avoid race condition with other services and avoids conflicts when using DLAMI with a gpu instance.
    
    ### Tests
    * Modified ChefSpec to verify new changes.
    
    ### References
    Backport of: https://github.com/aws/aws-parallelcluster-cookbook/pull/2341
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-platform/libraries/nvidia.rb
+++ cookbooks/aws-parallelcluster-platform/libraries/nvidia.rb
@@ -8,3 +8,13 @@ end
 def graphic_instance?
   !Mixlib::ShellOut.new("lspci | grep -i -o 'NVIDIA'").run_command.stdout.strip.empty?
 end
+
+#
+# Check if a process is running
+#
+def is_process_running(process_name)
+  ps = Mixlib::ShellOut.new("ps aux | grep '#{process_name}' | egrep -v \"grep .*#{process_name}\"")
+  ps.run_command
+
+  !ps.stdout.strip.empty?
+end
