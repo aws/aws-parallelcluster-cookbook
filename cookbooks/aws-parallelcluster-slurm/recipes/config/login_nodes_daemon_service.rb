b5ade78af4daf52f11ad17d3b7f75fa3e2aca5a7
Xuanqi He <xuanqihe@amazon.com>
2023-07-16 23:30:54 -0400

    [Login Nodes] Add loginmgtd daemon to login nodes.
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

---
+++ cookbooks/aws-parallelcluster-slurm/recipes/config/login_nodes_daemon_service.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+#
+# Cookbook:: aws-parallelcluster-slurm
+# Recipe:: login_nodes_daemon_service.rb
+#
+# Copyright:: 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the
+# License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+
+load_cluster_config(node['cluster']['login_cluster_config_path'])
+
+# Create the configuration file for loginmgtd
+template "#{node['cluster']['shared_dir_login_nodes']}/loginmgtd_config.json" do
+  source 'slurm/login/loginmgtd_config.json.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  variables(
+    gracetime_period: lazy { node['cluster']['config'].dig(:LoginNodes, :Pools, 0, :GracetimePeriod) }
+  )
+end
+
+# Create the termination hook for loginmgtd
+template "#{node['cluster']['shared_dir_login_nodes']}/loginmgtd_on_termination.sh" do
+  source 'slurm/login/loginmgtd_on_termination.sh.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+# Create the script to run loginmgtd
+template "#{node['cluster']['shared_dir_login_nodes']}/loginmgtd.sh" do
+  source 'slurm/login/loginmgtd.sh.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
