243c25f2a0e185ef71c5b0208cb5809037040c29
Xuanqi He <xuanqihe@amazon.com>
2023-07-17 21:07:14 -0400

    Address comments in the PR except for comments related to supervisord

--- cookbooks/aws-parallelcluster-slurm/recipes/config/login_nodes_daemon_service.rb
+++ cookbooks/aws-parallelcluster-slurm/recipes/config/login_nodes_daemon_service.rb
@@ -1,41 +1,37 @@
-require 'yaml'
-config = YAML.safe_load(File.read(node['cluster']['cluster_config_path']))
-GRACETIME_PERIOD = config["LoginNodes"]["Pools"][0]["GRACETIME_PERIOD"]
+GRACETIME_PERIOD = lazy { node['cluster']['config'].dig(:LoginNodes, :Pools, 0, :GracetimePeriod) }
 
 # Create configuration file
-template "#{node['cluster']['shared_dir']}/login_nodes_daemon_config.json" do
-  source 'login_nodes_daemon_service/login_nodes_daemon_config.json.erb'
+template "#{node['cluster']['shared_dir_login_nodes']}/login_nodes_daemon_config.json" do
+  source 'slurm/login/login_nodes_daemon_config.json.erb'
   owner 'root'
   group 'root'
   mode '0644'
   variables(
-    {
-      GRACETIME_PERIOD: GRACETIME_PERIOD,
-    }
+    GRACETIME_PERIOD: GRACETIME_PERIOD,
   )
 end
 
 # Create on_termination_script.sh
-template "#{node['cluster']['shared_dir']}/login_nodes_on_termination_script.sh" do
-  source 'login_nodes_daemon_service/login_nodes_on_termination_script.sh.erb'
+template "#{node['cluster']['shared_dir_login_nodes']}/login_nodes_on_termination_script.sh" do
+  source 'slurm/login/login_nodes_on_termination.sh.erb'
   owner 'root'
   group 'root'
-  mode '0755'
+  mode '0744'
 end
 
 # Create daemon_script.sh
-template "#{node['cluster']['shared_dir']}/login_nodes_daemon_script.sh" do
-  source 'login_nodes_daemon_service/login_nodes_daemon_script.sh.erb'
+template "#{node['cluster']['shared_dir_login_nodes']}/login_nodes_daemon_script.sh" do
+  source 'slurm/login/login_nodes_daemon.sh.erb'
   owner 'root'
   group 'root'
-  mode '0755'
+  mode '0744'
 end
 
 # Create supervisord configuration for the daemon
 file "/etc/supervisor/conf.d/login_nodes_daemon_service.conf" do
   content <<-EOF
 [program:login_nodes_daemon_service]
-command=bash #{node['cluster']['shared_dir']}/login_nodes_daemon_script.sh #{GRACETIME_PERIOD}
+command=bash #{node['cluster']['shared_dir_login_nodes']}/login_nodes_daemon_script.sh
 autostart=true
 autorestart=true
 stopasgroup=true
