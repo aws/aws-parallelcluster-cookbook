1cf81976c7384a53e2840f0a6e6a5432f5e9820d
Ryan Anderson <ndry@amazon.com>
2023-07-25 10:15:32 -0400

    Bump version to 3.7.0b1 (#2384)

--- cookbooks/aws-parallelcluster-shared/attributes/versions.rb
+++ cookbooks/aws-parallelcluster-shared/attributes/versions.rb
@@ -2,7 +2,7 @@
 default['cluster']['python-version'] = '3.9.16'
 
 # ParallelCluster versions
-default['cluster']['parallelcluster-version'] = '3.7.0'
-default['cluster']['parallelcluster-cookbook-version'] = '3.7.0'
-default['cluster']['parallelcluster-node-version'] = '3.7.0'
+default['cluster']['parallelcluster-version'] = '3.7.0b1'
+default['cluster']['parallelcluster-cookbook-version'] = '3.7.0b1'
+default['cluster']['parallelcluster-node-version'] = '3.7.0b1'
 default['cluster']['parallelcluster-awsbatch-cli-version'] = '1.1.0'
