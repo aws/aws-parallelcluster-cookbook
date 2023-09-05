dcc5debfc4fa1f786ab28612a402310c9a943dd4
Xuanqi He <xuanqihe@amazon.com>
2023-09-05 10:53:50 -0400

    set correct ownership to the munge key

--- cookbooks/aws-parallelcluster-slurm/resources/munge_key_manager.rb
+++ cookbooks/aws-parallelcluster-slurm/resources/munge_key_manager.rb
@@ -11,10 +11,15 @@ action :manage do
     # This block will fetch the munge key from Secrets Manager
     bash 'fetch_and_decode_munge_key' do
       user 'root'
+      group 'root'
       cwd '/tmp'
       code <<-FETCH_AND_DECODE
+        # Get encoded munge key from secrets manager and decode it
         encoded_key=$(aws secretsmanager get-secret-value --secret-id #{new_resource.munge_key_secret_arn} --query 'SecretString' --output text)
         echo $encoded_key | base64 -d > /etc/munge/munge.key
+        # Set ownership on the key
+        chown #{node['cluster']['munge']['user']}:#{node['cluster']['munge']['group']} /etc/munge/munge.key
+        # Enforce correct permission on the key
         chmod 0600 /etc/munge/munge.key
       FETCH_AND_DECODE
     end
