fcda8b41fc8dacc980643305fb7f01f43aa9cdb6
cloud303-tfurlong <tfurlong@cloud303.io>
2023-10-03 12:14:28 -0600

    _is_session_valid unit test to increase coverage

--- test/unit/dcv/test_dcv_authenticator.py
+++ test/unit/dcv/test_dcv_authenticator.py
@@ -159,6 +159,25 @@ def test_get_request_token_parameter(parameters, keys, result):
             DCVAuthenticator._extract_parameters_values(parameters, keys)
 
 
+def test_is_session_valid(mocker):
+    ps_aunx_output = (
+        b"USER   PID %CPU %MEM    VSZ   RSS TTY      STAT START   TIME COMMAND\n"
+        b"1000 63 0.0 0.0 4348844 3108   ??  Ss   23Jul19   2:32.46  /usr/libexec/dcv/dcvagent --mode full --session-id mysession\n"
+        b"2000 2949 0.3 0.4 860568 34328 ? Sl 20:10 0:18 /usr/lib/x86_64-linux-gnu/dcv/dcvagent --mode full --session-id mysession\n"
+    )
+    # Mock subprocess.check_output with realistic responses for `/usr/bin/id` and `/bin/ps aunx`
+    mocker.patch(AUTH_MODULE_MOCK_PATH + "subprocess.check_output", side_effect=[b"1000", ps_aunx_output])
+
+    # Test that the session is valid
+    DCVAuthenticator._is_session_valid("myuser", "mysession")
+
+    # Mock subprocess.check_output with realistic responses for `/usr/bin/id` and `/bin/ps aunx`
+    mocker.patch(AUTH_MODULE_MOCK_PATH + "subprocess.check_output", side_effect=[b"1000", ps_aunx_output])
+
+    # Test that the session is not valid
+    with pytest.raises(DCVAuthenticator.IncorrectRequestError):
+        DCVAuthenticator._is_session_valid("myuser", "wrongsession")
+
 def test_get_request_token(mocker):
     """Verify the first step of the authentication process, the retrieval of the Request Token."""
     # A nosec comment is appended to the following line in order to disable the B105 check.
