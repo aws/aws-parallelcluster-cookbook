1c645e7d26217898fa32069dd0b7f95a2895cca2
Nicola Sirena <nssirena@amazon.com>
2023-10-04 17:25:06 +0200

    Move slurm_resume configuration in a sub-recipe
    Also factor out the creation of some directory and add
    ScalingStrategy parameter to slurm_resume config
    
    Signed-off-by: Nicola Sirena <nssirena@amazon.com>

---
+++ cookbooks/aws-parallelcluster-slurm/recipes/config/config_head_node_directories.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+#
+# Cookbook:: aws-parallelcluster-slurm
+# Recipe:: config_head_node_directories
+#
+# Copyright:: 2013-2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the
+# License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Ensure config directory is in place
+directory "#{node['cluster']['slurm']['install_dir']}" do
+  user 'root'
+  group 'root'
+  mode '0755'
+end
+
+# Ensure config directory is in place
+directory "#{node['cluster']['slurm']['install_dir']}/etc" do
+  user 'root'
+  group 'root'
+  mode '0755'
+end
+
+# Create directory configured as StateSaveLocation
+directory '/var/spool/slurm.state' do
+  user node['cluster']['slurm']['user']
+  group node['cluster']['slurm']['group']
+  mode '0700'
+end
+
+# Copy pcluster config generator and templates
+remote_directory "#{node['cluster']['scripts_dir']}/slurm" do
+  source 'head_node_slurm/slurm'
+  mode '0755'
+  action :create
+  recursive true
+end
--- cookbooks/aws-parallelcluster-slurm/recipes/test/mock_slurm_dir.rb
+++ cookbooks/aws-parallelcluster-slurm/recipes/test/mock_slurm_dir.rb
@@ -1,6 +1,18 @@
+slurm_install_dir = '/opt/slurm'
+slurm_plugin_dir = '/etc/parallelcluster/slurm_plugin'
+
+# Ensure slurm plugin directory is in place for tests that require it
+directory slurm_plugin_dir do
+  user 'root'
+  group 'root'
+  mode '0755'
+  action :create
+  recursive true
+end
+
+# skips the fake export on docker
 return if on_docker?
 
-slurm_install_dir = '/opt/slurm'
 directory slurm_install_dir do
   mode '1777'
 end
