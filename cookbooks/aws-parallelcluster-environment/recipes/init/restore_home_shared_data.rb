f905257af649398789efd094a3b9fe3495181439
Ryan Anderson <ndry@amazon.com>
2023-10-10 13:44:59 -0400

    [Develop] Add support for an external shared /home (#2486)
    Users can now specify `/home` as a mount point in SharedStorage.  This was previously a reserved directory
    When a user specifies this, the data in `/home` is transferred without replacement to the external filesystem.
    This means that in order to share the `/home` directory across clusters, users must specify the same security
    credentials when creating the clusters.

---
+++ cookbooks/aws-parallelcluster-environment/recipes/init/restore_home_shared_data.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+#
+# Copyright:: 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the
+# License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+
+return if on_docker?
+
+if node['cluster']['node_type'] == 'HeadNode'
+  # Restore the shared storage home data if it doesn't already exist
+  # This is necessary to preserve any data in these directories that was
+  # generated during the build of ParallelCluster AMIs after converting to
+  # shared storage and backed up to a temporary location previously
+  # Remove the backup after the copy is done
+  bash "Restore /home" do
+    user 'root'
+    group 'root'
+    code <<-EOH
+      rsync -a --ignore-existing /tmp/home/ /home
+      rm -rf /tmp/home/
+    EOH
+  end
+end
