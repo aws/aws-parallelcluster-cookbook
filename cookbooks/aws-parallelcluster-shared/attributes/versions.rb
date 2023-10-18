57360b0d348f97c9dfa15035b64edcecb161e068
Jacopo De Amicis <jdamicis@amazon.it>
2023-10-18 17:17:28 +0200

    Bump version to 3.8.0b1
    Signed-off-by: Jacopo De Amicis <jdamicis@amazon.it>

--- cookbooks/aws-parallelcluster-shared/attributes/versions.rb
+++ cookbooks/aws-parallelcluster-shared/attributes/versions.rb
@@ -2,7 +2,7 @@
 default['cluster']['python-version'] = '3.9.17'
 
 # ParallelCluster versions
-default['cluster']['parallelcluster-version'] = '3.8.0'
-default['cluster']['parallelcluster-cookbook-version'] = '3.8.0'
-default['cluster']['parallelcluster-node-version'] = '3.8.0'
-default['cluster']['parallelcluster-awsbatch-cli-version'] = '1.1.0'
+default['cluster']['parallelcluster-version'] = '3.8.0b1'
+default['cluster']['parallelcluster-cookbook-version'] = '3.8.0b1'
+default['cluster']['parallelcluster-node-version'] = '3.8.0b1'
+default['cluster']['parallelcluster-awsbatch-cli-version'] = '1.2.0'
