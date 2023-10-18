a99b4aa0a79e92328d447075f77e090500cedef7
Jacopo De Amicis <jdamicis@amazon.it>
2023-10-18 13:51:55 +0200

    Bump version to 3.9.0
    Signed-off-by: Jacopo De Amicis <jdamicis@amazon.it>

--- cookbooks/aws-parallelcluster-computefleet/metadata.rb
+++ cookbooks/aws-parallelcluster-computefleet/metadata.rb
@@ -7,11 +7,11 @@ description 'AWS ParallelCluster compute fleet management'
 issues_url 'https://github.com/aws/aws-parallelcluster-cookbook/issues'
 source_url 'https://github.com/aws/aws-parallelcluster-cookbook'
 chef_version '>= 18'
-version '3.8.0'
+version '3.9.0'
 
 supports 'amazon', '= 2.0'
 supports 'centos', '= 7.0'
 supports 'ubuntu', '>= 20.04'
 supports 'redhat', '= 8.7'
 
-depends 'aws-parallelcluster-shared', '~> 3.8.0'
+depends 'aws-parallelcluster-shared', '~> 3.9.0'
