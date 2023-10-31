8febe6703bd55286857d639d31bc3adcc7cbc7cb
Xuanqi He <xuanqihe@amazon.com>
2023-10-31 16:36:57 -0400

    Correct a wrongly use lazy issue

--- cookbooks/aws-parallelcluster-slurm/recipes/config/config_check_login_stopped_script.rb
+++ cookbooks/aws-parallelcluster-slurm/recipes/config/config_check_login_stopped_script.rb
@@ -14,9 +14,6 @@
 
 require 'digest'
 
-cluster_name = node['cluster']['cluster_name'] || node['cluster']['stack_name']
-pool_name = lazy { node['cluster']['config'].dig(:LoginNodes, :Pools, 0, :Name) }
-
 def get_target_group_name(cluster_name, pool_name)
   partial_cluster_name = cluster_name[0..6]
   partial_pool_name = pool_name[0..6]
@@ -25,15 +22,18 @@ def get_target_group_name(cluster_name, pool_name)
   "#{partial_cluster_name}-#{partial_pool_name}-#{hash_value}"
 end
 
-target_group_name = get_target_group_name(cluster_name, pool_name)
-
 template "#{node['cluster']['scripts_dir']}/slurm/check_login_nodes_stopped.sh" do
   source 'slurm/head_node/check_login_nodes_stopped.sh.erb'
   owner 'root'
   group 'root'
   mode '0700'
   variables(
-    target_group_name: target_group_name,
+    target_group_name: lazy {
+      get_target_group_name(
+        node['cluster']['cluster_name'] || node['cluster']['stack_name'],
+        node['cluster']['config'].dig(:LoginNodes, :Pools, 0, :Name)
+      )
+    },
     region: node['cluster']['region']
   )
   only_if do
