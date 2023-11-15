581919472c95a1bb688fe3c220ebcf091ab9f8b5
hgreebe <141743196+hgreebe@users.noreply.github.com>
2023-11-15 09:53:29 -0500

    Use jinja templating to refactor cloudwatch config to make is easier to add and remove an OS (#2534)
    * Use jinja templating to refactor cloudwatch config to make is easier to add and remove OSes
    
    * Modify test to account for changes to cloudwatch_agent_config.json
    
    * Account for jinja templating when validating the cloudwatch agent json
    
    * Add common cloudwatch agent module

---
+++ cookbooks/aws-parallelcluster-environment/files/cloudwatch/cloudwatch_agent_common_utils.py
@@ -0,0 +1,17 @@
+import os
+
+from jinja2 import FileSystemLoader
+from jinja2.sandbox import SandboxedEnvironment
+
+CONFIG_ARGS = {
+    "default_platforms": ["amazon", "centos", "redhat", "rocky", "ubuntu"],
+}
+
+
+def render_jinja_template(template_file_path):
+    file_loader = FileSystemLoader(str(os.path.dirname(template_file_path)))
+    env = SandboxedEnvironment(loader=file_loader)
+    rendered_template = env.get_template(os.path.basename(template_file_path)).render(**CONFIG_ARGS)
+    with open(template_file_path, "w", encoding="utf-8") as f:
+        f.write(rendered_template)
+    return template_file_path
