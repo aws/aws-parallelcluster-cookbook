07491347308358c314d87d409acf9d32c80e416b
Luca Carrogu <carrogu@amazon.com>
2023-11-23 15:02:27 +0100

    Add checksum check on Munge download
    Add checksum check on Munge download
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
+++ cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
@@ -6,3 +6,5 @@ default['cluster']['slurm']['sha256'] = '3780773a80b73ea2edb4353318b4220188f4eda
 default['cluster']['slurm']['base_url'] = "https://github.com/SchedMD/slurm/archive"
 # Munge
 default['cluster']['munge']['munge_version'] = '0.5.15'
+default['cluster']['munge']['sha256'] = '51b2c81d1a7ec2ab5d486fa51b50c7e79eb1810ca6687b6ed65f3601abc55614'
+default['cluster']['munge']['base_url'] = "https://github.com/dun/munge/archive"
