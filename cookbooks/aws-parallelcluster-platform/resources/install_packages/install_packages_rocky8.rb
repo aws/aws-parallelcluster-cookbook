024a18cf402f19c63867eeaab754e447e9cf91af
Enrico Usai <usai@amazon.com>
2023-12-14 12:42:58 +0100

    Avoid to exit while trying to install a specific kernel-devel version
    This code has been created to try to install using `--releasever` parameter
    and if this command fails try to install the package from vault,
    anyway the `set -e` at the top of the failing was causing all the build to fail.
    
    Manually tested in a Rocky-8-EC2-Base-8.8-20230518.0.x86_64
    
    Related to: https://github.com/aws/aws-parallelcluster-cookbook/pull/2597
    
    Signed-off-by: Enrico Usai <usai@amazon.com>

--- cookbooks/aws-parallelcluster-platform/resources/install_packages/install_packages_rocky8.rb
+++ cookbooks/aws-parallelcluster-platform/resources/install_packages/install_packages_rocky8.rb
@@ -23,12 +23,12 @@ action :install_kernel_source do
   bash "Install kernel source" do
     user 'root'
     code <<-INSTALL_KERNEL_SOURCE
-    set -e
     package="#{kernel_source_package}-#{kernel_source_package_version}"
 
     # try to install kernel source for a specific release version
     dnf install -y ${package} --releasever #{node['platform_version']}
     if [ $? -ne 0 ]; then
+      set -e
       # Previous releases are moved into a vault area once a new minor release version is available for at least a week.
       # https://wiki.rockylinux.org/rocky/repo/#notes-on-devel
       wget https://dl.rockylinux.org/vault/rocky/#{node['platform_version']}/BaseOS/$(uname -m)/os/Packages/k/${package}.rpm
