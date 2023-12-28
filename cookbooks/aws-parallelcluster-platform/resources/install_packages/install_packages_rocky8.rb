d586522858532e1399dec40f14c08947f575ef06
Hanwen <hanwenli@amazon.com>
2023-12-28 09:20:03 -0800

    Packages installation changes for RHEL8, RHEL9, Rocky8, Rocky9
    Remove `pyparted`, `redhat-lsb`, `python2`, `python2-pip` installations because they are not available in RHEL9 and Rocky9.
    
    Add perl, dbus-devel installation because they were part of `redhat-lsb` and are dependencies of Slurm
    
    We are doing the same on RHEL8, RHEL9, Rocky8, Rocky9 to keep consistencies across OSes.
    
    Reference:
    pyparted: https://github.com/dcantrell/pyparted
    redhat-lsb: https://access.redhat.com/solutions/6973382
    perl: https://www.perl.org
    dbus-devel: https://packages.fedoraproject.org/pkgs/dbus/dbus-devel/
    
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- cookbooks/aws-parallelcluster-platform/resources/install_packages/install_packages_rocky8.rb
+++ cookbooks/aws-parallelcluster-platform/resources/install_packages/install_packages_rocky8.rb
@@ -48,11 +48,11 @@ def default_packages
   # environment-modules required by EFA, Intel MPI and ARM PL
   # iptables needed for IMDS setup
   packages = %w(vim ksh tcsh zsh openssl-devel ncurses-devel pam-devel net-tools openmotif-devel
-     libXmu-devel hwloc-devel libdb-devel tcl-devel automake autoconf pyparted libtool
-     httpd boost-devel redhat-lsb mlocate R atlas-devel
+     libXmu-devel hwloc-devel libdb-devel tcl-devel automake autoconf libtool
+     httpd boost-devel mlocate R atlas-devel
      blas-devel libffi-devel dkms libedit-devel jq
      libical-devel sendmail libxml2-devel libglvnd-devel
-     python2 python2-pip libgcrypt-devel libevent-devel glibc-static bind-utils
+     libgcrypt-devel libevent-devel glibc-static bind-utils
      iproute NetworkManager-config-routing-rules python3 python3-pip iptables libcurl-devel yum-plugin-versionlock
      moreutils curl environment-modules gcc gcc-c++ bzip2)
   packages.append("coreutils") unless on_docker?  # on docker image coreutils conflict with coreutils-single, already installed on it
