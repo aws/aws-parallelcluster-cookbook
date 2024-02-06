2f254e5e531bc9a0b855a211c9eacf956d2b7e73
Giacomo Marciani <mgiacomo@amazon.com>
2024-02-06 15:46:16 +0100

    [DFSM] Make login nodes update shared storage as part of the update workflow
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

--- cookbooks/aws-parallelcluster-platform/resources/fetch_config.rb
+++ cookbooks/aws-parallelcluster-platform/resources/fetch_config.rb
@@ -13,7 +13,8 @@ action :run do
   return if on_docker?
   Chef::Log.debug("Called fetch_config with update (#{new_resource.update})")
 
-  sync_file = "#{node['cluster']['shared_dir']}/cluster-config-version"
+  sync_file_compute_nodes = "#{node['cluster']['shared_dir']}/cluster-config-version"
+  sync_file_login_nodes = "#{node['cluster']['shared_dir_login_nodes']}/cluster-config-version"
 
   case node['cluster']['node_type']
   when 'HeadNode'
@@ -35,18 +36,19 @@ action :run do
     end
 
     # ensure config is shared also with login nodes
-    share_config_with_login_nodes
+    share_config_with_login_nodes # TODO: Improvement - do this only if cluster has login nodes
 
     # load cluster config into a node object
     load_cluster_config(node['cluster']['cluster_config_path'])
 
     # Write config version file to signal other cluster nodes that all configuration files within the shared folder
     # are aligned with the latest config version.
-    write_config_version_file(sync_file)
+    write_config_version_file(sync_file_compute_nodes)
+    write_config_version_file(sync_file_login_nodes) # TODO: Improvement - do this only if cluster has login nodes
   when 'ComputeFleet'
     if kitchen_test?
       fetch_cluster_config(node['cluster']['cluster_config_path']) unless ::File.exist?(node['cluster']['cluster_config_path'])
-      write_config_version_file(sync_file)
+      write_config_version_file(sync_file_compute_nodes)
     end
 
     if new_resource.update
@@ -54,7 +56,7 @@ action :run do
       # all configuration files within the shared folder are aligned with the latest config version.
       # This is required only on update because on create it is guaranteed that this recipe is executed on compute node
       # only after it has completed on head node.
-      wait_cluster_config_file(sync_file)
+      wait_cluster_config_file(sync_file_compute_nodes)
 
       # TODO: If the shared storage mapping files contain cluster-wide information and not node-specific data,
       #  then make the head node write the shared storage mapping files in the shared folder
@@ -68,7 +70,26 @@ action :run do
     # load cluster config into a node object
     load_cluster_config(node['cluster']['cluster_config_path'])
   when 'LoginNode'
-    raise "Cluster config not found in #{node['cluster']['login_cluster_config_path']}" unless ::File.exist?(node['cluster']['login_cluster_config_path'])
+    if kitchen_test?
+      fetch_cluster_config(node['cluster']['login_cluster_config_path']) unless ::File.exist?(node['cluster']['login_cluster_config_path'])
+      write_config_version_file(sync_file_login_nodes)
+    end
+
+    if new_resource.update
+      # Wait for the head node to write the config version file, which is the signal that
+      # all configuration files within the shared folder are aligned with the latest config version.
+      # This is required only on update because on create it is guaranteed that this recipe is executed on compute node
+      # only after it has completed on head node.
+      wait_cluster_config_file(sync_file_login_nodes)
+
+      # TODO: If the shared storage mapping files contain cluster-wide information and not node-specific data,
+      #  then make the head node write the shared storage mapping files in the shared folder
+      #  and let compute node consume them.
+      Chef::Log.info("Backing up old shared storages data from (#{node['cluster']['shared_storages_mapping_path']}) to (#{node['cluster']['previous_shared_storages_mapping_path']})")
+      ::FileUtils.cp_r(node['cluster']['shared_storages_mapping_path'], node['cluster']['previous_shared_storages_mapping_path'], remove_destination: true)
+    else
+      raise "Cluster config not found in #{node['cluster']['login_cluster_config_path']}" unless ::File.exist?(node['cluster']['login_cluster_config_path'])
+    end
     # load cluster config into a node object
     load_cluster_config(node['cluster']['login_cluster_config_path'])
   else
--- cookbooks/aws-parallelcluster-slurm/recipes/update/update_login_node.rb
+++ cookbooks/aws-parallelcluster-slurm/recipes/update/update_login_node.rb
@@ -15,4 +15,13 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
+# TODO: Move the only_if decision to the update_shared_storage recipe for better definition of responsibilities
+#  and to facilitate unit testing.
+ruby_block "update_shared_storages" do
+  block do
+    run_context.include_recipe 'aws-parallelcluster-environment::update_shared_storages'
+  end
+  only_if { are_mount_or_unmount_required? }
+end
+
 save_instance_config_version_to_dynamodb
