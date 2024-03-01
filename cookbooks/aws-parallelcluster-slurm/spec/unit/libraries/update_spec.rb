df36eb801a5d6f6354f7361e0c13d55967b17169
Giacomo Marciani <mgiacomo@amazon.com>
2024-03-01 10:55:33 +0100

    [DFSM] Make the check 'are_mount_or_unmount_required?' resilient to the lack of the changeset file.
    In particular, we consider mount/unmount not required if the change-set file is missing.
    
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

--- cookbooks/aws-parallelcluster-slurm/spec/unit/libraries/update_spec.rb
+++ cookbooks/aws-parallelcluster-slurm/spec/unit/libraries/update_spec.rb
@@ -1,18 +1,31 @@
 require 'spec_helper'
 
 describe "aws-parallelcluster-slurm:libraries:are_mount_unmount_required" do
+  CHANGE_SET_PATH = "/CHANGE_SET_PATH"
+
   let(:node) do
     {
-      "cluster" => { "shared_dir" => "/SHARED_DIR" },
+      "cluster" => { "change_set_path" => CHANGE_SET_PATH },
     }
   end
 
   shared_examples "the correct method" do |changeset, expected_result|
     it "returns #{expected_result}" do
-      allow(File).to receive(:read).with("/SHARED_DIR/change-set.json").and_return(JSON.dump(changeset))
+      if changeset.nil?
+        allow(File).to receive(:exist?).with(CHANGE_SET_PATH).and_return(false)
+        allow(File).to receive(:read).with(CHANGE_SET_PATH).and_call_original
+      else
+        allow(File).to receive(:exist?).with(CHANGE_SET_PATH).and_return(true)
+        allow(File).to receive(:read).with(CHANGE_SET_PATH).and_return(JSON.dump(changeset))
+      end
       result = are_mount_or_unmount_required?
       expect(result).to eq(expected_result)
     end
+  end
+
+  context "when changeset does not exist" do
+    changeset = nil
+    include_examples "the correct method", changeset, false
   end
 
   context "when changeset is empty" do
