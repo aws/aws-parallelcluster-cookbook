578098e3af63da578475451de56cc5fd6f5d2fa3
Hanwen <hanwenli@amazon.com>
2024-04-01 13:44:03 -0700

    Remove unnecessary code in external_slurmdbd_config
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- cookbooks/aws-parallelcluster-entrypoints/recipes/external_slurmdbd_config.rb
+++ cookbooks/aws-parallelcluster-entrypoints/recipes/external_slurmdbd_config.rb
@@ -12,34 +12,6 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Mock necessary attributes to reuse the recipes from ParallelCluster.
-node.default['cluster']['slurm']['install_dir'] = '/opt/slurm'
-node.default['cluster']['slurm']['user'] = 'slurm'
-node.default['cluster']['slurm']['group'] = 'slurm'
-node.default['cluster']['scripts_dir'] = '/opt/parallelcluster/scripts'
-node.default['cluster']['region'] = node['region']
-node.default['cluster']['slurmdbd_response_retries'] = 30
-node.default['cluster']['stack_name'] = node['stack_name']
-node.default['cluster']['munge']['user'] = 'munge'
-node.default['cluster']['munge']['group'] = node['cluster']['munge']['user']
-node.default['cluster']['node_type'] = 'ExternalSlurmDbd' # force node_type to ExternalSlurmDbd to configure CW agent
-node.default['cluster']['cw_logging_enabled'] = 'true' # enable CW agent logging
-node.default['cluster']['log_group_name'] = node['log_group_name'] # map the `log_group_name` coming from the dna.json to the proper variable expected by the recipes
-
-# rubocop:disable Lint/DuplicateBranch
-if platform?('amazon') && node['platform_version'] == "2"
-  node.default['cluster']['cluster_user'] = 'ec2-user'
-elsif platform?('centos') && node['platform_version'].to_i == 7
-  node.default['cluster']['cluster_user'] = 'centos'
-elsif platform?('redhat')
-  node.default['cluster']['cluster_user'] = 'ec2-user'
-elsif platform?('rocky')
-  node.default['cluster']['cluster_user'] = 'rocky'
-elsif platform?('ubuntu')
-  node.default['cluster']['cluster_user'] = 'ubuntu'
-end
-# rubocop:enable Lint/DuplicateBranch
-
 include_recipe 'aws-parallelcluster-slurm::config_head_node_directories'
 
 include_recipe 'aws-parallelcluster-slurm::external_slurmdbd_disable_unrequired_services'
