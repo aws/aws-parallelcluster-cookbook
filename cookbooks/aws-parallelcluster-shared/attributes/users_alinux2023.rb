750eef8adcecf3d065f8d9680f95973f22ed4c47
Himani Deshpande <himanidp@amazon.com>
2024-04-22 13:00:30 -0400

    [AL2023] Supporting Cluster User for Al2023

--- cookbooks/aws-parallelcluster-shared/attributes/users_alinux2023.rb
+++ cookbooks/aws-parallelcluster-shared/attributes/users_alinux2023.rb
@@ -1,3 +1,5 @@
 return unless platform?('amazon') && node['platform_version'].to_i == 2023
 
 default['cluster']['cluster_user'] = 'ec2-user'
+default['cluster']['cluster_user_home'] = "/home/#{node['cluster']['cluster_user']}"
+default['cluster']['cluster_user_local_home'] = "/local#{node['cluster']['cluster_user_home']}"
