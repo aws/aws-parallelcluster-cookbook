686f936d408e8ddb7c0515f754ba31862b7c5b52
Himani Deshpande <himanidp@amazon.com>
2024-04-25 20:37:58 -0400

    [AL2023] Adding installation of rsyslog and enabling the service

---
+++ cookbooks/aws-parallelcluster-platform/test/controls/enable_services_spec.rb
@@ -0,0 +1,9 @@
+control 'tag:install_service_is_enabled' do
+  only_if { !os_properties.on_docker? }
+
+  describe service('rsyslog') do
+    it { should be_installed }
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
