9fdd3a6ade93d403d9352355a2b123602b98937d
Giacomo Marciani <mgiacomo@amazon.com>
2024-05-30 17:38:37 +0200

    [Storage] Fix method used to get the AWS domain for FSx. In particular, now it does not need anymore to access the undefined node attribute in US ISO regions.
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

--- cookbooks/aws-parallelcluster-environment/libraries/fsx.rb
+++ cookbooks/aws-parallelcluster-environment/libraries/fsx.rb
@@ -1,4 +1,10 @@
 def aws_domain_for_fsx(region)
   # DNS names have the default AWS domain (amazonaws.com) also in China and GovCloud.
-  region.start_with?("us-iso") ? aws_domain : CLASSIC_AWS_DOMAIN
+  if region.start_with?("us-iso-")
+    US_ISO_AWS_DOMAIN
+  elsif region.start_with?("us-isob-")
+    US_ISOB_AWS_DOMAIN
+  else
+    CLASSIC_AWS_DOMAIN
+  end
 end
