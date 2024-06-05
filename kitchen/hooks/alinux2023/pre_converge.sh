5743cf4e987276e418debf8d496d96f265418e11
Hanwen <hanwenli@amazon.com>
2024-06-05 07:41:13 -0700

    [AL2023] remove prehook
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- kitchen/hooks/alinux2023/pre_converge.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -n "${KITCHEN_INSTANCE_HOSTNAME}" ]; then
-    export KITCHEN_EC2_USER='ec2-user'
-    echo "Install libxcrypt-compat package by using SSH key: ${KITCHEN_SSH_KEY_PATH}"
-
-    KITCHEN_EC2_INSTANCE_ID=$(ssh -o StrictHostKeyChecking=no -i "${KITCHEN_SSH_KEY_PATH}" \
-      "${KITCHEN_EC2_USER}@${KITCHEN_INSTANCE_HOSTNAME}" 'sudo yum install -y libxcrypt-compat')
-fi
