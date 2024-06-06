54582ac8149647bd6f0662f96a9748d15c5b3fd1
Hanwen <hanwenli@amazon.com>
2024-06-06 07:23:41 -0700

    [Dependencies] Upgrade munge to 0.5.16.
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
+++ cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
@@ -5,6 +5,6 @@ default['cluster']['slurm']['branch'] = ''
 default['cluster']['slurm']['sha256'] = 'b25127efd69a47c14bd65dfa3bff2687b5350c5290eafb601f923faebe6fd238'
 default['cluster']['slurm']['base_url'] = "https://github.com/SchedMD/slurm/archive"
 # Munge
-default['cluster']['munge']['munge_version'] = '0.5.15'
-default['cluster']['munge']['sha256'] = '51b2c81d1a7ec2ab5d486fa51b50c7e79eb1810ca6687b6ed65f3601abc55614'
+default['cluster']['munge']['munge_version'] = '0.5.16'
+default['cluster']['munge']['sha256'] = 'fa27205d6d29ce015b0d967df8f3421067d7058878e75d0d5ec3d91f4d32bb57'
 default['cluster']['munge']['base_url'] = "https://github.com/dun/munge/archive"
