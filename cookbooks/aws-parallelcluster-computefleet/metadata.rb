925a582ae74c70824de45625afef745da7ddf3b8
Hanwen <hanwenli@amazon.com>
2024-06-07 08:48:54 -0700

    Remove support information in metadata
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- cookbooks/aws-parallelcluster-computefleet/metadata.rb
+++ cookbooks/aws-parallelcluster-computefleet/metadata.rb
@@ -9,9 +9,4 @@ source_url 'https://github.com/aws/aws-parallelcluster-cookbook'
 chef_version '>= 18'
 version '3.10.0'
 
-supports 'amazon', '>= 2.0'
-supports 'centos', '= 7.0'
-supports 'ubuntu', '>= 20.04'
-supports 'redhat', '= 8.7'
-
 depends 'aws-parallelcluster-shared', '~> 3.10.0'
