33fd1d040fed1f30c04d1b35a90657a138ddf615
Hanwen <hanwenli@amazon.com>
2024-06-07 08:48:54 -0700

    Remove support information in metadata
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- cookbooks/aws-parallelcluster-platform/metadata.rb
+++ cookbooks/aws-parallelcluster-platform/metadata.rb
@@ -9,11 +9,6 @@ source_url 'https://github.com/aws/aws-parallelcluster-cookbook'
 chef_version '>= 18'
 version '3.10.0'
 
-supports 'amazon', '>= 2.0'
-supports 'centos', '= 7.0'
-supports 'ubuntu', '>= 20.04'
-supports 'redhat', '= 8.7'
-
 depends 'line', '~> 4.5.13'
 
 depends 'aws-parallelcluster-shared', '~> 3.10.0'
--- cookbooks/aws-parallelcluster-shared/metadata.rb
+++ cookbooks/aws-parallelcluster-shared/metadata.rb
@@ -9,11 +9,6 @@ source_url 'https://github.com/aws/aws-parallelcluster-cookbook'
 chef_version '>= 18'
 version '3.10.0'
 
-supports 'amazon', '>= 2.0'
-supports 'centos', '= 7.0'
-supports 'ubuntu', '>= 20.04'
-supports 'redhat', '= 8.7'
-
 depends 'pyenv', '~> 4.2.3'
 depends 'yum', '~> 7.4.13'
 depends 'yum-epel', '~> 5.0.2'
