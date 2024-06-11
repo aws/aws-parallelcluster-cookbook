b5cb46db8923fefba0081d8231b933f74418e03f
Giacomo Marciani <mgiacomo@amazon.com>
2024-06-11 19:20:02 +0200

    [Versioning] Bump version to 3.9.3.
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

--- cookbooks/aws-parallelcluster-shared/attributes/versions.rb
+++ cookbooks/aws-parallelcluster-shared/attributes/versions.rb
@@ -2,7 +2,7 @@
 default['cluster']['python-version'] = '3.9.17'
 
 # ParallelCluster versions
-default['cluster']['parallelcluster-version'] = '3.9.2'
-default['cluster']['parallelcluster-cookbook-version'] = '3.9.2'
-default['cluster']['parallelcluster-node-version'] = '3.9.2'
+default['cluster']['parallelcluster-version'] = '3.9.3'
+default['cluster']['parallelcluster-cookbook-version'] = '3.9.3'
+default['cluster']['parallelcluster-node-version'] = '3.9.3'
 default['cluster']['parallelcluster-awsbatch-cli-version'] = '1.2.0'
