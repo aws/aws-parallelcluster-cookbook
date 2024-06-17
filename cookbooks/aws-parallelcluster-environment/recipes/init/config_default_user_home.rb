c12e053109641a837bb80b0641942acb5a8d0eca
Xuanqi He <xuanqihe@amazon.com>
2024-06-17 13:17:28 -0400

    Minor changes on comments and variable names.

--- cookbooks/aws-parallelcluster-environment/recipes/init/config_default_user_home.rb
+++ cookbooks/aws-parallelcluster-environment/recipes/init/config_default_user_home.rb
@@ -66,8 +66,8 @@ end
 # 1. Verifies data integrity by comparing the original home directory and the new local home directory.
 # 2. If the data integrity check passes, it removes both the temporary backup directory and the original home directory.
 # 3. If the data integrity check fails, it outputs an error message and exits with an error code 1.
-# To avoid any confusion, ['cluster_user_home'] is the original dir, ['cluster_user_local_home'] is the new replaced dir
-bash "Verify data integrity for #{node['cluster']['cluster_user_home']}" do
+# To avoid any confusion, ['cluster_user_home'] is the original dir, ['cluster_user_local_home'] is the destination dir.
+bash "Verify data integrity for #{node['cluster']['cluster_user_local_home']}" do
   user 'root'
   group 'root'
   code <<-EOH
--- cookbooks/aws-parallelcluster-environment/spec/unit/recipes/config_default_user_home_spec.rb
+++ cookbooks/aws-parallelcluster-environment/spec/unit/recipes/config_default_user_home_spec.rb
@@ -24,18 +24,18 @@ describe 'aws-parallelcluster-environment::config_default_user_home' do
         end
 
         it 'moves the cluster user home directory with data integrity check' do
-          # To avoid any confusion, user_home is the original dir, user_local_home is the new replaced dir
-          user_home = "/home/user"
-          user_local_home = "/local/home/user"
-          expect(chef_run).to run_bash("Verify data integrity for #{user_home}").with(
+          # To avoid any confusion, user_home is the original dir, user_local_home is the destination dir.
+          original_user_home = "/home/user"
+          destination_user_local_home = "/local/home/user"
+          expect(chef_run).to run_bash("Verify data integrity for #{destination_user_local_home}").with(
             code: <<-CODE
-    diff_output=$(diff -r #{user_home} #{user_local_home})
-    if [[ $diff_output != *"Only in #{user_home}"* ]]; then
-      rm -rf /tmp#{user_home}
-      rm -rf #{user_home}
+    diff_output=$(diff -r #{original_user_home} #{destination_user_local_home})
+    if [[ $diff_output != *"Only in #{original_user_home}"* ]]; then
+      rm -rf /tmp#{original_user_home}
+      rm -rf #{original_user_home}
     else
-      only_in_cluster_user_home=$(echo "$diff_output" | grep "Only in #{user_home}")
-      echo "Data integrity check failed comparing #{user_local_home} and #{user_home}. Differences:"
+      only_in_cluster_user_home=$(echo "$diff_output" | grep "Only in #{original_user_home}")
+      echo "Data integrity check failed comparing #{destination_user_local_home} and #{original_user_home}. Differences:"
       echo "$only_in_cluster_user_home"
       systemctl start sshd
       exit 1
