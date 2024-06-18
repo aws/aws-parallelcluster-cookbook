326dbec100477172a2312fd350dcb067b74679b4
Hanwen <hanwenli@amazon.com>
2024-06-18 12:54:25 -0700

    [AL2023] Configure NFS server the same way as AL2
    This was discovered by a kitchen test checking NFS thread. After the commit, the kitchen test has been passed
    
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- cookbooks/aws-parallelcluster-environment/resources/nfs/nfs_alinux2023.rb
+++ cookbooks/aws-parallelcluster-environment/resources/nfs/nfs_alinux2023.rb
@@ -31,7 +31,8 @@ end
 action_class do
   def override_server_template
     edit_resource(:template, node['nfs']['config']['server_template']) do
-      cookbook 'nfs'
+      source 'nfs/default-nfs-kernel-server.conf.erb'
+      cookbook 'aws-parallelcluster-environment'
     end
   end
 end
