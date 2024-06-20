aws/aws-parallelcluster-cookbook

commit 581c14bcad073061b497065fc4413265b7555079

Fix image build failure with RHEL 8.10 or newer
Signed-off-by: Hanwen <hanwenli@amazon.com>

cookbooks/aws-parallelcluster-environment/resources/lustre/lustre_redhat8.rb
@@ -26,14 +26,16 @@ default_action :setup
 action :setup do
   version = node['platform_version']
   log "Installing FSx for Lustre. Platform version: #{version}, kernel version: #{node['cluster']['kernel_release']}"
-  if version.to_f < 8.2
-    raise "FSx for Lustre is not supported in this RHEL version #{version}, supported versions are >= 8.2"
-  elsif version.to_f == 8.7 && (node['cluster']['kernel_release'].include?("4.18.0-425.3.1.el8") || node['cluster']['kernel_release'].include?("4.18.0-425.13.1.el8_7"))
-    # Rhel8.7 kernel 4.18.0-425.3.1.el8 and 4.18.0-425.13.1.el8_7 has broken kABI compat
-    # See https://access.redhat.com/solutions/6985596 and https://github.com/openzfs/zfs/issues/14724
-    raise "FSx for Lustre is not supported in kernel version #{node['cluster']['kernel_release']} of RHEL #{version}, please update the kernel version"
-  else
-    action_install_lustre
+  if version.length == 3 # If version is 8.10 or more, this block is skipped
+    if version.to_f < 8.2
+      raise "FSx for Lustre is not supported in this RHEL version #{version}, supported versions are >= 8.2"
+    elsif version.to_f == 8.7 && (node['cluster']['kernel_release'].include?("4.18.0-425.3.1.el8") || node['cluster']['kernel_release'].include?("4.18.0-425.13.1.el8_7"))
+      # Rhel8.7 kernel 4.18.0-425.3.1.el8 and 4.18.0-425.13.1.el8_7 has broken kABI compat
+      # See https://access.redhat.com/solutions/6985596 and https://github.com/openzfs/zfs/issues/14724
+      raise "FSx for Lustre is not supported in kernel version #{node['cluster']['kernel_release']} of RHEL #{version}, please update the kernel version"
+    else
+      action_install_lustre
+    end
   end
 end
 

cookbooks/aws-parallelcluster-environment/resources/lustre/lustre_rocky8.rb
@@ -26,14 +26,16 @@ default_action :setup
 action :setup do
   version = node['platform_version']
   log "Installing FSx for Lustre. Platform version: #{version}, kernel version: #{node['cluster']['kernel_release']}"
-  if version.to_f < 8.2
-    raise "FSx for Lustre is not supported in this Rocky Linux version #{version}, supported versions are >= 8.2"
-  elsif version.to_f == 8.7 && (node['cluster']['kernel_release'].include?("4.18.0-425.3.1.el8") || node['cluster']['kernel_release'].include?("4.18.0-425.13.1.el8_7"))
-    # Rhel8.7 kernel 4.18.0-425.3.1.el8 and 4.18.0-425.13.1.el8_7 has broken kABI compat
-    # See https://access.redhat.com/solutions/6985596 and https://github.com/openzfs/zfs/issues/14724
-    raise "FSx for Lustre is not supported in kernel version #{node['cluster']['kernel_release']} of Rocky Linux #{version}, please update the kernel version"
-  else
-    action_install_lustre
+  if version.length == 3 # If version is 8.10 or more, this block is skipped
+    if version.to_f < 8.2
+      raise "FSx for Lustre is not supported in this Rocky Linux version #{version}, supported versions are >= 8.2"
+    elsif version.to_f == 8.7 && (node['cluster']['kernel_release'].include?("4.18.0-425.3.1.el8") || node['cluster']['kernel_release'].include?("4.18.0-425.13.1.el8_7"))
+      # Rhel8.7 kernel 4.18.0-425.3.1.el8 and 4.18.0-425.13.1.el8_7 has broken kABI compat
+      # See https://access.redhat.com/solutions/6985596 and https://github.com/openzfs/zfs/issues/14724
+      raise "FSx for Lustre is not supported in kernel version #{node['cluster']['kernel_release']} of Rocky Linux #{version}, please update the kernel version"
+    else
+      action_install_lustre
+    end
   end
 end
 

kitchen.docker.yml
@@ -70,7 +70,7 @@ platforms:
         kernel_release: '5.15.0-1028-aws'
   - name: rhel8
     driver:
-      image: <% if ENV['KITCHEN_RHEL8_IMAGE'] %> <%= ENV['KITCHEN_RHEL8_IMAGE'] %> <% else %> registry.access.redhat.com/ubi8/ubi:8.9 <% end %>
+      image: <% if ENV['KITCHEN_RHEL8_IMAGE'] %> <%= ENV['KITCHEN_RHEL8_IMAGE'] %> <% else %> registry.access.redhat.com/ubi8/ubi <% end %>
       intermediate_instructions:
         - RUN chmod +t /tmp
     attributes:
@@ -81,7 +81,7 @@ platforms:
         kernel_release: '4.18.0-477.13.1.el8_7.fake-value' # Use 477 version to match 8.8 kernel version available on docker
   - name: rocky8
     driver:
-      image: <% if ENV['KITCHEN_ROCKY8_IMAGE'] %> <%= ENV['KITCHEN_ROCKY8_IMAGE'] %> <% else %> dokken/rockylinux-8:sha-f885abd <% end %>
+      image: <% if ENV['KITCHEN_ROCKY8_IMAGE'] %> <%= ENV['KITCHEN_ROCKY8_IMAGE'] %> <% else %> dokken/rockylinux-8 <% end %>
     attributes:
       cluster:
         base_os: rocky8

commit 673f874d49fb3fd987b9240779a94a3f46e243f8

Fix Lustre not installed on RHEL 8.10
This commit fixes a bug introduced by https://github.com/aws/aws-parallelcluster-cookbook/pull/2764

Signed-off-by: Hanwen <hanwenli@amazon.com>

cookbooks/aws-parallelcluster-environment/resources/lustre/lustre_redhat8.rb
@@ -36,6 +36,8 @@ action :setup do
     else
       action_install_lustre
     end
+  else
+    action_install_lustre
   end
 end
 

cookbooks/aws-parallelcluster-environment/resources/lustre/lustre_rocky8.rb
@@ -36,6 +36,8 @@ action :setup do
     else
       action_install_lustre
     end
+  else
+    action_install_lustre
   end
 end
 