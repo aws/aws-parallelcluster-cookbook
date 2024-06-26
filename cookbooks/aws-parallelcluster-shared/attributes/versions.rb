a2b461399f399cabc281bcfd2a888742a9a82870
Hanwen <hanwenli@amazon.com>
2024-06-26 14:04:38 -0700

    Bump version to 3.11.0
    Signed-off-by: Hanwen <hanwenli@amazon.com>

--- cookbooks/aws-parallelcluster-shared/attributes/versions.rb
+++ cookbooks/aws-parallelcluster-shared/attributes/versions.rb
@@ -3,7 +3,7 @@ default['cluster']['python-version'] = '3.9.19'
 default['cluster']['python-major-minor-version'] = '3.9'
 
 # ParallelCluster versions
-default['cluster']['parallelcluster-version'] = '3.10.0'
-default['cluster']['parallelcluster-cookbook-version'] = '3.10.0'
-default['cluster']['parallelcluster-node-version'] = '3.10.0'
-default['cluster']['parallelcluster-awsbatch-cli-version'] = '1.3.0'
+default['cluster']['parallelcluster-version'] = '3.11.0'
+default['cluster']['parallelcluster-cookbook-version'] = '3.11.0'
+default['cluster']['parallelcluster-node-version'] = '3.11.0'
+default['cluster']['parallelcluster-awsbatch-cli-version'] = '1.4.0'
