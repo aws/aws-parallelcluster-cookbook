9eaf7326c5a72da81cf99c324d6488262b14e913
Chris Makin <cmakin@amazon.com>
2024-07-22 16:30:21 -0400

    Update kitchen test and spec tests for login node dcv
    * Added `dcv_enabled` attribute to the login node log rotation in `kitchen.platform-config.yml`.
    * Updated `log_rotation_spec` to include dcv logs for login nodes.

--- cookbooks/aws-parallelcluster-platform/spec/unit/recipes/log_rotation_spec.rb
+++ cookbooks/aws-parallelcluster-platform/spec/unit/recipes/log_rotation_spec.rb
@@ -218,13 +218,15 @@ describe 'aws-parallelcluster-platform::log_rotation' do
         end
       end
 
-      context "in the login node when log_rotation enabled and pam ssh key generation is enabled" do
+      context "in the login node when log_rotation, pam ssh key generation, and dcv are enabled" do
         cached(:chef_run) do
           runner = runner(platform: platform, version: version) do |node|
             node.override['cluster']['node_type'] = "LoginNode"
             node.override['cluster']['log_rotation_enabled'] = 'true'
+            node.override['cluster']['dcv_enabled'] = "login_node"
             node.override['cluster']["directory_service"]["generate_ssh_keys_for_users"] = 'true'
             node.override['cluster']["scheduler"] = 'slurm'
+            allow_any_instance_of(Object).to receive(:dcv_installed?).and_return(true)
           end
           runner.converge(described_recipe)
         end
@@ -235,12 +237,12 @@ describe 'aws-parallelcluster-platform::log_rotation' do
           parallelcluster_supervisord_log_rotation
           parallelcluster_cloud_init_output_log_rotation
           parallelcluster_pam_ssh_key_generator_log_rotation
+          parallelcluster_dcv_log_rotation
         )
         unexpected_config_files = %w(
           parallelcluster_bootstrap_error_msg_log_rotation
           parallelcluster_cfn_init_log_rotation
           parallelcluster_chef_client_log_rotation
-          parallelcluster_dcv_log_rotation
           parallelcluster_clustermgtd_log_rotation
           parallelcluster_clusterstatusmgtd_log_rotation
           parallelcluster_slurm_fleet_status_manager_log_rotation
