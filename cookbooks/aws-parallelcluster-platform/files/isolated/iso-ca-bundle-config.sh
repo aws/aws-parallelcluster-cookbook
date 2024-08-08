61d620481d4d4ee063fec15877ed10283cf4baf2
hgreebe <141743196+hgreebe@users.noreply.github.com>
2024-08-08 11:00:12 -0400

    [ADC] Configure certificates for login nodes in ADC (#2786)

--- cookbooks/aws-parallelcluster-platform/files/isolated/iso-ca-bundle-config.sh
+++ cookbooks/aws-parallelcluster-platform/files/isolated/iso-ca-bundle-config.sh
@@ -12,6 +12,10 @@ echo "export AWS_CA_BUNDLE=/etc/pki/${REGION}/certs/ca-bundle.pem" >> /etc/profi
 
 echo "export AWS_DEFAULT_REGION=${REGION}" >> /etc/profile.d/aws-cli-default-config.sh
 
-echo "Defaults env_keep += \"AWS_DEFAULT_REGION AWS_CA_BUNDLE\"" > /etc/sudoers.d/pcluster-aws-cli-envkeep
+echo "export REQUESTS_CA_BUNDLE=${AWS_CA_BUNDLE}" >> /etc/profile.d/aws-cli-default-config.sh
+
+echo "export SSL_CERT_FILE=${AWS_CA_BUNDLE}" >> /etc/profile.d/aws-cli-default-config.sh
+
+echo "Defaults env_keep += \"AWS_DEFAULT_REGION AWS_CA_BUNDLE REQUESTS_CA_BUNDLE SSL_CERT_FILE\"" > /etc/sudoers.d/pcluster-aws-cli-envkeep
 
 source /etc/profile.d/aws-cli-default-config.sh
