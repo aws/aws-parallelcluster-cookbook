854261b65cc9793a044518dbd0989407db9904a6
hgreebe <141743196+hgreebe@users.noreply.github.com>
2024-08-14 13:53:06 -0400

    [ADC] Set default node package for build image command (#2788)

--- cookbooks/aws-parallelcluster-shared/attributes/environment.rb
+++ cookbooks/aws-parallelcluster-shared/attributes/environment.rb
@@ -5,4 +5,5 @@ default['cluster']['aws_domain'] = aws_domain
 
 # URL for ParallelCluster Artifacts stored in public S3 buckets
 # ['cluster']['region'] will need to be defined by image_dna.json during AMI build.
-default['cluster']['artifacts_s3_url'] = "https://#{node['cluster']['region']}-aws-parallelcluster.s3.#{node['cluster']['region']}.#{node['cluster']['aws_domain']}/archives"
+default['cluster']['s3_url'] = "https://#{node['cluster']['region']}-aws-parallelcluster.s3.#{node['cluster']['region']}.#{node['cluster']['aws_domain']}"
+default['cluster']['artifacts_s3_url'] = "#{node['cluster']['s3_url']}/archives"
