9069b3134771e4a3e53c557e824c21efb39a5b50
hgreebe <141743196+hgreebe@users.noreply.github.com>
2024-08-14 13:53:06 -0400

    [ADC] Set default node package for build image command

--- cookbooks/aws-parallelcluster-awsbatch/recipes/install.rb
+++ cookbooks/aws-parallelcluster-awsbatch/recipes/install.rb
@@ -15,6 +15,7 @@
 # limitations under the License.
 
 return if platform?('redhat')
+return if aws_region.start_with?("us-iso")
 
 include_recipe "::awsbatch_virtualenv"
 
@@ -28,9 +29,6 @@ end
 
 # Check whether install a custom aws-parallelcluster-awsbatch-cli package or the standard one
 # Install awsbatch cli into awsbatch virtual env
-if aws_region.start_with?("us-iso") && !node['cluster']['custom_awsbatchcli_package'].empty?
-  node.default['cluster']['custom_awsbatchcli_package'] = "#{node['cluster']['artifacts_s3_url']}/dependencies/awsbatch/aws-parallelcluster.tgz"
-end
 if !node['cluster']['custom_awsbatchcli_package'].nil? && !node['cluster']['custom_awsbatchcli_package'].empty?
   # Install custom aws-parallelcluster package
   bash "install aws-parallelcluster-awsbatch-cli" do
