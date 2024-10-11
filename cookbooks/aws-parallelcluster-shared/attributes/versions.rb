8241227093876584dc4cf6262ae6f6c843d105f8
gmarciani <299799+gmarciani@users.noreply.github.com>
2024-10-11 17:37:30 +0000

    Bump version to 3.11.1

--- cookbooks/aws-parallelcluster-shared/attributes/versions.rb
+++ cookbooks/aws-parallelcluster-shared/attributes/versions.rb
@@ -3,7 +3,7 @@ default['cluster']['python-version'] = '3.9.20'
 default['cluster']['python-major-minor-version'] = '3.9'
 
 # ParallelCluster versions
-default['cluster']['parallelcluster-version'] = '3.11.0'
-default['cluster']['parallelcluster-cookbook-version'] = '3.11.0'
-default['cluster']['parallelcluster-node-version'] = '3.11.0'
+default['cluster']['parallelcluster-version'] = '3.11.1'
+default['cluster']['parallelcluster-cookbook-version'] = '3.11.1'
+default['cluster']['parallelcluster-node-version'] = '3.11.1'
 default['cluster']['parallelcluster-awsbatch-cli-version'] = '1.4.0'
