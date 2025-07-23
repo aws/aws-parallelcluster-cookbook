d925ce256422c08962c571ca8c1fcf5d4da3fd0f
Himani Anil Deshpande <himanidp@amazon.com>
2025-07-23 15:43:01 -0400

    [Nvidia-imex] Never Install NVIDIA Imex for AL2

--- cookbooks/aws-parallelcluster-platform/resources/nvidia_imex/nvidia_imex_amazon2.rb
+++ cookbooks/aws-parallelcluster-platform/resources/nvidia_imex/nvidia_imex_amazon2.rb
@@ -15,3 +15,8 @@
 provides :nvidia_imex, platform: 'amazon', platform_version: '2'
 
 use 'partial/_nvidia_imex_common.rb'
+
+def imex_installed
+  # We do not install NVIDIA-Imex for Alinux2 due to restriction on NVIDIA driver
+  return true
+end
