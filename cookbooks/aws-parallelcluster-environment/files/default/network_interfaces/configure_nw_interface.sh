9439ed7f037ee66ec9aa8c0f5ddbd9ecb0b681f5
Helena Greebe <hgreebe@amazon.com>
2025-08-04 07:32:00 -0400

    Remove device name and ip address from required parameters of configure_nw_interface.sh

--- cookbooks/aws-parallelcluster-environment/files/default/network_interfaces/configure_nw_interface.sh
+++ cookbooks/aws-parallelcluster-environment/files/default/network_interfaces/configure_nw_interface.sh
@@ -8,12 +8,13 @@
 
 set -e
 
+# commented out inputs are not required and don't exist for efa-only interfaces
 if
-  [ -z "${DEVICE_NAME}" ] ||          # name of the device
+#  [ -z "${DEVICE_NAME}" ] ||          # name of the device
   [ -z "${DEVICE_NUMBER}" ] ||        # index of the device
   [ -z "${NETWORK_CARD_INDEX}" ] ||   # index of the network card
   [ -z "${GW_IP_ADDRESS}" ] ||        # gateway ip address
-  [ -z "${DEVICE_IP_ADDRESS}" ] ||    # ip address to assign to the interface
+#  [ -z "${DEVICE_IP_ADDRESS}" ] ||    # ip address to assign to the interface
   [ -z "${CIDR_PREFIX_LENGTH}" ] ||   # the prefix length of the device IP cidr block
   [ -z "${NETMASK}" ]                 # netmask to apply to device ip address
 then
