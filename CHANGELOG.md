7dcfeb2a0bd4ab281bc2e7403c1e74b42247a50e
Helena Greebe <hgreebe@amazon.com>
2025-08-07 10:40:11 -0400

    Update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,6 +15,7 @@ This file is used to list changes made in each version of the AWS ParallelCluste
 - Addressed cluster id mismatch known issue by deleting the file `/var/spool/slurm.state/clustername` before configuring Slurm accounting.
 - Upgrade DCV to version 2024.0-19030.
 - Remove `berkshelf`. All cookbooks are local and do not need `berkshelf` dependency management.
+- Add support for GB200 instance types
 
 **BUG FIXES**
 - Fix a race condition in CloudWatch Agent startup that could cause nodes bootstrap failures.
--- cookbooks/aws-parallelcluster-environment/files/default/network_interfaces/configure_nw_interface.sh
+++ cookbooks/aws-parallelcluster-environment/files/default/network_interfaces/configure_nw_interface.sh
@@ -8,13 +8,11 @@
 
 set -e
 
-# commented out inputs are not required and don't exist for efa-only interfaces
+# commented out inputs that are not required and don't exist for efa-only interfaces
 if
-#  [ -z "${DEVICE_NAME}" ] ||          # name of the device
   [ -z "${DEVICE_NUMBER}" ] ||        # index of the device
   [ -z "${NETWORK_CARD_INDEX}" ] ||   # index of the network card
   [ -z "${GW_IP_ADDRESS}" ] ||        # gateway ip address
-#  [ -z "${DEVICE_IP_ADDRESS}" ] ||    # ip address to assign to the interface
   [ -z "${CIDR_PREFIX_LENGTH}" ] ||   # the prefix length of the device IP cidr block
   [ -z "${NETMASK}" ]                 # netmask to apply to device ip address
 then
