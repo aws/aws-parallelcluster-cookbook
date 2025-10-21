41cbd7bc87631f7d2cd74d91575d5b5cf681f01e
Giacomo Marciani <mgiacomo@amazon.com>
2025-10-21 15:17:47 -0400

    [Performance] Remove cfn-hup from compute nodes as a temporary mitigation to restore good performance.
    Running cfn-hup on compute nodes can cause a major performance degradation on large clusters.
    
    This change will be vended as a custom cookbook to mitigate the performance degradation.
    
    Side effect of this change is that cluster updates will not be supported.

--- cookbooks/aws-parallelcluster-platform/spec/unit/recipes/supervisord_config_spec.rb
+++ cookbooks/aws-parallelcluster-platform/spec/unit/recipes/supervisord_config_spec.rb
@@ -70,7 +70,6 @@ describe 'aws-parallelcluster-platform::supervisord_config' do
 
         it 'has the correct content' do
           is_expected.to render_file('/etc/parallelcluster/parallelcluster_supervisord.conf')
-            .with_content("[program:cfn-hup]")
             .with_content("[program:computemgtd]")
 
           is_expected.not_to render_file('/etc/parallelcluster/parallelcluster_supervisord.conf')
