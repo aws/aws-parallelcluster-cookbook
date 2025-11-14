ccd2a0ea42f52428a8702e6d30c5813f05e54384
Himani Anil Deshpande <himanidp@amazon.com>
2025-11-14 12:25:54 -0500

    [LibJwt] Update libJWt version to v1.18.4 for all OS except for AL2

--- cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
+++ cookbooks/aws-parallelcluster-slurm/attributes/versions.rb
@@ -8,3 +8,11 @@ default['cluster']['slurm']['base_url'] = "#{node['cluster']['artifacts_s3_url']
 default['cluster']['munge']['munge_version'] = '0.5.16'
 default['cluster']['munge']['sha256'] = 'fa27205d6d29ce015b0d967df8f3421067d7058878e75d0d5ec3d91f4d32bb57'
 default['cluster']['munge']['base_url'] = "#{node['cluster']['artifacts_s3_url']}/dependencies/munge"
+# LibJwt
+default['cluster']['jwt']['version'] = '1.18.4'
+default['cluster']['jwt']['sha256'] = '8496257cb39ee7dddfdfc919e7b80a997399b0319f9fdcbefd374b0e4f147159'
+if platform?('amazon') && node['platform_version'] == "2"
+  default['cluster']['jwt']['version'] = '1.17.0'
+  default['cluster']['jwt']['sha256'] = '617778f9687682220abf9b7daacbe72bab7c2985479f8bee4db9648bd2440687'
+end
+default['cluster']['jwt']['base_url'] = "#{node['cluster']['artifacts_s3_url']}/dependencies/jwt"
