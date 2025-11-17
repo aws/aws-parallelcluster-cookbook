027cf0df1f11d1d9182261a2050ca4741cb1dfd1
Giacomo Marciani <mgiacomo@amazon.com>
2025-11-17 13:15:31 -0500

    [Test] Add test coverage for function `install_packages:default_packages`.

--- cookbooks/aws-parallelcluster-platform/spec/unit/resources/install_packages_spec.rb
+++ cookbooks/aws-parallelcluster-platform/spec/unit/resources/install_packages_spec.rb
@@ -10,6 +10,87 @@ class ConvergeInstallPackages
   end
 end
 
+describe 'install_packages default_packages' do
+  EXPECTED_PACKAGES = {
+    'amazon2' => %w(vim ksh tcsh zsh openssl11-devel ncurses-devel pam-devel net-tools openmotif-devel
+                    libXmu-devel hwloc-devel libdb-devel tcl-devel automake autoconf pyparted libtool
+                    httpd boost-devel system-lsb mlocate atlas-devel glibc-static iproute
+                    libffi-devel dkms libedit-devel sendmail cmake byacc libglvnd-devel libgcrypt-devel libevent-devel
+                    libxml2-devel perl-devel tar gzip bison flex gcc gcc-c++ patch
+                    rpm-build rpm-sign system-rpm-config cscope ctags diffstat doxygen elfutils
+                    gcc-gfortran git indent intltool patchutils rcs subversion swig systemtap curl
+                    jq wget python-pip NetworkManager-config-routing-rules
+                    python3 python3-pip iptables libcurl-devel yum-plugin-versionlock
+                    coreutils moreutils environment-modules bzip2 dos2unix),
+    'amazon2023' => %w(ksh tcsh zsh openssl-devel ncurses-devel pam-devel net-tools
+                       libXmu-devel hwloc-devel libdb-devel tcl-devel automake autoconf libtool
+                       httpd boost-devel mlocate R atlas-devel
+                       blas-devel libffi-devel dkms libedit-devel jq
+                       libical-devel sendmail libxml2-devel libglvnd-devel
+                       libgcrypt-devel libevent-devel glibc-static bind-utils
+                       iproute python3 python3-pip libcurl-devel git
+                       coreutils environment-modules gcc gcc-c++ bzip2 iptables vim yum-plugin-versionlock dos2unix),
+    'redhat' => %w(vim ksh tcsh zsh openssl-devel ncurses-devel pam-devel net-tools openmotif-devel
+                    libXmu-devel hwloc-devel libdb-devel tcl-devel automake autoconf libtool
+                    httpd boost-devel mlocate R atlas-devel
+                    blas-devel libffi-devel dkms libedit-devel jq
+                    libical-devel sendmail libxml2-devel libglvnd-devel
+                    libgcrypt-devel libevent-devel glibc-static bind-utils
+                    iproute NetworkManager-config-routing-rules python3 python3-pip iptables libcurl-devel yum-plugin-versionlock
+                    coreutils moreutils curl environment-modules gcc gcc-c++ bzip2 dos2unix),
+    'rocky' => %w(vim ksh tcsh zsh openssl-devel ncurses-devel pam-devel net-tools openmotif-devel
+                   libXmu-devel hwloc-devel libdb-devel tcl-devel automake autoconf libtool
+                   httpd boost-devel mlocate R atlas-devel
+                   blas-devel libffi-devel dkms libedit-devel jq
+                   libical-devel sendmail libxml2-devel libglvnd-devel
+                   libgcrypt-devel libevent-devel glibc-static bind-utils
+                   iproute NetworkManager-config-routing-rules python3 python3-pip iptables libcurl-devel yum-plugin-versionlock
+                   moreutils curl environment-modules gcc gcc-c++ bzip2 dos2unix coreutils),
+    'ubuntu' => %w(vim ksh tcsh zsh libssl-dev ncurses-dev libpam-dev net-tools libhwloc-dev dkms
+                     tcl-dev automake autoconf libtool librrd-dev libapr1-dev libconfuse-dev
+                     apache2 libboost-dev libdb-dev libncurses5-dev libpam0g-dev libxt-dev
+                     libmotif-dev libxmu-dev libxft-dev man-db jq
+                     r-base libblas-dev libffi-dev libxml2-dev
+                     libgcrypt20-dev libevent-dev iproute2 python3 python3-pip
+                     libatlas-base-dev libglvnd-dev iptables libcurl4-openssl-dev
+                     coreutils moreutils curl python3-parted environment-modules libdbus-1-dev dos2unix),
+  }.freeze
+
+  for_all_oses do |platform, version|
+    context "on #{platform}#{version}" do
+      cached(:chef_run) do
+        runner = runner(platform: platform, version: version, step_into: ['install_packages'])
+        ConvergeInstallPackages.setup(runner)
+      end
+      cached(:resource) do
+        chef_run.find_resource('install_packages', 'setup')
+      end
+
+      it 'returns expected packages' do
+        expected_key = %(amazon).include?(platform) ? "#{platform}#{version}" : "#{platform}"
+        expect(resource.default_packages).to eq(EXPECTED_PACKAGES[expected_key])
+      end
+
+      case "#{platform}#{version}"
+      when 'amazon2'
+        it 'excludes moreutils for us-iso regions' do
+          allow(resource).to receive(:aws_region).and_return('us-iso-WHATEVER')
+          expect(resource.default_packages).not_to include('moreutils')
+        end
+      when 'redhat8'
+        it 'excludes multiple packages for us-iso regions' do
+          allow(resource).to receive(:aws_region).and_return('us-iso-WHATEVER')
+          packages = resource.default_packages
+          %w(openmotif-devel hwloc-devel R blas-devel dkms libedit-devel glibc-static
+             NetworkManager-config-routing-rules yum-plugin-versionlock moreutils).each do |pkg|
+            expect(packages).not_to include(pkg)
+          end
+        end
+      end
+    end
+  end
+end
+
 describe 'install_packages:setup' do
   for_all_oses do |platform, version|
     context "on #{platform}#{version}" do
