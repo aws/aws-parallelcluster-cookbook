7aed198b9f03950f50dcd5f14c6eb9d8c6ab7089
Himani Anil Deshpande <himanidp@amazon.com>
2025-11-17 14:58:44 -0500

    [EFS] Add cmake and perl which are pre-requisite for efs-utils

--- cookbooks/aws-parallelcluster-environment/resources/efs/partial/_redhat_based.rb
+++ cookbooks/aws-parallelcluster-environment/resources/efs/partial/_redhat_based.rb
@@ -24,5 +24,5 @@ def install_script_code(efs_utils_tarball, efs_utils_package, efs_utils_version)
 end
 
 def prerequisites
-  %w(rpm-build make rust go cargo openssl-devel)
+  %w(rpm-build make rust go cargo openssl-devel cmake perl)
 end
