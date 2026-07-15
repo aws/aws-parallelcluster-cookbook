1b32fabc573c495ae998e0320d1bbbd969ae029f
Himani Anil Deshpande <himanidp@amazon.com>
2026-07-15 13:26:55 -0400

    [Deps] Upgrade PMIx 5.0.10 -> 5.0.11

--- cookbooks/aws-parallelcluster-slurm/attributes/slurm_attributes.rb
+++ cookbooks/aws-parallelcluster-slurm/attributes/slurm_attributes.rb
@@ -13,8 +13,8 @@ default["cluster"]["scheduler_compute_resource_name"] = nil
 default['cluster']['enable_nss_slurm'] = node['cluster']['directory_service']['enabled']
 
 # PMIX Version and Checksum
-default['cluster']['pmix']['version'] = '5.0.10'
-default['cluster']['pmix']['sha256'] = '7c0be0b5f85cc605fc51b44642fab72494e739ef9c1aa52a8741b847d6907b43'
+default['cluster']['pmix']['version'] = '5.0.11'
+default['cluster']['pmix']['sha256'] = '11d91183c4fd77117d9e7f186a1f4fde182895314b18fc5783fee7e2e5595e88'
 default['cluster']['pmix']['base_url'] = "#{node['cluster']['artifacts_s3_url']}/dependencies/pmix"
 
 # Slurmdbd
