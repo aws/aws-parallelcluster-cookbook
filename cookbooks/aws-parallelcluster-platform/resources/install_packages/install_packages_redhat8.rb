58296b2dfd864a08d644e68b2ffa3f3510a50846
hanwenli <hanwenli@amazon.com>
2026-09-23 11:21:57 -0700

    Upgrade RHEL EPEL dependencies to v3 for us-iso
    This change doesn't affect other OSes or other AWS partitions

--- cookbooks/aws-parallelcluster-platform/resources/install_packages/install_packages_redhat8.rb
+++ cookbooks/aws-parallelcluster-platform/resources/install_packages/install_packages_redhat8.rb
@@ -42,7 +42,7 @@ end
 action :install_extras do
   if aws_region.start_with?("us-iso")
     remote_file "epel_deps.tar.gz" do
-      source "#{node['cluster']['artifacts_s3_url']}/dependencies/epel/rhel8/x86_64/deps-v2.tar.gz"
+      source "#{node['cluster']['artifacts_s3_url']}/dependencies/epel/rhel8/x86_64/deps-v3.tar.gz"
       mode '0644'
       retries 3
       retry_delay 5
